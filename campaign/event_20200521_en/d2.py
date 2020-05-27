4302a66bd788860ae6af62ac5beb5599e4f989b7
kyO The NinjA <erikferreira@outlook.com>
2020-05-27 20:47:48 -0300

    Update d2.py

--- campaign/event_20200521_en/d2.py
+++ campaign/event_20200521_en/d2.py
@@ -23,15 +23,6 @@ MAP.weight_data = """
     40 30 40 30 40 40 40 40 40
     40 40 40 40 40 30 40 40 20
 """
-MAP.spawn_data = [
-    {'battle': 0, 'enemy': 2, 'siren': 3},
-    {'battle': 1, 'enemy': 1},
-    {'battle': 2, 'enemy': 2, 'siren': 2},
-    {'battle': 3, 'enemy': 1},
-    {'battle': 4, 'enemy': 2, 'siren': 1},
-    {'battle': 5, 'enemy': 1},
-    {'battle': 6, 'boss': 1},
-]
 
 class Config:
     SUBMARINE = 1
@@ -39,11 +30,12 @@ class Config:
 
     POOR_MAP_DATA = True
     MAP_HAS_AMBUSH = False
-    MAP_HAS_FLEET_STEP = True
-    MAP_HAS_MOVABLE_ENEMY = True
+    MAP_HAS_FLEET_STEP = False
+    MAP_HAS_MOVABLE_ENEMY = False
     MAP_HAS_SIREN = True
     MAP_HAS_DYNAMIC_RED_BORDER = True
     MAP_SIREN_COUNT = 3
+
 
 class Campaign(CampaignBase):
     MAP = MAP
