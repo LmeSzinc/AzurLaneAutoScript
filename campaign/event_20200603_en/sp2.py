a0668712b1feabf334424c554c144438bfb5d872
kyO The NinjA <erikferreira@outlook.com>
2020-06-03 16:35:23 -0300

    Add: [EN] Add 'Prelude under the Moon' SP2 support

--- campaign/event_20200603_en/sp2.py
+++ campaign/event_20200603_en/sp2.py
@@ -4,17 +4,28 @@ from module.map.map_grids import SelectedGrids, RoadGrids
 from module.logger import logger
 
 
-MAP = CampaignMap()
-MAP = CampaignMap('sp1')
-MAP.shape = 'h5'
-MAP.map_data = '''
-    ++ ++ ++ -- ME ME MB ++
-    -- -- -- ME -- ME -- --
-    -- -- ++ -- ME ++ MB --
-    SP SP -- ME -- ++ -- ME
-    SP SP -- -- ME -- ME --
-'''
+MAP = CampaignMap('sp2')
+MAP.shape = 'g7'
 MAP.camera_data = ['D3', 'D5']
+MAP.map_data = '''
+    ++ ++ -- ME -- ME MB
+    MS ++ -- -- ME -- MB
+    ME -- ME -- ++ ++ ++
+    -- -- -- ME -- ME MB
+    -- -- ++ -- ++ ME --
+    SP SP -- -- ++ -- ME
+    ++ SP -- ME -- ME --
+'''
+
+A1, B1, C1, D1, E1, F1, G1, \
+A2, B2, C2, D2, E2, F2, G2, \
+A3, B3, C3, D3, E3, F3, G3, \
+A4, B4, C4, D4, E4, F4, G4, \
+A5, B5, C5, D5, E5, F5, G5, \
+A6, B6, C6, D6, E6, F6, G6, \
+A7, B7, C7, D7, E7, F7, G7, \
+    = MAP.flatten()
+
 MAP.spawn_data = [
     {'battle': 0, 'enemy': 3},
     {'battle': 1, 'enemy': 1},
@@ -27,7 +38,6 @@ MAP.spawn_data = [
 class Config:
     SUBMARINE = 0
     FLEET_BOSS = 0
-
     MAP_HAS_AMBUSH = False
 
 
@@ -36,6 +46,7 @@ class Campaign(CampaignBase):
 
     def battle_0(self):
         return self.battle_default()
+
 
     def battle_4(self):
         return self.brute_clear_boss()
