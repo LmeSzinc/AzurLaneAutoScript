c786c9d31d600155b82b6bf18c04908cc69e4bee
whoamikyo <kyo@kyo.ninja>
2020-06-22 19:48:33 -0300

    Map: Updated 10-4

--- campaign/campaign_main/campaign_10_4.py
+++ campaign/campaign_main/campaign_10_4.py
@@ -3,35 +3,36 @@ from module.map.map_base import CampaignMap
 from module.map.map_grids import SelectedGrids, RoadGrids
 from module.logger import logger
 
+
 MAP = CampaignMap('10-4')
 MAP.shape = 'I6'
-MAP.map_data = '''
+MAP.camera_data = ['D2', 'D4', 'F2', 'F4']
+MAP.camera_data_spawn_point = []
+MAP.map_data = """
     MB ME ME -- ++ MB -- ++ ++
-    ME ME ++ ME ++ -- ME -- ++
-    -- -- ME ME ME ++ ME ME --
-    ++ ++ ++ -- ME -- ME ++ ME
-    SP -- -- ME -- ++ -- ME --
-    SP -- -- -- ME ++ ME -- MB
-'''
-MAP.weight_data = '''
-    10 30 30 10 10 10 10 10 10
-    5 30 10 30 10 10 5 10 10
-    10 10 5 4 30 10 5 30 10
-    10 10 10 10 10 10 5 10 30
-    10 10 10 10 10 10 10 30 10
-    10 10 10 10 30 10 5 10 10
-'''
-
+    ME ME ++ ME ++ -- Me -- ++
+    -- __ ME Me ME ++ ME ME --
+    ++ ++ ++ -- ME -- Me ++ ME
+    SP -- -- Me -- ++ __ ME --
+    SP -- -- -- Me ++ ME -- MB
+"""
+MAP.weight_data = """
+    10 10 10 10 10 10 10 10 10
+    10 10 10 10 10 10 10 10 10
+    10 10 10 10 10 10 10 10 10
+    10 10 10 10 10 10 10 10 10
+    10 10 10 10 10 10 10 10 10
+    10 10 10 10 10 10 10 10 10
+"""
 MAP.spawn_data = [
-     {'battle': 0, 'enemy': 4},
-     {'battle': 1, 'enemy': 2}, 
-     {'battle': 2, 'enemy': 2}, 
-     {'battle': 3, 'enemy': 1}, 
-     {'battle': 4, 'enemy': 1},
-     {'battle': 5,},
-     {'battle': 6, 'boss': 1},
- ]
-
+    {'battle': 0, 'enemy': 4},
+    {'battle': 1, 'enemy': 2},
+    {'battle': 2, 'enemy': 2},
+    {'battle': 3, 'enemy': 1},
+    {'battle': 4, 'enemy': 1},
+    {'battle': 5},
+    {'battle': 6, 'boss': 1},
+]
 A1, B1, C1, D1, E1, F1, G1, H1, I1, \
 A2, B2, C2, D2, E2, F2, G2, H2, I2, \
 A3, B3, C3, D3, E3, F3, G3, H3, I3, \
@@ -41,27 +42,11 @@ A6, B6, C6, D6, E6, F6, G6, H6, I6, \
     = MAP.flatten()
 
 step_on = SelectedGrids([E4, D3, G4, C3])
-road_main = RoadGrids([[D5, D3], [C3, A2], [E4, G4], [G2, G3, G6]])
+road_main = RoadGrids([D5, E4, D3, C3, A2, G4, H5, G3, G2])
 roadblocks_d4 = RoadGrids([[D5, E6], E4, D3])
 
 class Config:
-    INTERNAL_LINES_HOUGHLINES_THRESHOLD = 40
-    EDGE_LINES_HOUGHLINES_THRESHOLD = 40
-    COINCIDENT_POINT_ENCOURAGE_DISTANCE = 1.5
-    INTERNAL_LINES_FIND_PEAKS_PARAMETERS = {
-        'height': (150, 255 - 24),
-        'width': (0.9, 10),
-        'prominence': 10,
-        'distance': 35,
-    }
-    EDGE_LINES_FIND_PEAKS_PARAMETERS = {
-        'height': (255 - 24, 255),
-        'prominence': 10,
-        'distance': 50,
-        'width': (0, 10),
-        'wlen': 1000,
-    }
-
+    pass
 
 class Campaign(CampaignBase):
     MAP = MAP
@@ -80,8 +65,8 @@ class Campaign(CampaignBase):
     def battle_6(self):
         boss = self.map.select(is_boss=True)
         if boss:
-            if not self.check_accessibility(boss[0], fleet=2):
-                if self.clear_roadblocks([road_main]):
+            if not self.check_accessibility(boss[0], fleet='boss'):
+                if self.clear_potential_roadblocks([road_main]):
                     return True
 
         return self.fleet_2.clear_boss()
