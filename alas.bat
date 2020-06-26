57f51c1a0c0b7c7dfec707aab2b2b00fcb1a8ab8
whoamikyo <kyo@kyo.ninja>
2020-06-26 10:41:11 -0300

    Added option in updater to keep local changes
    - Now you can choose if want keep local changes like any custom map that you already done

--- alas.bat
+++ alas.bat
@@ -91,7 +91,7 @@ goto alas
 		if %menu%==1 GOTO en
 		if %menu%==2 GOTO cn
 		if %menu%==3 GOTO jp
-		if %menu%==4 GOTO updater_menu
+		if %menu%==4 GOTO choose_update_mode
 		if %menu%==exit GOTO EOF
 		
 		else (
@@ -162,7 +162,41 @@ rem 		cd AzurLaneAutoScript
 rem 		echo.
 rem 		goto updater_menu
 rem 	)
-rem :: -----------------------------------------------------------------------------
+:: -----------------------------------------------------------------------------
+:choose_update_mode
+	cls
+	echo.
+	echo.
+	echo	::Choose update method::
+	echo.
+	echo	1. Overwrite local changes (Will undo any local changes)
+	echo	2. Keep local changes (Useful if you have customized a map)
+	echo	3. Back to main menu
+	echo.
+	echo	:: Type a 'number' and press ENTER
+	echo	:: Type 'exit' to quit
+	echo.
+
+	set /P choice=
+		if %choice%==1 GOTO updater_menu
+		if %choice%==2 GOTO update_menu_local
+		if %choice%==3 GOTO alas
+		if %choice%==exit GOTO EOF
+		
+		else (
+		cls
+	echo.
+	echo  :: Incorrect Input Entered
+	echo.
+	echo     Please type a 'number' or 'exit'
+	echo     Press any key to return to the menu...
+	echo.
+		pause > NUL
+		goto alas
+		)
+
+
+:: -----------------------------------------------------------------------------
 :updater_menu
 SET GIT_ALAS=%~dp0toolkit\Git\cmd\git.exe
 SET GLP=%GIT_ALAS%
@@ -171,9 +205,8 @@ SET GLP=%GIT_ALAS%
 	echo	:: This update only will work if you downloaded ALAS on
 	echo	:: Release tab and installed with Easy_Install-v2.bat
 	echo.
-	echo	::DISCLAIMER::
+	echo	::Overwrite local changes::
 	echo.
-	echo	JUST RUN UPDATER INSIDE AzurLaneAutoScript FOLDER
 	echo.
 	echo	1. https://github.com/LmeSzinc/AzurLaneAutoScript (Main Repo, When in doubt, use it)
 	echo	2. https://github.com/whoamikyo/AzurLaneAutoScript (Mirrored Fork)
@@ -202,7 +235,48 @@ SET GLP=%GIT_ALAS%
 	echo     Press any key to return to the menu...
 	echo.
 		pause > NUL
-		goto updater_menu
+		goto alas
+		)
+:: -----------------------------------------------------------------------------
+:update_menu_local
+SET GIT_ALAS=%~dp0toolkit\Git\cmd\git.exe
+SET GLP=%GIT_ALAS%
+	cls
+	echo.
+	echo	:: This update only will work if you downloaded ALAS on
+	echo	:: Release tab and installed with Easy_Install-v2.bat
+	echo.
+	echo	::Keep local changes::
+	echo.
+	echo.
+	echo	1. https://github.com/LmeSzinc/AzurLaneAutoScript (Main Repo, When in doubt, use it)
+	echo	2. https://github.com/whoamikyo/AzurLaneAutoScript (Mirrored Fork)
+	echo	3. https://github.com/whoamikyo/AzurLaneAutoScript (nightly build, dont use)
+	echo	4. https://gitee.com/lmeszinc/AzurLaneAutoScript.git (Recommended for CN users)
+	echo	5. Back to main menu
+	echo.
+	echo	:: Type a 'number' and press ENTER
+	echo	:: Type 'exit' to quit
+	echo.
+
+	set /P choice=
+		if %choice%==1 GOTO LmeSzinc_local
+		if %choice%==2 GOTO whoamikyo_local
+		if %choice%==3 GOTO nightly_local
+		if %choice%==4 GOTO gitee_local
+		if %choice%==5 GOTO alas
+		if %choice%==exit GOTO EOF
+		
+		else (
+		cls
+	echo.
+	echo  :: Incorrect Input Entered
+	echo.
+	echo     Please type a 'number' or 'exit'
+	echo     Press any key to return to the menu...
+	echo.
+		pause > NUL
+		goto alas
 		)
 :: -----------------------------------------------------------------------------
 :LmeSzinc
@@ -223,7 +297,7 @@ SET GLP=%GIT_ALAS%
 		echo AzurLaneAutoScript\toolkit\Git\cmd
 		echo.
         pause > NUL
-        goto updater_menu
+        goto alas
 	)
 :: -----------------------------------------------------------------------------
 :whoamikyo
@@ -243,7 +317,7 @@ SET GLP=%GIT_ALAS%
 		echo check if you have this directory:
 		echo AzurLaneAutoScript\toolkit\Git\cmd
         pause > NUL
-        goto updater_menu
+        goto alas
 	)
 :: -----------------------------------------------------------------------------
 :nightly
@@ -256,14 +330,34 @@ SET GLP=%GIT_ALAS%
 	call %GLP% pull --ff-only whoamikyo nightly
 	echo Press any key to proceed
 	pause > NUL
-	goto updater_menu
+	goto alas
 	) else (
 		echo  :: Git not detected, maybe there was an installation issue
 		echo check if you have this directory:
 		echo AzurLaneAutoScript\toolkit\Git\cmd
 		echo.
         pause > NUL
-        goto updater_menu
+        goto alas
+	)
+:: -----------------------------------------------------------------------------
+:gitee
+	call %GLP% --version >nul
+	if %errorlevel% == 0 (
+	echo GIT Found! Proceeding..
+	echo Updating from LmeSzinc repository..
+	call %GLP% fetch lmeszincgitee master
+	call %GLP% reset --hard lmeszincgitee/master
+	call %GLP% pull --ff-only lmeszincgitee master
+	echo DONE!
+	echo Press any key to proceed
+	pause > NUL
+	goto updater_menu
+	) else (
+		echo  :: Git not detected, maybe there was an installation issue
+		echo check if you have this directory:
+		echo AzurLaneAutoScript\toolkit\Git\cmd
+        pause > NUL
+        goto alas
 	)
 :: -----------------------------------------------------------------------------
 rem :check_connection
@@ -290,25 +384,6 @@ rem         pause > NUL
 rem         goto start_gitee
 rem 	)
 :: -----------------------------------------------------------------------------
-:gitee
-	call %GLP% --version >nul
-	if %errorlevel% == 0 (
-	echo GIT Found! Proceeding..
-	echo Updating from LmeSzinc repository..
-	call %GLP% fetch lmeszincgitee master
-	call %GLP% reset --hard lmeszincgitee/master
-	call %GLP% pull --ff-only lmeszincgitee master
-	echo DONE!
-	echo Press any key to proceed
-	pause > NUL
-	goto updater_menu
-	) else (
-		echo  :: Git not detected, maybe there was an installation issue
-		echo check if you have this directory:
-		echo AzurLaneAutoScript\toolkit\Git\cmd
-        pause > NUL
-        goto updater_menu
-	)
 :: -----------------------------------------------------------------------------
 :toolkit
 	call %GLP% --version >nul
@@ -341,6 +416,89 @@ rem 	)
         goto updater_menu
 	)
 :: -----------------------------------------------------------------------------
+rem Keep local changes
+:: -----------------------------------------------------------------------------
+:LmeSzinc_local
+	call %GLP% --version >nul
+	if %errorlevel% == 0 (
+	echo GIT Found! Proceeding..
+	echo Updating from LmeSzinc repository..
+	call %GLP% stash
+	call %GLP% pull origin master
+	call %GLP% stash pop
+	echo DONE!
+	echo Press any key to proceed
+	pause > NUL
+	goto update_menu_local
+	) else (
+		echo  :: Git not detected, maybe there was an installation issue
+		echo check if you have this directory:
+		echo AzurLaneAutoScript\toolkit\Git\cmd
+		echo.
+        pause > NUL
+        goto alas
+	)
+:: -----------------------------------------------------------------------------
+:whoamikyo_local
+	call %GLP% --version >nul
+	if %errorlevel% == 0 (
+	echo GIT Found! Proceeding..
+	echo Updating from whoamikyo repository..
+	call %GLP% stash
+	call %GLP% pull whoamikyo master
+	call %GLP% stash pop
+	echo DONE!
+	echo Press any key to proceed
+	pause > NUL
+	goto update_menu_local
+	) else (
+		echo  :: Git not detected, maybe there was an installation issue
+		echo check if you have this directory:
+		echo AzurLaneAutoScript\toolkit\Git\cmd
+        pause > NUL
+        goto alas
+	)
+:: -----------------------------------------------------------------------------
+:nightly_local
+	call %GLP% --version >nul
+	if %errorlevel% == 0 (
+	echo GIT Found! Proceeding..
+	echo Updating from whoamikyo nightly repository..
+	call %GLP% stash
+	call %GLP% pull whoamikyo nightly
+	call %GLP% stash pop
+	echo Press any key to proceed
+	pause > NUL
+	goto update_menu_local
+	) else (
+		echo  :: Git not detected, maybe there was an installation issue
+		echo check if you have this directory:
+		echo AzurLaneAutoScript\toolkit\Git\cmd
+		echo.
+        pause > NUL
+        goto alas
+	)
+:: -----------------------------------------------------------------------------
+:gitee_local
+	call %GLP% --version >nul
+	if %errorlevel% == 0 (
+	echo GIT Found! Proceeding..
+	echo Updating from LmeSzinc repository..
+	call %GLP% stash
+	call %GLP% pull lmeszincgitee master
+	call %GLP% stash pop
+	echo DONE!
+	echo Press any key to proceed
+	pause > NUL
+	goto update_menu_local
+	) else (
+		echo  :: Git not detected, maybe there was an installation issue
+		echo check if you have this directory:
+		echo AzurLaneAutoScript\toolkit\Git\cmd
+        pause > NUL
+        goto alas
+	)
+
 :EOF
 exit
 
