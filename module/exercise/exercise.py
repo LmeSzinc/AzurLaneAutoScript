fd1a3da943d5c1298fc88397fe53ed18fad006fb
LmeSzinc <lmeszincsales@gmail.com>
2020-07-17 05:34:51 +0800

    Fix: Solve merge conflicts

--- module/exercise/exercise.py
+++ module/exercise/exercise.py
@@ -26,7 +26,6 @@ class Exercise(ExerciseCombat):
                 if success:
                     return success
             self._new_opponent()
-            self._opponent_fleet_check_all()
         return False
 
     def run(self):
