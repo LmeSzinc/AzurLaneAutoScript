af9f028df7a00ebdbec707f706f6561826382722
Nicholas Jin <nicholas.jin@gmail.com>
2020-07-16 20:26:14 -0400

    improved drop statistics module

--- dev_tools/item_statistics.py
+++ dev_tools/item_statistics.py
@@ -23,14 +23,15 @@ from module.logger import logger
 Set your folder here
 Examples: xxx/campaign_7_2
 """
-IMAGE_FOLDER = 'screenshots/campaign_10_3_HARD'
+IMAGE_FOLDER = 'screenshots/SkyboundOratorio/d1'
 STATUS_ITEMS_INTERVAL = 10
-TEMPLATE_THRESHOLD = 0.9
+# 0.9 is sometimes unable to distinguish between equipment of different rarities
+TEMPLATE_THRESHOLD = 0.95
 
 BATTLE_STATUS_FOLDER = f'{IMAGE_FOLDER}/status'
 GET_ITEMS_FOLDER = f'{IMAGE_FOLDER}/get_items'
 TEMPLATE_FOLDER = f'{IMAGE_FOLDER}/item_template'
-ALL_TEMPLATES_FOLDER = f'screenshots/item_templates_all'
+TEMPLATE_ARCHIVE_FOLDER = f'screenshots/item_template_archive'
 
 for f_ in [TEMPLATE_FOLDER]:
     if not os.path.exists(f_):
@@ -40,6 +41,8 @@ ITEM_GRIDS_1_ODD = ButtonGrid(origin=(336, 298), delta=(128, 0), button_shape=(9
 ITEM_GRIDS_1_EVEN = ButtonGrid(origin=(400, 298), delta=(128, 0), button_shape=(96, 96), grid_shape=(4, 1))
 ITEM_GRIDS_2 = ButtonGrid(origin=(336, 227), delta=(128, 142), button_shape=(96, 96), grid_shape=(5, 2))
 ENEMY_GENRE_BUTTON = Button(area=(782, 285, 961, 319), color=(), button=(), name='ENEMY_GENRE')
+
+
 
 
 class AmountOcr(Ocr):
@@ -92,31 +95,139 @@ class ItemTemplate:
         image = Image.fromarray(self.image)
         image.save(f'{TEMPLATE_FOLDER}/{name}.png')
 
+    def save_archive(self, name):
+        image = Image.fromarray(self.image)
+        image.save(f'{TEMPLATE_ARCHIVE_FOLDER}/{name}.png')
+
+"""
+Ideal workflow:
+Run dev_tools/item_statistics on a directory. it generates templates and stores a copy
+in both the map and in the archive
+
+At your leisure, you can rename the templates in either directory.
+
+On the next pass, the script scans for matching templates in both directories and
+attempts to match the names accordingly.
+
+Assumptions:
+Templates are essentially unique.
+
+
+Pseudocode:
+
+instantiate map list and master list
+    for each entry in map list:
+        if name is int:
+            check master list for match
+            if match and match name is not int:
+                use master name
+
+
+for item in image:
+    match item to map list
+    if item in map list:
+        if name is number:
+            search master list
+
+    if item not in map list:
+        if item in master list:
+            use master list name, duplicate entry in map list
+        if item not in master list
+            add new template (numbered) to both lists
+"""
 
 class ItemTemplateGroup:
     def __init__(self):
+        # all templates
+        self.templates_archive = {}
+        # map templates
         self.templates = {}
+
+        for file in os.listdir(TEMPLATE_ARCHIVE_FOLDER):
+            name = file[:-4]
+            image = Image.open(f'{TEMPLATE_ARCHIVE_FOLDER}/{file}').convert('RGB')
+            self.templates_archive[name] = ItemTemplate(image)
+
         for file in os.listdir(TEMPLATE_FOLDER):
             name = file[:-4]
             image = Image.open(f'{TEMPLATE_FOLDER}/{file}').convert('RGB')
             self.templates[name] = ItemTemplate(image)
 
+        # Ensures names match. If mismatched, resolve the mismatch
+        for name, template in self.templates.items():
+            for name_archive, template_archive in self.templates_archive.items():
+                if template.match(template_archive.image) and name_archive != name:
+                    logger.warning(f"template name mismatch between template collections")
+
+                    # If both names mismatched and numeric/numeric or nonnumeric/nonnumeric,
+                    # assign them a new numeric id.
+                    if (name_archive.isdigit() and name.isdigit()) or (not name_archive.isdigit() and not name.isdigit()):
+                        if (not name_archive.isdigit()) and (not name.isdigit()):
+                            logger.warning(f"Identical template has different names, assigning new numeric ID")
+                        if name_archive.isdigit() and name.isdigit():
+                            logger.warning(f"Mismatched numeric IDs, assigning a new one")
+                        os.remove(f'{TEMPLATE_FOLDER}/{name}.png')
+                        os.remove(f'{TEMPLATE_ARCHIVE_FOLDER}/{name_archive}.png')
+                        self.templates.pop(name)
+                        self.templates_archive.pop(name_archive)
+
+                        name = [int(n) for n in self.templates_archive.keys() if n.isdigit()]
+                        if len(name):
+                            name = str(max(name) + 1)
+                        else:
+                            name = str(len(self.templates.keys()) + 1)
+
+                        logger.info(f'Generating replacement template: {name}')
+                        self.templates[name] = template
+                        self.templates_archive[name] = template
+
+                        template.save(name)
+                        template_archive.save_archive(name)
+
+
+                    # Change the numeric name to the non-numeric one.
+                    else:
+                        if name.isdigit():
+                            logger.info(f"renaming template in map directory: {name} -> {name_archive}")
+                            os.remove(f'{TEMPLATE_FOLDER}/{name}.png')
+                            self.templates.pop(name)
+                            self.templates[name_archive] = template
+                            template_archive.save(name_archive)
+                        elif name_archive.isdigit():
+                            logger.info(f"renaming template in archive: {name_archive} -> {name}")
+                            os.remove(f'{TEMPLATE_ARCHIVE_FOLDER}/{name_archive}.png')
+                            self.templates_archive.pop(name_archive)
+                            self.templates_archive[name] = template
+                            template.save_archive(name)
+        # Ensures every template in the map folder also exists in the archive
+        for name, template in self.templates.items():
+            if name not in self.templates_archive.keys():
+                logger.warning(f"map template {name} missing from archive! Added")
+                template.save_archive(name)
     def match(self, item):
         for name, template in self.templates.items():
             if template.match(item.image):
                 return name
+        for name, template in self.templates_archive.items():
+            if template.match(item.image):
+                self.templates[name] = template
+                template.save(name)
+                return name
 
         template = ItemTemplate(item.get_template())
-        name = [int(n) for n in self.templates.keys() if n.isdigit()]
+        name = [int(n) for n in self.templates_archive.keys() if n.isdigit()]
         if len(name):
             name = str(max(name) + 1)
         else:
-            name = str(len(self.templates.keys()) + 1)
+            name = str(len(self.templates_archive.keys()) + 1)
 
         logger.info(f'New item template: {name}')
         self.templates[name] = template
+        self.templates_archive[name] = template
         template.save(name)
+        template.save_archive(name)
         return name
+
 
 
 template_group = ItemTemplateGroup()
