55345df19952f73dcb87544a8b6dfce7c647d8ce
LmeSzinc <lmeszincsales@gmail.com>
2020-07-26 16:24:52 +0800

    Fix: event_20200723_cn D2 D3 function name typo

--- campaign/event_20200723_cn/d3.py
+++ campaign/event_20200723_cn/d3.py
@@ -94,7 +94,7 @@ class Campaign(CampaignBase):
 
         return self.battle_default()
 
-    def bettle_5(self):
+    def battle_5(self):
         if self.clear_enemy(scale=(1,)):
             return True
         if self.clear_enemy(scale=(2,), genre=['light', 'main', 'enemy', 'carrier']):
