8df41b3e01eeaad4e7dafadde4017abc398c5c11
whoamikyo <kyo@kyo.ninja>
2020-08-06 12:49:45 -0300

    Opt: optmize 11-4 and 12-4
    Docs: added wiki link on readme

--- campaign/campaign_main/campaign_12_4.py
+++ campaign/campaign_main/campaign_12_4.py
@@ -3,30 +3,30 @@ from module.map.map_base import CampaignMap
 from module.map.map_grids import SelectedGrids, RoadGrids
 from module.logger import logger
 
-
 MAP = CampaignMap('12-4')
 MAP.shape = 'K8'
-MAP.map_data = '''
+MAP.camera_data = ['D2', 'D6', 'H2', 'H6']
+MAP.camera_data_spawn_point = []
+MAP.map_data = """
     MB MB ME -- ME ++ ++ ++ MB MB ++
-    ME ++ -- ME -- MA ++ ++ ME ME ++
-    -- ME -- ME ME -- ME ME -- ME --
-    ++ -- ME ++ ++ ME ME -- ++ ++ ME
+    ME ++ -- ME -- MA ++ ++ ME Me ++
+    -- ME __ Me Me -- Me Me -- Me --
+    ++ -- ME ++ ++ Me ME __ ++ ++ ME
     ++ ME ME -- ME ME -- ME -- ++ --
-    ++ -- ME ME -- ME ME ++ -- -- ME
-    ME -- ME -- ME -- ME -- -- ME --
+    ++ __ Me Me -- Me ME ++ __ -- ME
+    ME -- Me -- Me -- Me -- -- ME --
     -- -- -- ME SP SP ++ ++ ++ ME --
-'''
-MAP.weight_data = '''
+"""
+MAP.weight_data = """
     10 10 10 10 10 10 10 10 10 10 10
     10 10 10 10 10 10 10 10 10 10 10
     10 10 10 10 10 10 10 10 10 10 10
-    10 10 10 10 10 10 10 10 10 10 20
-    10 10 10 10 10 10 10 10 10 10 20
     10 10 10 10 10 10 10 10 10 10 10
     10 10 10 10 10 10 10 10 10 10 10
     10 10 10 10 10 10 10 10 10 10 10
-'''
-MAP.camera_data = ['D3', 'D6', 'H3', 'H6']
+    10 10 10 10 10 10 10 10 10 10 10
+    10 10 10 10 10 10 10 10 10 10 10
+"""
 MAP.spawn_data = [
     {'battle': 0, 'enemy': 2},
     {'battle': 1, 'enemy': 2},
@@ -36,8 +36,6 @@ MAP.spawn_data = [
     {'battle': 5},
     {'battle': 6, 'boss': 1},
 ]
-# MAP.in_map_swipe_preset_data = (2, 1)
-
 A1, B1, C1, D1, E1, F1, G1, H1, I1, J1, K1, \
 A2, B2, C2, D2, E2, F2, G2, H2, I2, J2, K2, \
 A3, B3, C3, D3, E3, F3, G3, H3, I3, J3, K3, \
@@ -48,9 +46,7 @@ A7, B7, C7, D7, E7, F7, G7, H7, I7, J7, K7, \
 A8, B8, C8, D8, E8, F8, G8, H8, I8, J8, K8, \
     = MAP.flatten()
 
-# ROAD_MAIN = RoadGrids([[B6, C5]])
-
-road_main = RoadGrids([[B6, C5]])
+road_main = RoadGrids([[H3, B6, C5]])
 
 class Config:
     INTERNAL_LINES_FIND_PEAKS_PARAMETERS = {
