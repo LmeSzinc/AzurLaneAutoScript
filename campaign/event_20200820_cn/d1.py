7f35bd83e564a6baed836be7e8783da5036aa694
LmeSzinc <lmeszincsales@gmail.com>
2020-08-21 04:55:48 +0800

    Fix: Wrong spawn data in D1

--- campaign/event_20200820_cn/d1.py
+++ campaign/event_20200820_cn/d1.py
@@ -31,7 +31,8 @@ MAP.spawn_data = [
     {'battle': 1, 'enemy': 1},
     {'battle': 2, 'enemy': 2},
     {'battle': 3, 'enemy': 1},
-    {'battle': 4, 'enemy': 2},
+    # Extract: {'battle': 4, 'enemy': 2},
+    {'battle': 4, 'enemy': 1},
     {'battle': 5, 'enemy': 1, 'boss': 1},
 ]
 A1, B1, C1, D1, E1, F1, G1, H1, I1, \
