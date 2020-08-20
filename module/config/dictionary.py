e19ca73683bf0f77dcc331f1d0b47833d0f41e8c
LmeSzinc <lmeszincsales@gmail.com>
2020-08-20 22:23:33 +0800

    Add: Chapter AB in Scherzo of Iron and Blood Rerun (event_20200820_cn)

--- module/config/dictionary.py
+++ module/config/dictionary.py
@@ -551,6 +551,7 @@ dic_event = {
         '复刻特别演习埃塞克斯级': 'raid_20200624',
         '永夜幻光': 'event_20200723_cn',
         '复刻最重要的宝物': 'event_20200806_cn',
+        '复刻铁血音符誓言': 'event_20200820_cn',
     },
     'en': {
         'Iris_of_Light_and_Dark': 'event_20200521_en',
@@ -559,12 +560,14 @@ dic_event = {
         'Air_Raid_Drills_with_Essex': 'raid_20200624',
         'Ink_Stained_Steel_Sakura_Rerun': 'event_20200716_en',
         'Aurora_Noctis': 'event_20200723_cn',
+        'Scherzo_of_Iron_and_Blood_Rerun': 'event_20200820_cn',
     },
     'jp': {
         '峡湾間の反撃': 'event_20200603_jp',
         '特別演習超空強襲波（復刻）': 'raid_20200624',
         '極夜照らす幻光': 'event_20200723_cn',
         '鉄血鮫とエニグマ（復刻）': 'event_20200806_cn',
+        '黒鉄の楽章_誓いの海（復刻）': 'event_20200820_cn',
     },
 }
 
