77f5442da0d9bdb8c791ba85a1ae557e3f56cddf
LmeSzinc <lmeszincsales@gmail.com>
2020-09-07 22:49:01 +0800

    Dev: Update grids_debug.py

--- dev_tools/grids_debug.py
+++ dev_tools/grids_debug.py
@@ -2,48 +2,21 @@ import module.config.server as server
 
 server.server = 'cn'  # Don't need to edit, it's used to avoid error.
 
-from module.map.grids import Grids
+from module.map_detection.view import View
 from module.config.config import AzurLaneConfig
 from PIL import Image
+import numpy as np
 
 """
 This file is use to debug a perspective error.
-It will call the map detection module (module/map/grids.py), outside Alas.
+It will call the map detection module (module/map_detection/view.py), outside Alas.
 """
-
-"""
-Put image here.
-"""
-file = ''
 
 
 class Config:
-    pass
-
-    INTERNAL_LINES_HOUGHLINES_THRESHOLD = 40
-    EDGE_LINES_HOUGHLINES_THRESHOLD = 40
-    COINCIDENT_POINT_ENCOURAGE_DISTANCE = 1.5
-
-    INTERNAL_LINES_FIND_PEAKS_PARAMETERS = {
-        'height': (150, 255 - 24),
-        'width': (0.9, 10),
-        'prominence': 10,
-        'distance': 35,
-    }
-    EDGE_LINES_FIND_PEAKS_PARAMETERS = {
-        'height': (255 - 24, 255),
-        'prominence': 10,
-        'distance': 50,
-        'width': (0, 10),
-        'wlen': 1000
-    }
-
-    # These are the full configuration in module/config/config.py
     """
-    MAP_HAS_SIREN = False
-    MAP_HAS_DYNAMIC_RED_BORDER = False
-    MAP_SIREN_TEMPLATE = ['1', '2', '3', 'DD']
-
+    Here are the default settings.
+    """
     # Parameters for scipy.signal.find_peaks
     # https://docs.scipy.org/doc/scipy/reference/generated/scipy.signal.find_peaks.html
     INTERNAL_LINES_FIND_PEAKS_PARAMETERS = {
@@ -74,10 +47,63 @@ class Config:
     ERROR_LINES_TOLERANCE = (-10, 10)
     MID_DIFF_RANGE_H = (129 - 3, 129 + 3)
     MID_DIFF_RANGE_V = (129 - 3, 129 + 3)
+
     """
+    Step 1:
+        Paste your config here.
+    """
+    pass
 
 
-cfg = AzurLaneConfig().merge(Config())
-grids = Grids(Image.open(file), cfg)
-grids.predict()
-grids.show()
+"""
+Step 2:
+    Put your image here.
+"""
+file = ''
+image = np.array(Image.open(file).convert('RGB'))
+
+
+"""
+Step 3:
+    Choose one method, uncomment the code, run.
+    There will be logs of local map view, and an image popup showing grid lines. Check if they are correct.
+"""
+# ==============================
+# Method 1
+# Perspective backend.
+# ==============================
+# cfg = Config()
+# cfg.DETECTION_BACKEND = 'perspective'
+# view = View(AzurLaneConfig('template').merge(cfg))
+# view.load(image)
+# view.predict()
+# view.show()
+# view.backend.draw()
+
+# ==============================
+# Method 2:
+# Homography with real-time perspective calculation.
+# This is the default method in Alas currently.
+# ==============================
+cfg = Config()
+cfg.DETECTION_BACKEND = 'homography'
+view = View(AzurLaneConfig('template').merge(cfg))
+view.load(image)
+view.predict()
+view.show()
+view.backend.draw()
+
+# ==============================
+# Method 3:
+# Homography with hard-coded perspective data (HOMO_STORAGE).
+# Get HOMO_STORAGE from log or from method 2.
+# ==============================
+# cfg = Config()
+# cfg.DETECTION_BACKEND = 'homography'
+# view = View(AzurLaneConfig('template').merge(cfg))
+# homo_storage = ()  # Paste your HOMO_STORAGE here.
+# view.backend.load_homography(storage=homo_storage)
+# view.load(image)
+# view.predict()
+# view.show()
+# view.backend.draw()
