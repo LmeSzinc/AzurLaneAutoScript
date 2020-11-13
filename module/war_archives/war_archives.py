ac45a16439480e50f15d2a5dc7a27b2bad53c6d9
nEEtdo0d <chibi.mailynn@gmail.com>
2020-11-13 01:09:04 -0500

    Fix: OCR must be done in the archive campaign stage map, verify able to

--- module/war_archives/war_archives.py
+++ module/war_archives/war_archives.py
@@ -8,12 +8,14 @@ DATA_KEY_CAMPAIGN = DigitCounter(OCR_DATA_KEY_CAMPAIGN, letter=(255, 247, 247), 
 
 class CampaignWarArchives(CampaignRun, CampaignBase):
     def triggered_stop_condition(self):
-        # Check for 0 data keys left to use
-        current, remain, total = DATA_KEY_CAMPAIGN.ocr(self.device.image)
-        logger.info(f'Inventory: {current} / {total}, Remain: {current}')
-        if remain == total:
-            logger.hr('Triggered out of data keys')
-            return True
+        # In case already inside campaign, OCR cannot be read otherwise
+        if self._in_archives_campaign():
+            # Check for 0 data keys left to use
+            current, remain, total = DATA_KEY_CAMPAIGN.ocr(self.device.image)
+            logger.info(f'Inventory: {current} / {total}, Remain: {current}')
+            if remain == total:
+                logger.hr('Triggered out of data keys')
+                return True
 
         # Else, check other stop conditions
         return super().triggered_stop_condition()
