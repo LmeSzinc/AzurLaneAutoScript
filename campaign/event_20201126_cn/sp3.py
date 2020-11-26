2a49ecaa46b40699567b99bc12e9b7a3e21962cc
LmeSzinc <37934724+LmeSzinc@users.noreply.github.com>
2020-11-26 22:33:18 +0800

    Add: Siren templates in SP3

--- campaign/event_20201126_cn/sp3.py
+++ campaign/event_20201126_cn/sp3.py
@@ -55,6 +55,9 @@ class Config(ConfigBase):
     MAP_HAS_AMBUSH = False
     # ===== End of generated config =====
 
+    MAP_SWIPE_MULTIPLY = 1.626
+    MAP_SWIPE_MULTIPLY_MINITOUCH = 1.572
+
 
 class Campaign(CampaignBase):
     MAP = MAP
