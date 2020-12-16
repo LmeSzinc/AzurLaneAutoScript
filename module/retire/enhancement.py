8649e67b9243526b47f80105c0eae26f02ea6734
nEEtdo0d <chibi.mailynn@gmail.com>
2020-12-15 23:18:43 -0500

    Opt: Revise enhancement module implementation similarly to ambush.py using templates

--- module/retire/enhancement.py
+++ module/retire/enhancement.py
@@ -1,11 +1,38 @@
 import numpy as np
+from random import choice
 
 from module.base.timer import Timer
 from module.base.utils import color_bar_percentage
-from module.combat.assets import GET_ITEMS_1
 from module.logger import logger
+from module.combat.assets import GET_ITEMS_1
 from module.retire.assets import *
+from module.template.assets import TEMPLATE_ENHANCE_SUCCESS, TEMPLATE_ENHANCE_FAILED, TEMPLATE_ENHANCE_IN_BATTLE
+from module.handler.assets import INFO_BAR_DETECT
 from module.retire.dock import Dock, CARD_GRIDS
+
+
+VALID_SHIP_TYPES = ['dd', 'ss', 'cl', 'ca', 'bb', 'cv', 'repair', 'others']
+
+
+def enhance_letter_preprocess(image):
+    """
+    Args:
+        image (np.ndarray):
+
+    Returns:
+        np.ndarray
+    """
+    image = image.astype(float)
+    image = (image - 64) / 0.75
+    image[image > 255] = 255
+    image[image < 0] = 0
+    image = image.astype('uint8')
+    return image
+
+
+TEMPLATE_ENHANCE_SUCCESS.image = enhance_letter_preprocess(TEMPLATE_ENHANCE_SUCCESS.image)
+TEMPLATE_ENHANCE_FAILED.image = enhance_letter_preprocess(TEMPLATE_ENHANCE_FAILED.image)
+TEMPLATE_ENHANCE_IN_BATTLE.image = enhance_letter_preprocess(TEMPLATE_ENHANCE_IN_BATTLE.image)
 
 
 class Enhancement(Dock):
@@ -63,193 +90,119 @@ class Enhancement(Dock):
         self.dock_filter_set(category='index', type='all', enable=True)
         self.dock_filter_confirm()
 
-    def _enhance_confirm(self):
+    def _enhance_confirm(self, skip_first_screenshot=True):
         """
         Pages:
             in: EQUIP_CONFIRM
             out: page_ship_enhance, without info_bar
         """
-        executed = False
-        while 1:
-            self.device.screenshot()
 
-            # if self.appear_then_click(ENHANCE_CONFIRM, offset=(30, 30), interval=3):
-            #     continue
+        confirm_timer = Timer(1.5, count=3).start()
+        while 1:
+            if skip_first_screenshot:
+                skip_first_screenshot = False
+            else:
+                self.device.screenshot()
+
             if self.appear_then_click(EQUIP_CONFIRM, offset=(30, 30), interval=3):
+                confirm_timer.reset()
                 continue
             if self.appear_then_click(EQUIP_CONFIRM_2, offset=(30, 30), interval=3):
+                confirm_timer.reset()
                 continue
             if self.appear(GET_ITEMS_1, interval=2):
                 self.device.click(GET_ITEMS_1_RETIREMENT_SAVE)
                 self.interval_reset(ENHANCE_CONFIRM)
-                executed = True
+                confirm_timer.reset()
                 continue
 
             # End
-            if executed and self.appear(ENHANCE_CONFIRM, offset=(30, 30)):
-                self.ensure_no_info_bar()
-                break
+            if self.appear(ENHANCE_CONFIRM, offset=(30, 30)):
+                if confirm_timer.reached():
+                    break
+            else:
+                confirm_timer.reset()
 
-    def _enhance_choose(self, skip_first_screenshot=True):
+    def _enhance_choose(self, ship_count):
         """
+        Re-vamped implementation, utilizing
+        templates and info_bar text to determine
+        whether a ship can or cannot be enhanced
+        Method similar to ambush.py
+
         Pages:
             in: page_ship_enhance, without info_bar
             out: EQUIP_CONFIRM
+
+        Args:
+            ship_count (int): ship_count, must be
+            non-zero positive integer
+
+        Returns:
+            True if able to enhance otherwise False
+            Always paired with current ship_count
         """
-        end_activate_timer = Timer(2, count=2) if self.config.DEVICE_CONTROL_METHOD == 'minitouch' else Timer(2, count=1)
-        trapped_timer = Timer(15, count=3).start()
-        trapped = False
+        skip_until_ensured = True
+        enhanced = False
         while 1:
-            if skip_first_screenshot:
-                skip_first_screenshot = False
-            else:
-                self.device.screenshot()
+            # Base Case: No more ships left to check for this category
+            if ship_count <= 0:
+                logger.info('Reached maximum number to check, exiting current category')
+                return False, ship_count
 
-            if self.appear(EQUIP_CONFIRM, offset=(30, 30)):
-                return True
-
-            if not end_activate_timer.reached_and_reset():
-                continue
-
-            ensured = self.equip_sidebar_ensure(index=4)
-            if ensured:
-                self.wait_until_appear(ENHANCE_RECOMMEND, offset=(5, 5), skip_first_screenshot=True)
-            else:
-                continue
-
-            status = color_bar_percentage(self.device.image, area=ENHANCE_RELOAD.area, prev_color=(231, 178, 74))
-            logger.attr('Reload_enhanced', f'{int(status * 100)}%')
-            choose = np.sum(np.array(self.device.image.crop(ENHANCE_FILLED.area)) > 200) > 100
-
-            if trapped or self.info_bar_count():
-                if status > 0.98:
-                    logger.info('Fully enhanced for this ship')
-                    swiped = self.equip_view_next(check_button=ENHANCE_RECOMMEND)
-                    self.ensure_no_info_bar()
-                    if not swiped:
-                        return False
-                    trapped_timer.reset()
-                    trapped = False
+            if skip_until_ensured:
+                if not self.equip_sidebar_ensure(index=4):
                     continue
-                else:
-                    if choose:
-                        logger.info('Unable to enhance this ship')
-                        swiped = self.equip_view_next(check_button=ENHANCE_RECOMMEND)
-                        self.ensure_no_info_bar()
-                        if not swiped:
-                            return False
-                        trapped_timer.reset()
-                        trapped = False
-                        continue
-                    else:
-                        logger.info('Enhancement material exhausted')
-                        return False
-
-            if not trapped_timer.reached_and_reset() and self.appear_then_click(ENHANCE_RECOMMEND, offset=(5, 5), interval=2):
-                self.device.sleep(0.3)
-                self.device.click(ENHANCE_CONFIRM)
-            else:
-                logger.warning('Current status appears trapped, will force stat gauge check')
-                trapped = True
-
-    def _enhance_choose_simple(self, current_index=0, skip_first_screenshot=True):
-        """
-        Description:
-            Simplified _enhance_choose focuses only on
-            appearance of info_bars, stat gauge is ignored
-            Minimum 2 times before swiping, if maximum of
-            4 times is detected, a forced swipe is initiated
-            A maximum of 4 ships will be checked to account
-            for 'in battle' status, hard-coded otherwise if
-            desired can be configurable
-
-        Pages:
-            in: page_ship_enhance, without info_bar
-            out: EQUIP_CONFIRM
-        """
-        end_activate_timer = Timer(2, count=2) if self.config.DEVICE_CONTROL_METHOD == 'minitouch' else Timer(2, count=1)
-        next_timer = Timer(2, count=1).start()
-        trapped_timer = Timer(15, count=3).start()
-        trapped = False
-        while 1:
-            if skip_first_screenshot:
-                skip_first_screenshot = False
+                self.wait_until_appear(ENHANCE_RECOMMEND, offset=(5, 5), skip_first_screenshot=True)
+                skip_until_ensured = False
             else:
                 self.device.screenshot()
 
-            if self.appear(EQUIP_CONFIRM, offset=(30, 30)):
-                return True, current_index
-
-            if not end_activate_timer.reached_and_reset():
-                continue
-
-            ensured = self.equip_sidebar_ensure(index=4)
-            if ensured:
-                self.wait_until_appear(ENHANCE_RECOMMEND, offset=(5, 5), skip_first_screenshot=True)
-            else:
-                continue
-
-            if trapped or self.info_bar_count():
-                if trapped or next_timer.reached():
-                    logger.info('Unable to enhance this ship, swipe to next')
-                    swiped = self.equip_view_next(check_button=ENHANCE_RECOMMEND)
+            # Respond accordingly based on info_bar information
+            if self.info_bar_count():
+                image = enhance_letter_preprocess(np.array(self.device.image.crop(INFO_BAR_DETECT.area)))
+                if TEMPLATE_ENHANCE_SUCCESS.match(image):
+                    enhanced = True
+                elif TEMPLATE_ENHANCE_FAILED.match(image):
+                    logger.info('Enhancement failed. Swiping to next ship if feasible')
                     self.ensure_no_info_bar()
-                    if not swiped or current_index >= 3:
-                        logger.info('Cannot swipe or ship check threshold reached, exiting current category')
-                        return False, current_index
-                    else:
-                        next_timer.reset()
-                        trapped_timer.reset()
-                        trapped = False
-                        logger.info(f'Try next ship: {3 - current_index}/3 remaining until give up')
-                        current_index += 1
+                    if self.equip_view_next(check_button=ENHANCE_RECOMMEND):
+                        skip_iteration = True
+                        ship_count -= 1
                         continue
+                    else:
+                        logger.info('Swiped failed, exiting current category')
+                        return False, ship_count
+                elif TEMPLATE_ENHANCE_IN_BATTLE.match(image):
+                    logger.info('Enhancement impossible, ship currently in battle. Swiping to next ship if feasible')
+                    self.ensure_no_info_bar()
+                    if self.equip_view_next(check_button=ENHANCE_RECOMMEND):
+                        skip_iteration = True
+                        continue
+                    else:
+                        logger.info('Swiped failed, exiting current category')
+                        return False, ship_count
 
-            if not trapped_timer.reached_and_reset() and self.appear_then_click(ENHANCE_RECOMMEND, offset=(5, 5), interval=2):
+
+            # Possible trapped case in which info_bar will never appear
+            # so long as EQUIP_CONFIRM remains appeared
+            if enhanced or self.appear(EQUIP_CONFIRM, offset=(30, 30)):
+                logger.info('Enhancement Successful')
+                return True, ship_count
+
+            # Perform actions to attempt enhancement
+            if self.appear_then_click(ENHANCE_RECOMMEND, offset=(5, 5), interval=2):
                 self.device.sleep(0.3)
                 self.device.click(ENHANCE_CONFIRM)
-            else:
-                logger.warning('Current status appears trapped, will force swipe to next ship')
-                trapped = True
+                logger.info('Enhancing...')
 
     def enhance_ships(self, favourite=None):
         """
-        Pages:
-            in: page_dock
-            out: page_dock
+        Enhance target ships by specified order
+        of types listed in ENHANCE_ORDER_STRING
 
-        Args:
-            favourite (bool):
-
-        Returns:
-            int: total enhanced
-        """
-        if favourite is None:
-            favourite = self.config.ENHANCE_FAVOURITE
-
-        logger.hr('Enhancement')
-        logger.info(f'Favourite={favourite}')
-        self._enhance_enter(favourite=favourite)
-        total = 0
-
-        # At least one card present, able to enhance
-        if self._enhance_enter(favourite=favourite):
-            while 1:
-                if not self._enhance_choose():
-                    break
-                self._enhance_confirm()
-                total += 10
-                if total >= self._retire_amount:
-                    break
-
-        self._enhance_quit()
-        return total
-
-    def enhance_ships_order(self, favourite=None):
-        """
-        Info:
-            Target ships in order of specified
-            type listing by ENHANCE_ORDER_STRING
+        Invalid types are treated as requesting
+        from ALAS to choose a valid one at random
 
         Pages:
             in: page_dock
@@ -267,25 +220,39 @@ class Enhancement(Dock):
         logger.hr('Enhancement by type')
         total = 0
 
+        # Process ENHANCE_ORDER_STRING if any into ship_types
         ship_types = [s.strip().lower() for s in self.config.ENHANCE_ORDER_STRING.split('>')]
-        enable_simple = True
-        if ship_types == ['']:
-            enable_simple = False
+        ship_types = list(filter(('').__ne__, ship_types))
+        if len(ship_types) == 0:
             ship_types = [None]
         logger.attr('Enhance Order', ship_types)
 
+        # Process available ship types for choice randomization
+        # Removing types that have already been specified by
+        # ENHANCE_ORDER_STRING
+        available_ship_types = VALID_SHIP_TYPES.copy()
+        [available_ship_types.remove(s) for s in ship_types if s in available_ship_types]
+
         for ship_type in ship_types:
-            index = 0 # Helper variable only for _enhance_choose_simple
+            # None check, do not execute if is None
+            # Otherwise, select a type at random since
+            # user has specified an unrecognized type
+            if ship_type is not None and ship_type not in VALID_SHIP_TYPES:
+                ship_type = choice(available_ship_types)
+                available_ship_types.remove(ship_type)
+
             logger.info(f'Favourite={favourite}, Ship Type={ship_type}')
+
+            # Continue if at least 1 CARD_GRID is selectable
+            # otherwise skip to next ship type
             if not self._enhance_enter(favourite=favourite, ship_type=ship_type):
                 logger.hr(f'Dock Empty by ship type {ship_type}')
                 continue
 
+            # current_count = self.config.ENHANCE_CHECK_PER_CATEGORY
+            current_count = 2
             while 1:
-                if enable_simple:
-                    choose_result, index = self._enhance_choose_simple(current_index=index)
-                else:
-                    choose_result = self._enhance_choose()
+                choose_result, current_count = self._enhance_choose(ship_count=current_count)
                 if not choose_result:
                     break
                 self._enhance_confirm()
@@ -309,7 +276,7 @@ class Enhancement(Dock):
         self.ui_click(RETIRE_APPEAR_3, check_button=DOCK_FILTER, skip_first_screenshot=True)
         self.handle_dock_cards_loading()
 
-        total = self.enhance_ships_order()
+        total = self.enhance_ships()
 
         self.dock_quit()
         self.config.DOCK_FULL_TRIGGERED = True
