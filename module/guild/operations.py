3739b3045aba3f38dec38721a23186a2baa9c35d
nEEtdo0d <chibi.mailynn@gmail.com>
2021-01-20 13:48:32 -0500

    Fix: Exit too early to join operation Ensure if join operation is present, click Ensure if unable to join operation click safe area to exit to reach page that can exit

--- module/guild/operations.py
+++ module/guild/operations.py
@@ -1,11 +1,14 @@
 from module.base.button import ButtonGrid
+from module.base.decorator import cached_property
 from module.base.timer import Timer
 from module.base.utils import *
 from module.guild.assets import *
 from module.guild.base import GuildBase
+from module.handler.assets import INFO_BAR_DETECT
+from module.handler.info_handler import info_letter_preprocess
 from module.logger import logger
 from module.map_detection.utils import Points
-from module.template.assets import TEMPLATE_OPERATIONS_RED_DOT
+from module.template.assets import TEMPLATE_OPERATIONS_RED_DOT, TEMPLATE_OPERATIONS_INSUFFICIENT
 
 RECORD_OPTION_DISPATCH = ('RewardRecord', 'operations_dispatch')
 RECORD_SINCE_DISPATCH = (6, 12, 18, 21,)
@@ -14,11 +17,19 @@ RECORD_SINCE_BOSS = (0,)
 
 
 class GuildOperations(GuildBase):
+    @cached_property
+    def _load_operations_template(self):
+        TEMPLATE_OPERATIONS_INSUFFICIENT.image = info_letter_preprocess(TEMPLATE_OPERATIONS_INSUFFICIENT.image)
+        return True
+
     def _guild_operations_ensure(self, skip_first_screenshot=True):
         """
         Ensure guild operation is loaded
         After entering guild operation, background loaded first, then dispatch/boss
         """
+        _ = self._load_operations_template
+        confirm_timer = Timer(1.5, count=3).start()
+        confirm_info = False
         while 1:
             if skip_first_screenshot:
                 skip_first_screenshot = False
@@ -26,11 +37,20 @@ class GuildOperations(GuildBase):
                 self.device.screenshot()
 
             if self.appear_then_click(GUILD_OPERATIONS_JOIN, interval=3):
+                confirm_timer.reset()
                 continue
+
+            if self.info_bar_count() and not confirm_info:
+                image = info_letter_preprocess(np.array(self.device.image.crop(INFO_BAR_DETECT.area)))
+                if TEMPLATE_OPERATIONS_INSUFFICIENT.match(image) and not self.appear(GUILD_OPERATIONS_ACTIVE_CHECK, offset=(20, 20)):
+                    logger.info('Insufficient monthly attempts to join operation')
+                    self.device.click(GUILD_OPERATIONS_CLICK_SAFE_AREA)
+                    confirm_info = True
+                    confirm_timer.reset()
 
             # End
             if self.appear(GUILD_BOSS_ENTER) or self.appear(GUILD_OPERATIONS_ACTIVE_CHECK, offset=(20, 20)):
-                if not self.info_bar_count():
+                if not self.info_bar_count() and confirm_timer.reached():
                     break
 
     def _guild_operation_get_mode(self):
