8fa789da7c2a430d77f3274ad1f25b62d52d96e3
nEEtdo0d <chibi.mailynn@gmail.com>
2021-02-06 00:53:43 -0500

    Upd: get_map_shape implementation sufficient for EN as is optimized for by server index and attribute for OCR object

--- module/os/map_operation.py
+++ module/os/map_operation.py
@@ -1,12 +1,23 @@
 from module.base.utils import *
+from module.base.decorator import Config
+import module.config.server as server
 from module.map.map_operation import MapOperation
 from module.os.assets import *
 from module.ocr.ocr import Ocr
 from module.logger import logger
 from module.os.map_data import DIC_OS_MAP
 
+
 class OSMapOperation(MapOperation):
     os_map_name = 'Unknown'
+
+    @Config.when(SERVER='en')
+    def _ocr_map_name(self):
+        return Ocr(MAP_NAME, lang='cnocr', letter=(214, 235, 235), threshold=160, name='OCR_OS_MAP_NAME')
+
+    @Config.when(SERVER='cn')
+    def _ocr_map_name(self):
+        return Ocr(MAP_NAME, lang='cnocr', letter=(235, 235, 235), threshold=160, name='OCR_OS_MAP_NAME')
 
     def is_meowfficer_searching(self):
         """
@@ -30,9 +41,7 @@ class OSMapOperation(MapOperation):
             self.device.image, area=MEOWFFICER_SEARCHING_PERCENTAGE.area, prev_color=(74, 223, 255))
 
     def get_map_shape(self):
-        # For CN only
-        ocr = Ocr(MAP_NAME, lang='cnocr', letter=(235, 235, 235), threshold=160, name='OCR_OS_MAP_NAME')
-        name = ocr.ocr(self.device.image)
+        name = self._ocr_map_name().ocr(self.device.image)
         if '-' in name:
             name = name.split('-')[0]
         else:
@@ -40,10 +49,10 @@ class OSMapOperation(MapOperation):
         logger.info(f'Map name processed: {name}')
 
         for index, chapter in DIC_OS_MAP.items():
-            if name == chapter['cn']:
+            if name == chapter[server.server]:
                 self.os_map_name = name
                 logger.info(
-                    f"Current OS map: {chapter['cn']}, "
+                    f"Current OS map: {chapter[server.server]}, "
                     f"id: {index}, shape: {chapter['shape']}, hazard_level: {chapter['hazard_level']}"
                 )
                 return chapter['shape']
