dfc5e03a65d62f47b8498e183f5b7711647ce526
LmeSzinc <37934724+LmeSzinc@users.noreply.github.com>
2021-02-25 18:09:01 +0800

    Fix: Siren scaling in Khorovod of Dawns Rime chapter A

--- campaign/event_20210225_cn/a1.py
+++ campaign/event_20210225_cn/a1.py
@@ -53,6 +53,11 @@ class Config:
     MAP_HAS_AMBUSH = False
     # ===== End of generated config =====
 
+    MAP_ENEMY_GENRE_DETECTION_SCALING = {
+        'DD': 1.111,
+        'CL': 1.111,
+    }
+
 
 class Campaign(CampaignBase):
     MAP = MAP
