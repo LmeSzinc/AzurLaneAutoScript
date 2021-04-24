4a7bb632b548cd27f4cf34688b2310515f1a0cd7
LmeSzinc <37934724+LmeSzinc@users.noreply.github.com>
2021-04-24 15:52:28 +0800

    Opt: Enemy preference in Daedalian Hymn

--- campaign/event_20210422_cn/d3.py
+++ campaign/event_20210422_cn/d3.py
@@ -77,6 +77,21 @@ class Campaign(CampaignBase):
         if self.clear_siren():
             return True
 
+        if self.clear_enemy(scale=(2,)):
+            return True
+        if self.clear_enemy(scale=(3,)):
+            return True
+
+        return self.battle_default()
+
+    def battle_5(self):
+        if self.clear_enemy(scale=(1,)):
+            return True
+        if self.clear_enemy(scale=(2,)):
+            return True
+        if self.clear_enemy(scale=(3,)):
+            return True
+
         return self.battle_default()
 
     def battle_6(self):
