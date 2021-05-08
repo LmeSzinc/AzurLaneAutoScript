c5c5db8f800354aceb2cf19018c3de39787428e7
nEEtdo0d <chibi.mailynn@gmail.com>
2021-05-08 13:46:18 -0400

    Rev: [EN] Undo commit 642a8239ae0ffde679cc4ca895576795d923cb95
    Fix: Remove redundant sleep before wait

--- module/equipment/equipment.py
+++ module/equipment/equipment.py
@@ -27,7 +27,6 @@ class Equipment(UI):
                 swipe_timer.reset()
                 self.device.swipe(vector=(distance, 0), box=SWIPE_AREA.area, random_range=SWIPE_RANDOM_RANGE,
                                   padding=0, duration=(0.1, 0.12), name='EQUIP_SWIPE')
-                self.device.sleep((0.5, 0.8))
                 self.wait_until_appear(check_button)
                 swipe_count += 1
 
