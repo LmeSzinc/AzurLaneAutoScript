c0597e7835e41a18a91e10ff391c78dfafcb9b19
nEEtdo0d <chibi.mailynn@gmail.com>
2021-07-09 20:44:02 -0400

    Fix: incorrect attr reference, incorrect expression

--- module/ui/page_bar.py
+++ module/ui/page_bar.py
@@ -22,7 +22,7 @@ class PageBar:
         self.inactive_color = inactive_color
         self.additional = additional
         self.is_reversed = is_reversed
-        self.name = name if not None else grids._name
+        self.name = name if name is not None else grids._name
 
         self.grid_shape = self.grids.grid_shape
         self.is_vertical = True if self.grid_shape[1] > self.grid_shape[0] else False
@@ -37,14 +37,14 @@ class PageBar:
         Args:
             main (ModuleBase):
             index (int):
-                Varies on self.grid
+                Varies on self.grids
 
         Returns:
             bool: if changed.
         """
         current = 0
         total = 0
-        buttons = self.grid.buttons()
+        buttons = self.grids.buttons()
         if self.is_reversed:
             buttons.reverse()
         for idx, button in enumerate(buttons):
@@ -92,7 +92,7 @@ class PageBar:
                 pos = (0, diff)
             else:
                 pos = (diff, 0)
-            main.device.click(self.grid[pos])
+            main.device.click(self.grids[pos])
         else:
             logger.warning(f'Target index {index} cannot be clicked')
         return True
@@ -106,7 +106,7 @@ class PageBar:
         Args:
             main (ModuleBase):
             index (int):
-                Varies on self.grid
+                Varies on self.grids
             skip_first_screenshot (bool):
 
         Returns:
