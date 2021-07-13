b28027f13569d18ecd011e69d606e41b0cf3b035
nEEtdo0d <chibi.mailynn@gmail.com>
2021-07-12 22:25:23 -0400

    Fix: [EN] use normal appear for innate interval option

--- module/ui/ui.py
+++ module/ui/ui.py
@@ -301,7 +301,7 @@ class UI(InfoHandler):
         if self.appear(EVENT_LIST_CHECK, offset=(30, 30), interval=5) \
                 or self.appear(RESHMENU_CHECK, offset=(30, 30), interval=5) \
                 or self.appear(RESEARCH_CHECK, offset=(30, 30), interval=5) \
-                or self.ui_page_appear(page_shipyard):
+                or self.appear(SHIPYARD_CHECK, offset=(30, 30), interval=5):
             self.device.click(GOTO_MAIN)
             self._ui_additional_reward_goto_main_timer.reset()
             return True
