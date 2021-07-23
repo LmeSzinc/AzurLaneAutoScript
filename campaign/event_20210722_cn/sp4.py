5b7a31b4626ad22ace9ea52908e927803dafda2b
LmeSzinc <37934724+LmeSzinc@users.noreply.github.com>
2021-07-23 21:01:37 +0800

    Add: Siren templates in Azur Anthem SP4

--- campaign/event_20210722_cn/sp4.py
+++ campaign/event_20210722_cn/sp4.py
@@ -58,6 +58,10 @@ class Config(ConfigBase):
     MAP_HAS_AMBUSH = False
     # ===== End of generated config =====
 
+    MAP_SWIPE_MULTIPLY = 1.541
+    MAP_SWIPE_MULTIPLY_MINITOUCH = 1.490
+    MAP_ENSURE_EDGE_INSIGHT_CORNER = 'bottom'
+
 
 class Campaign(CampaignBase):
     MAP = MAP
