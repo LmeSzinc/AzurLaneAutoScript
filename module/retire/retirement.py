c3683a5cb219d5505c48577c7812db815682a55d
Ray <railzy00@gmail.com>
2021-08-14 13:22:49 +0800

    Fix: [JP] use SR_SSR_CONFIRM buttom in JP

--- module/retire/retirement.py
+++ module/retire/retirement.py
@@ -124,7 +124,7 @@ class Retirement(Enhancement):
             if self.config.RETIRE_SR or self.config.RETIRE_SSR or self.config.RETIREMENT_METHOD == 'one_click_retire':
                 if self.handle_popup_confirm('RETIRE_SR_SSR'):
                     continue
-                if self.config.SERVER == 'en' and \
+                if (self.config.SERVER == 'en' or self.config.SERVER == 'jp') and \
                         self.appear_then_click(SR_SSR_CONFIRM, offset=self._popup_offset, interval=2):
                     continue
             if self.appear_then_click(SHIP_CONFIRM, offset=(30, 30), interval=2):
