c62f216706d3ad7e9f05fa2487530afbdcb8faf3
18870 <a20110123@163.com>
2021-09-17 13:31:24 +0800

    Fix: Able to handle multiple machine fortresses in B3/D3

--- campaign/event_20210916_cn/campaign_base.py
+++ campaign/event_20210916_cn/campaign_base.py
@@ -1,10 +1,12 @@
+from typing import List
+
 from module.campaign.campaign_base import CampaignBase as CampaignBase_
 from module.logger import logger
 from module.map_detection.grid import GridInfo
 
 
 class CampaignBase(CampaignBase_):
-    MACHINE_FORTRESS: GridInfo
+    MACHINE_FORTRESS: List[GridInfo]
 
     def handle_clear_mode_config_cover(self):
         if super().handle_clear_mode_config_cover():
@@ -13,7 +15,8 @@ class CampaignBase(CampaignBase_):
         else:
             if hasattr(self, 'MACHINE_FORTRESS'):
                 logger.info(f'Set machine fortress: {self.MACHINE_FORTRESS}')
-                self.MACHINE_FORTRESS.manual_siren = True
+                for grid in self.MACHINE_FORTRESS:
+                    grid.manual_siren = True
             else:
                 logger.info(f'No machine fortress in this stage')
                 return False
