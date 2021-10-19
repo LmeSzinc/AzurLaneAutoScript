b76f162a773d5c9aa0f97f835a8d12906c2642cb
呜路塞 <44494948+wulusai2333@users.noreply.github.com>
2021-10-19 14:48:31 +0800

    Start and pause services and hide windows
    - `alas_start.bat` if port 22236 is occupied, kill the process and start services
    - `alas_down.bat` kill the process on port 22236

---
+++ alas_down.bat
@@ -0,0 +1,5 @@
+@echo off
+if not "%2"=="snow" mshta vbscript:createobject("wscript.shell").run("""%~F0"" wind snow",vbhide)(window.close)&&exit
+for /f "tokens=5" %%a in ('netstat /ano ^| findstr 22267') do taskkill /F /pid %%a
+::conda activate alas && python gui.py
+exit
