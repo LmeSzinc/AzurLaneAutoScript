55b9cc59e729554e2c19a3ac7f4847bc208b5ecd
vlziw9y8 <93026912+vlziw9y8@users.noreply.github.com>
2021-10-23 14:09:13 +0800

    Fix invalid warning when do first adb connect

--- module/device/connection.py
+++ module/device/connection.py
@@ -86,11 +86,10 @@ class Connection:
             for _ in range(3):
                 msg = self.adb_command(['connect', serial]).decode("utf-8").strip()
                 logger.info(msg)
-                if 'already' in msg:
+                if 'connected' in msg:
                     return True
-                else:
-                    logger.warning(f'Failed to connect {serial} after 3 trial.')
-                    return False
+            logger.warning(f'Failed to connect {serial} after 3 trial.')
+            return False
 
     def connect(self, serial):
         """Connect to a device.
