fff3f59cafc6f10aa84450cc01a84d99833a211b
julystone <julei@esunny.cc>
2021-10-26 17:46:04 +0800

    ADD：os_meowfficer_farming add specificSea Explore

--- module/os/operation_siren.py
+++ module/os/operation_siren.py
@@ -263,16 +263,24 @@ class OperationSiren(Reward, OSMap):
                 self.config.OS_ACTION_POINT_PRESERVE = self.config.OpsiMeowfficerFarming_ActionPointPreserve
 
             # (1252, 1012) is the coordinate of zone 134 (the center zone) in os_globe_map.png
-            zones = self.zone_select(hazard_level=self.config.OpsiMeowfficerFarming_HazardLevel) \
-                .delete(SelectedGrids([self.zone])) \
-                .delete(SelectedGrids(self.zones.select(is_port=True))) \
-                .sort_by_clock_degree(center=(1252, 1012), start=self.zone.location)
+            zone_id = self.config.OpsiMeowfficerFarming_SeaArea
+            if zone_id != 0:
+                logger.hr(f'OS meowfficer farming, zone_id={zone_id}', level=1)
+                self.globe_goto(zone_id)
+                self.run_auto_search()
+                self.handle_fleet_repair(revert=False)
+                self.globe_goto(self.zone_nearest_azur_port(zone=zone_id))
+            else:
+                zones = self.zone_select(hazard_level=self.config.OpsiMeowfficerFarming_HazardLevel) \
+                    .delete(SelectedGrids([self.zone])) \
+                    .delete(SelectedGrids(self.zones.select(is_port=True))) \
+                    .sort_by_clock_degree(center=(1252, 1012), start=self.zone.location)
 
-            logger.hr(f'OS meowfficer farming, zone_id={zones[0].zone_id}', level=1)
-            self.globe_goto(zones[0])
-            self.run_auto_search()
-            self.handle_fleet_repair(revert=False)
-            self.config.check_task_switch()
+                logger.hr(f'OS meowfficer farming, zone_id={zones[0].zone_id}', level=1)
+                self.globe_goto(zones[0])
+                self.run_auto_search()
+                self.handle_fleet_repair(revert=False)
+                self.config.check_task_switch()
 
     def os_explore(self):
         """
