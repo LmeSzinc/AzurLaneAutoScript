bdf091ada3354f9ddc6a32e849ae24d789607fda
julystone <julei@esunny.cc>
2021-11-09 16:38:41 +0800

    Add:reward_weekly_mission

--- module/config/config_generated.py
+++ module/config/config_generated.py
@@ -179,6 +179,7 @@ class GeneratedConfig:
     Reward_CollectCoin = True
     Reward_CollectExp = True
     Reward_CollectMission = True
+    Reward_CollectWeeklyMission = True
 
     # Group `GeneralShop`
     GeneralShop_UseGems = False
