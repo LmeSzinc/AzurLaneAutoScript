809708ec9791011603b84f3da0b3bc102590d8b1
LmeSzinc <37934724+LmeSzinc@users.noreply.github.com>
2021-11-11 22:29:47 +0800

    Refactor: Data Key (#601)

--- module/data_key/data_key.py
+++ module/data_key/data_key.py
@@ -1,86 +1,70 @@
-from module.base.timer import Timer
 from module.combat.assets import GET_ITEMS_1
+from module.data_key.assets import *
 from module.logger import logger
 from module.ocr.ocr import DigitCounter
-from module.data_key.assets import OCR_DATA_KEY, DATA_KEY_COLLECT, DATA_KEY_COLLECTED
-from module.ui.assets import WAR_ARCHIVES_CHECK
+from module.ui.assets import WAR_ARCHIVES_CHECK, CAMPAIGN_MENU_GOTO_WAR_ARCHIVES
 from module.ui.ui import UI, page_archives
-from module.war_archives.assets import WAR_ARCHIVES_EX_ON
 
 DATA_KEY = DigitCounter(OCR_DATA_KEY, letter=(255, 247, 247), threshold=64)
-RECORD_OPTION = ('RewardRecord', 'data_key')
-RECORD_SINCE = (0,)
 
 
 class RewardDataKey(UI):
-    def _data_key_collect_confirm(self, skip_first_screenshot=True):
+    def _data_key_collect(self, skip_first_screenshot=True):
         """
-        Wait for results: get_items or cancel popup.
-
         Pages:
             in: page_archives
-            out: page_archives
+            out: page_archives, DATA_KEY_COLLECTED
         """
-        confirm_timer = Timer(1.5, count=3).start()
+        logger.hr('Data Key Collect')
         while 1:
             if skip_first_screenshot:
                 skip_first_screenshot = False
             else:
                 self.device.screenshot()
 
-            if self.appear(GET_ITEMS_1, offset=5, interval=3):
-                # Clicking any blank area in page_archives will exit to page_campaign.
-                # Click WAR_ARCHIVES_EX to avoid this, if double clicking GET_ITEMS.
-                self.device.click(WAR_ARCHIVES_EX_ON)
-                confirm_timer.reset()
+            if self.appear_then_click(DATA_KEY_COLLECT, offset=(20, 20), interval=3):
+                continue
+            if self.appear(GET_ITEMS_1, offset=20, interval=3):
+                self.device.click(DATA_KEY_COLLECT)
                 continue
             if self.handle_popup_confirm('DATA_KEY_LIMIT'):
                 # If it's in 29/30 means user is not doing war achieves frequently,
                 # no need to bother losing one key, just make it full filled.
-                confirm_timer.reset()
+                continue
+            if self.appear_then_click(CAMPAIGN_MENU_GOTO_WAR_ARCHIVES, offset=(20, 20), interval=3):
+                # Sometimes quit to page_campaign_menu accidentally.
                 continue
 
             # End
-            if self.appear(WAR_ARCHIVES_CHECK, offset=(20, 20)):
-                if confirm_timer.reached():
-                    return True
-            else:
-                confirm_timer.reset()
+            if self.appear(WAR_ARCHIVES_CHECK, offset=(20, 20)) and self.appear(DATA_KEY_COLLECTED, offset=(20, 20)):
+                logger.info('Data key collect finished')
+                break
 
     def data_key_collect(self):
         """
         Execute data key collection
 
         Returns:
-            bool: If success to run.
+            bool: If execute a collection.
 
         Pages:
             in: page_any
-            out: page_main
+            out: page_archives
         """
         self.ui_ensure(page_archives)
 
-        for n in range(3):
-            self.device.screenshot()
-            if self.appear(DATA_KEY_COLLECTED):
-                logger.info('Data key has been collected')
-                self.ui_goto_main()
-                return True
+        if self.appear(DATA_KEY_COLLECTED, offset=(20, 20)):
+            logger.info('Data key has been collected')
+            return False
 
-            current, remain, total = DATA_KEY.ocr(self.device.image)
-            logger.info(f'Inventory: {current} / {total}, Remain: {remain}')
-            if remain <= 0:
-                logger.info('No more room for additional data key')
-                self.ui_goto_main()
-                return True
+        current, remain, total = DATA_KEY.ocr(self.device.image)
+        logger.info(f'Inventory: {current} / {total}, Remain: {remain}')
+        if remain <= 0:
+            logger.info('No more room for additional data key')
+            return False
 
-            self.device.click(DATA_KEY_COLLECT)
-            self._data_key_collect_confirm()
-            continue
-
-        logger.warning('Too many tries on data key collection, skip and try again on next task run')
-        self.ui_goto_main()
-        return False
+        self._data_key_collect()
+        return True
 
     def run(self):
         """
@@ -90,7 +74,5 @@ class RewardDataKey(UI):
             in: page_any
             out: page_main
         """
-        if not self.data_key_collect():
-            self.config.task_delay(success=False)
-        else:
-            self.config.task_delay(server_update=True)
+        self.data_key_collect()
+        self.config.task_delay(server_update=True)
