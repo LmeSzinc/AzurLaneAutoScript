d64e08baa7bab54310a6779aded6ea8dadb06391
Fernando Ayats <ayatsfer@gmail.com>
2021-11-28 12:56:30 +0100

    Improved Docker init script

--- dev_tools/Docker-run.sh
+++ dev_tools/Docker-run.sh
@@ -1,38 +1,54 @@
 #!/bin/env bash
 
 pprint() { # Pretty print
-    echo -e "==> \e[36m$1\e[39m"
+    echo -e "==> \e[36m${1}\e[39m"
 }
 prun() { # Pretty run
-    pprint "$ $1"
-    eval " $1" || exit 1
+    pprint "$ ${1}"
+    eval " ${1}" || exit 1
+}
+prun_or_continue() { # Pretty run or continue
+    pprint "$ ${1}"
+    eval " ${1}" || pprint "↳ Not needed!"
 }
 
-SOURCE="$(dirname $(realpath $BASH_SOURCE))" # Poiting to dev_tools folder
+SOURCE="$(dirname $(realpath ${BASH_SOURCE}))" # Poiting to dev_tools folder
 CONTAINER="azurlaneautoscript"
 
+# Sanity checks
+if [[ "$(id -u)" -eq 0 ]]; then
+    pprint "Don't run this as root!"
+    exit 1
+fi
+# Create a lockfile so only one instance of this script can run
+if [[ -f "${XDG_RUNTIME_DIR}/${CONTAINER}.lock" ]]; then
+    pprint "This script is already running!"
+    pprint "If this is not the case, please restart your computer!"
+    exit 1
+else
+    touch "${XDG_RUNTIME_DIR}/${CONTAINER}.lock"
+fi
+
+# ALAS update
 pprint "Updating this repo"
+prun "cd ${SOURCE}/.."
 prun "git fetch origin master"
 prun "git stash"
 prun "git pull --ff origin master"
-pprint "git stash pop"
-git stash pop || pprint "Not neeeded"
+prun_or_continue "git stash pop"
 
-pprint "Checking for existing config file"
-if [[ ! -f "$SOURCE/../config/alas.json" ]]; then
-    prun "cp -a \"$SOURCE/../config/template.json\" \"$SOURCE/../config/alas.json\""
-else
-    pprint "Config file OK"
-fi
-
+# Container cleanup
 pprint "Killing any previous container"
-prun "docker ps | grep $CONTAINER | awk '{print \$1}' | xargs -r -n1 docker kill"
+prun "docker ps | grep ${CONTAINER} | awk '{print \$1}' | xargs -r -n1 docker kill"
+pprint "Deleting old containers"
+prun "docker ps -a | grep ${CONTAINER} | awk '{print \$1}' | xargs -r -n1 docker rm"
 
-pprint "Deleting old containers..."
-prun "docker ps -a | grep $CONTAINER | awk '{print \$1}' | xargs -r -n1 docker rm"
+pprint "Build the image"
+prun "docker build -t ${CONTAINER} -f ${SOURCE}/Dockerfile ${SOURCE}/.."
 
-pprint "Build the container"
-prun "docker build -t $CONTAINER -f $SOURCE/Dockerfile $SOURCE/.."
+pprint "Killing any adb servers in the host machine"
+prun_or_continue "adb kill-server"
 
 pprint "Running the container"
-prun "docker run --net=host --volume=$SOURCE/../config/alas.json:/app/AzurLaneAutoScript/config/alas.json:rw --interactive --tty $CONTAINER"
+trap "rm ${XDG_RUNTIME_DIR}/${CONTAINER}.lock && docker kill ${CONTAINER}" EXIT
+prun "docker run --net=host --volume=${SOURCE}/..:/app/AzurLaneAutoScript:rw --interactive --tty --name ${CONTAINER} ${CONTAINER}"
