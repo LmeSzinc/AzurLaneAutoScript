9653735b678f904a5a311d80adc6defdec3e4802
LmeSzinc <37934724+LmeSzinc@users.noreply.github.com>
2021-12-05 15:42:27 +0800

    Opt: Better detection of GUILD_REPORT_AVAILABLE

--- module/guild/lobby.py
+++ module/guild/lobby.py
@@ -1,12 +1,34 @@
+import numpy as np
+
+from module.base.button import Button
 from module.base.timer import Timer
+from module.base.utils import color_similarity_2d, area_offset
 from module.combat.assets import GET_ITEMS_1, GET_ITEMS_2, GET_ITEMS_3
 from module.guild.assets import *
 from module.guild.base import GuildBase
 from module.logger import logger
+from module.map_detection.utils import Points
 from module.ui.assets import GUILD_CHECK
 
 
 class GuildLobby(GuildBase):
+    def guild_lobby_get_report(self):
+        """
+        Returns:
+            Button: Button to enter guild report.
+        """
+        # Find red color in the area of GUILD_REPORT_AVAILABLE
+        image = color_similarity_2d(self.image_area(GUILD_REPORT_AVAILABLE), color=(255, 8, 8))
+        points = np.array(np.where(image > 221)).T[:, ::-1]
+        if len(points):
+            # The center of red dot
+            points = Points(points).group(threshold=40) + GUILD_REPORT_AVAILABLE.area[:2]
+            # Shift to the center of report icon
+            area = area_offset((-51, -45, -13, 0), offset=points[0])
+            return Button(area=area, color=(255, 255, 255), button=area, name='GUILD_REPORT')
+        else:
+            return None
+
     def _guild_lobby_collect(self, skip_first_screenshot=True):
         """
         Performs collect actions if report rewards
@@ -21,15 +43,18 @@ class GuildLobby(GuildBase):
             out: ANY
         """
         confirm_timer = Timer(1.5, count=3).start()
+        click_timer = Timer(3)
         while 1:
             if skip_first_screenshot:
                 skip_first_screenshot = False
             else:
                 self.device.screenshot()
 
-            if self.appear_then_click(GUILD_REPORT_AVAILABLE, interval=3):
-                confirm_timer.reset()
-                continue
+            if click_timer.reached() and self.appear(GUILD_CHECK):
+                button = self.guild_lobby_get_report()
+                if button is not None:
+                    self.device.click(button)
+                    click_timer.reset()
 
             if self.appear_then_click(GUILD_REPORT_CLAIM, interval=3):
                 confirm_timer.reset()
