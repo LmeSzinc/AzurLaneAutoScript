3c483885e13d56145de267660973f5dc51068fce
LmeSzinc <37934724+LmeSzinc@users.noreply.github.com>
2021-12-24 00:21:36 +0800

    Revert "Fix: Add counter to retry timer for slower retries on slow PCs", increase retry interval instead
    This reverts commit e0bb6123

--- module/base/base.py
+++ module/base/base.py
@@ -50,9 +50,9 @@ class ModuleBase:
         if interval:
             if button.name in self.interval_timer:
                 if self.interval_timer[button.name].limit != interval:
-                    self.interval_timer[button.name] = Timer(interval, count=int(interval // 0.5))
+                    self.interval_timer[button.name] = Timer(interval)
             else:
-                self.interval_timer[button.name] = Timer(interval, count=int(interval // 0.5))
+                self.interval_timer[button.name] = Timer(interval)
             if not self.interval_timer[button.name].reached():
                 return False
 
