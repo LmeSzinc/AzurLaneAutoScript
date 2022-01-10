1100c2b3522222c064f55aa5c85d89543b8c1ed3
nEEtdo0d <chibi.mailynn@gmail.com>
2022-01-09 19:53:01 -0500

    Add: core and medal shop variant adapted to base update

---
+++ module/shop_beta/shop_core.py
@@ -0,0 +1,114 @@
+from module.base.button import ButtonGrid
+from module.base.decorator import cached_property
+from module.logger import logger
+from module.ocr.ocr import Digit
+from module.shop.assets import *
+from module.shop.base import ShopBase, ShopItemGrid
+
+OCR_SHOP_CORE = Digit(SHOP_CORE, letter=(239, 239, 239), name='OCR_SHOP_CORE')
+OCR_SHOP_AMOUNT = Digit(SHOP_AMOUNT, letter=(239, 239, 239), name='OCR_SHOP_AMOUNT')
+
+
+class CoreShop(ShopBase):
+    _shop_core = 0
+
+    @cached_property
+    def shop_items(self):
+        """
+        Returns:
+            ShopItemGrid:
+        """
+        shop_grid = self.shop_grid
+        shop_core_items = ShopItemGrid(shop_grid, templates={}, amount_area=(60, 74, 96, 95))
+        shop_core_items.load_template_folder('./assets/shop/core')
+        shop_core_items.load_cost_template_folder('./assets/shop/cost')
+        return shop_core_items
+
+    def shop_currency(self):
+        """
+        Ocr shop core currency
+
+        Returns
+            int: core amount
+        """
+        self._shop_core = OCR_SHOP_CORE.ocr(self.device.image)
+        logger.info(f'Core: {self._shop_core}')
+        return self._shop_core
+
+    def shop_check_item(self, item):
+        """
+        Args:
+            item: Item to check
+
+        Returns:
+            bool:
+        """
+        if item.price > self._shop_core:
+            return False
+        return True
+
+    def shop_buy_amount_execute(self, item):
+        """
+        Args:
+            item: Item to check
+
+        Returns:
+            bool: implicating failed to execute
+        """
+        index_offset = (40, 20)
+        limit = 0
+
+        # In case either -/+ shift position, use
+        # shipyard ocr trick to accurately parse
+        self.appear(AMOUNT_MINUS, offset=index_offset)
+        self.appear(AMOUNT_PLUS, offset=index_offset)
+        area = OCR_SHOP_AMOUNT.buttons[0]
+        OCR_SHOP_AMOUNT.buttons = [(AMOUNT_MINUS.button[2] + 3, area[1], AMOUNT_PLUS.button[0] - 3, area[3])]
+
+        # Total number that can be purchased
+        # altogether based on clicking max
+        # Needs small delay for stable image
+        self.appear_then_click(AMOUNT_MAX)
+        self.device.sleep((0.3, 0.5))
+        self.device.screenshot()
+        limit = OCR_SHOP_AMOUNT.ocr(self.device.image)
+        if not limit:
+            self.device.click(SHOP_CLICK_SAFE_AREA)  # Close amount prompt
+            return False
+
+        # Adjust purchase amount if needed
+        while 1:
+            if (limit * item.price) <= self._shop_core:
+                break
+            else:
+                limit -= 1
+
+        self.ui_ensure_index(limit, letter=OCR_SHOP_AMOUNT, prev_button=AMOUNT_MINUS, next_button=AMOUNT_PLUS,
+                             skip_first_screenshot=True)
+        self.device.click(SHOP_BUY_CONFIRM_AMOUNT)
+        return True
+
+    def shop_interval_clear(self):
+        """
+        Clear interval on select assets for
+        shop_core_buy_handle
+        """
+        self.interval_clear(SHOP_BUY_CONFIRM_AMOUNT)
+
+    def shop_buy_handle(self, item):
+        """
+        Handle shop_core buy interface if detected
+
+        Args:
+            item: Item to handle
+
+        Returns:
+            bool: whether interface was detected and handled
+        """
+        if self.appear(SHOP_BUY_CONFIRM_AMOUNT, offset=(20, 20), interval=3):
+            if not self.shop_buy_amount_execute(item):
+                logger.warning('Failed to purchase amount item')
+            self.interval_reset(SHOP_BUY_CONFIRM_AMOUNT)
+            return True
+
+        return False
---
+++ module/shop_beta/shop_medal.py
@@ -0,0 +1,93 @@
+from module.base.button import ButtonGrid
+from module.base.decorator import Config
+from module.base.decorator import cached_property
+from module.logger import logger
+from module.ocr.ocr import Digit
+from module.shop.assets import *
+from module.shop.base import ShopBase, ShopItemGrid
+
+OCR_SHOP_MEDAL = Digit(SHOP_MEDAL, letter=(239, 239, 239), name='OCR_SHOP_MEDAL')
+
+
+class MedalShop(ShopBase):
+    _shop_medal = 0
+
+    @cached_property
+    def shop_grid(self):
+        """
+        Returns:
+            ButtonGrid:
+        """
+        shop_grid = ButtonGrid(
+            origin=(197, 193), delta=(223, 190), button_shape=(100.5, 101.5), grid_shape=(3, 2), name='SHOP_GRID')
+        return shop_grid
+
+    @cached_property
+    @Config.when(SERVER='jp')
+    def shop_items(self):
+        """
+        Returns:
+            ShopItemGrid:
+        """
+        # JP has thinner letters, increase threshold to 128
+        price_ocr = Digit([], letter=(255, 223, 57), threshold=128, name='Price_ocr')
+        shop_grid = self.shop_grid
+        shop_medal_items = ShopItemGrid(
+            shop_grid, templates={}, amount_area=(60, 74, 96, 95), price_area=(52, 135, 132, 162))
+        shop_medal_items.load_template_folder('./assets/shop/medal')
+        shop_medal_items.load_cost_template_folder('./assets/shop/cost')
+        shop_medal_items.similarity = 0.88  # Lower the threshold for consistent matches of PR/DRBP
+        shop_medal_items.price_ocr = price_ocr
+        return shop_medal_items
+
+    @cached_property
+    @Config.when(SERVER=None)
+    def shop_items(self):
+        """
+        Returns:
+            ShopItemGrid:
+        """
+        shop_grid = self.shop_grid
+        shop_medal_items = ShopItemGrid(
+            shop_grid, templates={}, amount_area=(60, 74, 96, 95), price_area=(52, 134, 132, 162))
+        shop_medal_items.load_template_folder('./assets/shop/medal')
+        shop_medal_items.load_cost_template_folder('./assets/shop/cost')
+        shop_medal_items.similarity = 0.88  # Lower the threshold for consistent matches of PR/DRBP
+        return shop_medal_items
+
+    def shop_currency(self):
+        """
+        Ocr shop medal currency
+
+        Returns:
+            int: medal amount
+        """
+        self._shop_medal = OCR_SHOP_MEDAL.ocr(self.device.image)
+        logger.info(f'Medal: {self._shop_medal}')
+        return self._shop_medal
+
+    def shop_has_loaded(self, items):
+        """
+        If any item parsed with a default
+        price of 5000; then shop cannot
+        be safely bought from yet
+
+        Returns:
+            bool
+        """
+        for item in items:
+            if int(item.price) == 5000:
+                return False
+        return True
+
+    def shop_check_item(self, item):
+        """
+        Args:
+            item: Item to check
+
+        Returns:
+            bool:
+        """
+        if item.price > self._shop_medal:
+            return False
+        return True
