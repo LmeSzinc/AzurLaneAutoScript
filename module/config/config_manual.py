58fa9b5b789a055698291b418c2dab90534e4dc9
LmeSzinc <37934724+LmeSzinc@users.noreply.github.com>
2022-01-25 01:02:35 +0800

    Opt: Prioritize Exercise in SCHEDULER_PRIORITY

--- module/config/config_manual.py
+++ module/config/config_manual.py
@@ -8,14 +8,15 @@ class ManualConfig:
 
     SCHEDULER_PRIORITY = """
     Restart
-    > Research > Commission > Tactical > Dorm > Meowfficer > Guild
-    > Gacha > SupplyPack
+    > Research > Commission > Tactical
+    > Exercise
+    > Dorm > Meowfficer > Guild > Gacha > SupplyPack
     > Reward > BattlePass
     > ShopFrequent > ShopOnce > Shipyard > DataKey
     > OpsiExplore
     > OpsiDaily > OpsiShop
     > OpsiAbyssal > OpsiStronghold > OpsiObscure
-    > Exercise > Daily > Hard > OpsiAshAssist
+    > Daily > Hard > OpsiAshAssist
     > Sos > EventSp > EventAb > EventCd > RaidDaily > WarArchives > MaritimeEscort
     > OpsiMeowfficerFarming
     > Event > Raid > Main
@@ -62,6 +63,7 @@ class ManualConfig:
     ASCREENCAP_FILEPATH_LOCAL = './bin/ascreencap'
     ASCREENCAP_FILEPATH_REMOTE = '/data/local/tmp/ascreencap'
     MINITOUCH_FILEPATH_REMOTE = '/data/local/tmp/minitouch'
+    HERMIT_FILEPATH_LOCAL = './bin/hermit/Hermit.apk'
 
     """
     module.campaign.gems_farming
