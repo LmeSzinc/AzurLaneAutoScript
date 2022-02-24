ab3c8531f47922d428563237e832aea9ba9c168b
18870 <a20110123@163.com>
2022-02-24 22:09:59 +0800

    Opt: Reformat codes using `Black`
    goodbye autopep8

--- module/webui/updater.py
+++ module/webui/updater.py
@@ -25,7 +25,7 @@ class Config(DeployConfig):
         self.config = {}
         self.read()
         self.write()
-    
+
     def execute(self, command, allow_failure=False):
         """
         Args:
@@ -36,19 +36,19 @@ class Config(DeployConfig):
             bool: If success.
                 Terminate installation if failed to execute and not allow_failure.
         """
-        command = command.replace(r'\\', '/').replace('\\', '/').replace('\"', '"')
+        command = command.replace(r"\\", "/").replace("\\", "/").replace('"', '"')
         print(command)
         error_code = os.system(command)
         if error_code:
             if allow_failure:
-                print(f'[ allowed failure ], error_code: {error_code}')
+                print(f"[ allowed failure ], error_code: {error_code}")
                 return False
             else:
-                print(f'[ failure ], error_code: {error_code}')
+                print(f"[ failure ], error_code: {error_code}")
                 # self.show_error()
                 raise ExecutionError
         else:
-            print(f'[ success ]')
+            print(f"[ success ]")
             return True
 
 
@@ -61,47 +61,48 @@ class Updater(Config, GitManager, PipManager):
     @property
     def delay(self):
         self.read()
-        return int(self.config['CheckUpdateInterval'])*60
+        return int(self.config["CheckUpdateInterval"]) * 60
 
     @property
     def schedule_time(self):
         self.read()
-        t = self.config['AutoRestartTime']
-        if t != '':
+        t = self.config["AutoRestartTime"]
+        if t != "":
             return datetime.time.fromisoformat(t)
         else:
             return None
 
     @cached_property
     def repo(self):
-        return self.config['Repository']
+        return self.config["Repository"]
 
     @cached_property
     def branch(self):
-        return self.config['Branch']
+        return self.config["Branch"]
 
     def execute_output(self, command) -> str:
-        command = command.replace(
-            r'\\', '/').replace('\\', '/').replace('\"', '"')
-        log = subprocess.run(command, capture_output=True,
-                             text=True, encoding='utf8', shell=True).stdout
+        command = command.replace(r"\\", "/").replace("\\", "/").replace('"', '"')
+        log = subprocess.run(
+            command, capture_output=True, text=True, encoding="utf8", shell=True
+        ).stdout
         return log
 
-    def get_commit(self, revision='', n=1, short_sha1=False) -> Tuple:
+    def get_commit(self, revision="", n=1, short_sha1=False) -> Tuple:
         """
         Return:
             (sha1, author, isotime, message,)
         """
-        ph = 'h' if short_sha1 else 'H'
+        ph = "h" if short_sha1 else "H"
 
         log = self.execute_output(
-            f'"{self.git}" log {revision} --pretty=format:"%{ph}---%an---%ad---%s" --date=iso -{n}')
+            f'"{self.git}" log {revision} --pretty=format:"%{ph}---%an---%ad---%s" --date=iso -{n}'
+        )
 
         if not log:
             return None, None, None, None
 
-        logs = log.split('\n')
-        logs = list(map(lambda log: tuple(log.split('---')), logs))
+        logs = log.split("\n")
+        logs = list(map(lambda log: tuple(log.split("---")), logs))
 
         if n == 1:
             return logs[0]
@@ -109,23 +110,27 @@ class Updater(Config, GitManager, PipManager):
             return logs
 
     def _check_update(self) -> bool:
-        self.state = 'checking'
-        source = 'origin'
+        self.state = "checking"
+        source = "origin"
         for _ in range(3):
-            if self.execute(f'"{self.git}" fetch {source} {self.branch}', allow_failure=True):
+            if self.execute(
+                f'"{self.git}" fetch {source} {self.branch}', allow_failure=True
+            ):
                 break
         else:
             logger.warning("Git fetch failed")
             return False
 
         log = self.execute_output(
-            f'"{self.git}" log --not --remotes={source}/* -1 --oneline')
+            f'"{self.git}" log --not --remotes={source}/* -1 --oneline'
+        )
         if log:
             logger.info(
-                f"Cannot find local commit {log.split()[0]} in upstream, skip update")
+                f"Cannot find local commit {log.split()[0]} in upstream, skip update"
+            )
             return False
 
-        sha1, _, _, message = self.get_commit(f'..{source}/{self.branch}')
+        sha1, _, _, message = self.get_commit(f"..{source}/{self.branch}")
 
         if sha1:
             logger.info(f"New update avaliable")
@@ -139,40 +144,40 @@ class Updater(Config, GitManager, PipManager):
         """
         Deprecated
         """
-        self.state = 'checking'
-        r = self.repo.split('/')
+        self.state = "checking"
+        r = self.repo.split("/")
         owner = r[3]
         repo = r[4]
-        if 'gitee' in r[2]:
+        if "gitee" in r[2]:
             base = "https://gitee.com/api/v5/repos/"
             headers = {}
-            token = self.config['ApiToken']
+            token = self.config["ApiToken"]
             if token:
                 para = {"access_token": token}
         else:
             base = "https://api.github.com/repos/"
-            headers = {
-                'Accept': 'application/vnd.github.v3.sha'
-            }
+            headers = {"Accept": "application/vnd.github.v3.sha"}
             para = {}
-            token = self.config['ApiToken']
+            token = self.config["ApiToken"]
             if token:
-                headers['Authorization'] = 'token ' + token
+                headers["Authorization"] = "token " + token
 
         try:
             list_commit = requests.get(
-                base + f"{owner}/{repo}/branches/{self.branch}", headers=headers, params=para)
+                base + f"{owner}/{repo}/branches/{self.branch}",
+                headers=headers,
+                params=para,
+            )
         except Exception as e:
             logger.exception(e)
             logger.warning("Check update failed")
             return 0
 
         if list_commit.status_code != 200:
-            logger.warning(
-                f"Check update failed, code {list_commit.status_code}")
+            logger.warning(f"Check update failed, code {list_commit.status_code}")
             return 0
         try:
-            sha = list_commit.json()['commit']['sha']
+            sha = list_commit.json()["commit"]["sha"]
         except Exception as e:
             logger.exception(e)
             logger.warning("Check update failed when parsing return json")
@@ -186,7 +191,10 @@ class Updater(Config, GitManager, PipManager):
 
         try:
             get_commit = requests.get(
-                base + f"{owner}/{repo}/commits/" + local_sha, headers=headers, params=para)
+                base + f"{owner}/{repo}/commits/" + local_sha,
+                headers=headers,
+                params=para,
+            )
         except Exception as e:
             logger.exception(e)
             logger.warning("Check update failed")
@@ -195,14 +203,15 @@ class Updater(Config, GitManager, PipManager):
         if get_commit.status_code != 200:
             # for develops
             logger.info(
-                f"Cannot find local commit {local_sha[:8]} in upstream, skip update")
+                f"Cannot find local commit {local_sha[:8]} in upstream, skip update"
+            )
             return 0
 
         logger.info(f"Update {sha[:8]} avaliable")
         return 1
 
     def check_update(self):
-        if self.state in (0, 'failed', 'finish'):
+        if self.state in (0, "failed", "finish"):
             self.state = self._check_update()
 
     @retry(ExecutionError, tries=3, delay=5, logger=None)
@@ -226,24 +235,24 @@ class Updater(Config, GitManager, PipManager):
         return True
 
     def run_update(self):
-        if self.state not in ('failed', 0, 1):
+        if self.state not in ("failed", 0, 1):
             return
         self._start_update()
 
     def _start_update(self):
-        self.state = 'start'
+        self.state = "start"
         instances = ProcessManager.running_instances()
         names = []
         for alas in instances:
-            names.append(alas.config_name + '\n')
+            names.append(alas.config_name + "\n")
 
         logger.info("Waiting all running alas finish.")
         self._wait_update(instances, names)
 
     def _wait_update(self, instances, names):
-        if self.state == 'cancel':
+        if self.state == "cancel":
             self.state = 1
-        self.state = 'wait'
+        self.state = "wait"
         self.event.set()
         _instances = instances.copy()
         while _instances:
@@ -251,9 +260,8 @@ class Updater(Config, GitManager, PipManager):
                 if not alas.alive:
                     _instances.remove(alas)
                     logger.info(f"Alas [{alas.config_name}] stopped")
-                    logger.info(
-                        f"Remains: {[alas.config_name for alas in _instances]}")
-            if self.state == 'cancel':
+                    logger.info(f"Remains: {[alas.config_name for alas in _instances]}")
+            if self.state == "cancel":
                 self.state = 1
                 self.event.clear()
                 ProcessManager.restart_processes(instances, self.event)
@@ -262,21 +270,22 @@ class Updater(Config, GitManager, PipManager):
         self._run_update(instances, names)
 
     def _run_update(self, instances, names):
-        self.state = 'run update'
+        self.state = "run update"
         logger.info("All alas stopped, start updating")
 
         if self.update():
             if Setting.reload:
-                self.state = 'reload'
-                with open('./config/reloadalas', mode='w') as f:
+                self.state = "reload"
+                with open("./config/reloadalas", mode="w") as f:
                     f.writelines(names)
                 from module.webui.app import clearup
+
                 self._trigger_reload(2)
                 clearup()
             else:
-                self.state = 'finish'
+                self.state = "finish"
         else:
-            self.state = 'failed'
+            self.state = "failed"
             logger.warning("Update failed")
             self.event.clear()
             ProcessManager.restart_processes(instances, self.event)
@@ -285,9 +294,10 @@ class Updater(Config, GitManager, PipManager):
     @staticmethod
     def _trigger_reload(delay=2):
         def trigger():
-            with open('./config/reloadflag', mode='w'):
+            with open("./config/reloadflag", mode="w"):
                 # app ended here and uvicorn will restart whole app
                 pass
+
         timer = threading.Timer(delay, trigger)
         timer.start()
 
@@ -309,17 +319,17 @@ class Updater(Config, GitManager, PipManager):
                 yield
                 continue
             if not self.run_update():
-                self.state = 'failed'
+                self.state = "failed"
             th._task.delay = get_next_time(self.schedule_time)
             yield
 
     def cancel(self):
-        self.state = 'cancel'
+        self.state = "cancel"
 
 
 updater = Updater()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     pass
     # if updater.check_update():
     updater.update()
