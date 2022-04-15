d63935f514c3d0eabd2b30e1714e01c77780b548
Egor Nats <kyo@kyo.ninja>
2022-04-14 22:44:01 -0300

    Fix: fixed missing imports

--- campaign/campaign_hard/campaign_hard.py
+++ campaign/campaign_hard/campaign_hard.py
@@ -4,7 +4,7 @@ from module.exception import CampaignEnd
 from module.hard.equipment import HardEquipment
 from module.logger import logger
 from module.map.assets import FLEET_PREPARATION, MAP_PREPARATION
-from module.ui.ui import CAMPAIGN_CHECK
+from module.ui.assets import CAMPAIGN_CHECK
 
 
 class Config:
