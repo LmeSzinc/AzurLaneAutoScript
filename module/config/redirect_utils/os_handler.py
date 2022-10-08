c363534c5ab0bd11eb87f7761adf5ef664935953
Bookbug <willyhappyya@gmail.com>
2022-10-08 23:29:23 +0800

    Fix:replace str with bool

--- module/config/redirect_utils/os_handler.py
+++ module/config/redirect_utils/os_handler.py
@@ -2,9 +2,7 @@ def action_point_redirect(value):
     """
     redirect attr about action point.
     """
-    if value == 'false':
-        return '0'
-    elif value == 'true':
+    if value:
         return '5'
     else:
         return '0'
