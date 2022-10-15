e74922ed502aba386c833231b3e8c9009b6a9ba8
sevenstar <sevenstar@gmail.com>
2022-10-15 12:22:10 +0800

    add wararchive: Crescendo of Polaris 新增作战档案：激奏的Polaris

--- module/war_archives/dictionary.py
+++ module/war_archives/dictionary.py
@@ -12,6 +12,7 @@ dic_archives_template = {
     'war_archives_20180726_cn': TEMPLATE_IRIS_OF_LIGHT_AND_DARK,
     'war_archives_20181227_cn': TEMPLATE_CRIMSON_ECHOES,
     'war_archives_20200820_cn': TEMPLATE_SCHERZO_OF_IRON_AND_BLOOD,
+    'war_archives_20211014_cn': TEMPLATE_CRESCENDO_OF_POLARIS,
     'war_archives_20190911_cn': TEMPLATE_EMPYREAL_TRAGICOMEDY,
     'war_archives_20210325_cn': TEMPLATE_ASHEN_SIMULACRUM,
     'war_archives_20210624_cn': TEMPLATE_SWIRLING_CHERRY_BLOSSOMS,
