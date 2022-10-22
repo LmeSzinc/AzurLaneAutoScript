371c7e356db7cf15cae89cdea5c7512dc5944c72
SarContDeli <48789988+SaiCateDoan@users.noreply.github.com>
2022-10-22 11:22:14 +0800

    Opt: optimized general filter

--- module/commission/preset.py
+++ module/commission/preset.py
@@ -6,17 +6,20 @@ SHORTEST_FILTER = """
 > 4 > 5 > 5:20
 > 6 > 7 > 8 > 9 > 10 > 12
 """
+
 DICT_FILTER_PRESET = {
-    'general': """DailyEvent
-    > Gem-8 > Gem-4 > Gem-2
-    > NightDrill-8 > NightDrill-7 > NightDrill-6
-    > ExtraDrill-0:20 > ExtraDrill-1 > ExtraDrill-2 > ExtraDrill-2:40 > ExtraDrill-3:20 > ExtraDrill-5:20
-    > Box-6 > Box-3 > Box-1
-    > DailyCube-0:30 > UrgentCube-1:30 > DailyCube-1:30 > UrgentCube-1:45 > UrgentCube-2:15 > UrgentCube-3
+    'chip': """
+    DailyEvent
+    > Gem-8 > Gem-4 > Gem-2 > ExtraCube-0:30 
+    > NightDrill-6 > ExtraDrill-2 > NightDrill-7 > NightDrill-8 
+    > ExtraDrill-3:20 > ExtraDrill-5:20 > ExtraDrill-0:20 > ExtraDrill-1 > ExtraDrill-2:40 
+    > UrgentCube-1:30 > UrgentCube-1:45 > UrgentCube-3 
+    > UrgentBox-6 > UrgentBox-3 > UrgentBox-1
+    > ExtraCube-1:30 > UrgentCube-2:15
     > Major
     > DailyChip > DailyResource
-    > UrgentBook-2:30 > UrgentBook-2 > UrgentBook-1:20 > UrgentBook-1:40
-    > Daily-0:20 > Daily-0:30 > Daily-1:00 > Daily-1:30 > Daily-2:00
+    > Extra-0:20 > Extra-0:30 > Extra-1:00 > Extra-1:30 > Extra-2:00
     > NightOil > NightCube
-    > shortest"""
+    > shortest
+    """
 }
