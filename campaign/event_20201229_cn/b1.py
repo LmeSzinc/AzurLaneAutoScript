8e8298477543af17cf3dfcbb3f2737280282197d
LmeSzinc <37934724+LmeSzinc@users.noreply.github.com>
2022-11-11 01:47:30 +0800

    Opt: Perspective parameters Ininverted Orthant

--- campaign/event_20201229_cn/b1.py
+++ campaign/event_20201229_cn/b1.py
@@ -64,6 +64,19 @@ class Config:
     MAP_HAS_AMBUSH = False
     # ===== End of generated config =====
 
+    INTERNAL_LINES_FIND_PEAKS_PARAMETERS = {
+        'height': (150, 255 - 17),
+        'width': (0.9, 10),
+        'prominence': 10,
+        'distance': 35,
+    }
+    EDGE_LINES_FIND_PEAKS_PARAMETERS = {
+        'height': (255 - 17, 255),
+        'prominence': 10,
+        'distance': 50,
+        'wlen': 1000
+    }
+    HOMO_EDGE_COLOR_RANGE = (0, 17)
     MAP_SWIPE_MULTIPLY = 1.561
     MAP_SWIPE_MULTIPLY_MINITOUCH = 1.509
     MAP_ENEMY_GENRE_DETECTION_SCALING = {
