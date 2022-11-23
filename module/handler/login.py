6b2d4877d0949a29eabe86c6d3656100ce670908
SarContDeli <48789988+SaiCateDoan@users.noreply.github.com>
2022-11-23 18:55:22 +0800

    Fix: Remove restart emulator at 0 am

--- module/handler/login.py
+++ module/handler/login.py
@@ -153,12 +153,7 @@ class LoginHandler(UI):
 
     def app_restart(self):
         logger.hr('App restart')
-        if self.config.RestartEmulator_Enable \
-                and self.config.Scheduler_NextRun.strftime('%H:%M:%S') \
-                == get_server_next_update(self.config.Scheduler_ServerUpdate).strftime('%H:%M:%S'):
-            self.device.emulator_restart()
-        else:
-            self.device.app_stop()
+        self.device.app_stop()
         self.device.app_start()
         self.handle_app_login()
         # self.ensure_no_unfinished_campaign()
