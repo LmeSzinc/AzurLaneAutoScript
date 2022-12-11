b1881cc5b7f5c9bbbd74641ad400a7e460835eae
LmeSzinc <37934724+LmeSzinc@users.noreply.github.com>
2022-12-11 23:44:34 +0800

    Add: [CN][TW] GUI translation for OpSi Archive Zones

--- module/config/config_generated.py
+++ module/config/config_generated.py
@@ -380,9 +380,6 @@ class GeneratedConfig:
     # Group `OpsiAbyssal`
     OpsiAbyssal_ForceRun = False
 
-    # Group `OpsiArchive`
-    OpsiArchive_Unused = False
-
     # Group `OpsiStronghold`
     OpsiStronghold_ForceRun = False
 
