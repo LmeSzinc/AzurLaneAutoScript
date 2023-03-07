ae76d8f9925e0c764a8c8db7f11f9357f28df3be
Horizon101011 <43370844+Horizon101011@users.noreply.github.com>
2023-03-07 17:14:29 +0800

    Add: Finish DockScanner preliminarily
    Will add to Tools soon for more tests

--- module/retire/scanner.py
+++ module/retire/scanner.py
@@ -1,5 +1,5 @@
 from abc import ABCMeta, abstractmethod
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from typing import Any, Dict, List, Tuple, Union
 
 import cv2
@@ -7,7 +7,9 @@ import numpy as np
 
 import module.config.server as server
 from module.base.button import ButtonGrid
-from module.base.utils import color_similar, crop, extract_letters, get_color, limit_in, save_image
+from module.base.utils import (color_similar, crop, extract_letters, get_color, limit_in,
+                               random_normal_distribution_int,
+                               random_rectangle_point)
 from module.combat.level import LevelOcr
 from module.logger import logger
 from module.ocr.ocr import Digit
@@ -17,7 +19,8 @@ from module.retire.assets import (TEMPLATE_FLEET_1, TEMPLATE_FLEET_2,
                                   TEMPLATE_IN_BATTLE, TEMPLATE_IN_COMMISSION, TEMPLATE_IN_HARD,
                                   TEMPLATE_IN_EVENT_FLEET)
 from module.retire.dock import (CARD_EMOTION_GRIDS, CARD_GRIDS,
-                                CARD_LEVEL_GRIDS, CARD_RARITY_GRIDS)
+                                CARD_LEVEL_GRIDS, CARD_RARITY_GRIDS,
+                                DOCK_SCROLL)
 
 
 class EmotionDigit(Digit):
@@ -55,6 +58,7 @@ class Ship:
     fleet: int = 0
     status: str = ''
     button: Any = None
+    hash_: str = field(default='', repr=False)
 
     def satisfy_limitation(self, limitation) -> bool:
         for key in self.__dict__:
@@ -76,6 +80,40 @@ class Ship:
                         return False
 
         return True
+
+
+class DHash:
+    EQ_THRES: int = 30
+
+    def __init__(self, image, size=8) -> None:
+        self.code = DHash.gen_hash(image, size)
+
+    @staticmethod
+    def gen_hash(image, size=8) -> str:
+        if len(image.shape) > 2:
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+        image = cv2.resize(image, (size + 1, size + 1))
+        row_diff = np.packbits(image[:-1, :-1] > image[1:, :-1])
+        col_diff = np.packbits(image[:-1, :-1] > image[:-1, 1:])
+        row_hash: str = ''.join([f'{i:>02x}' for i in row_diff])
+        col_hash: str = ''.join([f'{i:>02x}' for i in col_diff])
+
+        return f'{row_hash}{col_hash}'
+
+    @staticmethod
+    def distance(__x, __y) -> int:
+        if isinstance(__x, DHash) and isinstance(__y, DHash):
+            __x, __y = int(__x.code, 16), int(__y.code, 16)
+        elif isinstance(__x, str) and isinstance(__y, str):
+            __x, __y = int(__x, 16), int(__y, 16)
+
+        return bin(__x ^ __y).count('1')
+
+    def __eq__(self, __o: object) -> bool:
+        return type(self) == type(__o) and DHash.distance(self, __o) < DHash.EQ_THRES
+
+    def __repr__(self) -> str:
+        return self.code
 
 
 class Scanner(metaclass=ABCMeta):
@@ -147,6 +185,10 @@ class LevelScanner(Scanner):
 
     def limit_value(self, value) -> int:
         return limit_in(value, 1, 125)
+    
+    def move(self, vector) -> None:
+        super().move(vector)
+        self.ocr_model.buttons = [button.area for button in self.grids.buttons]
 
 
 class EmotionScanner(Scanner):
@@ -168,6 +210,10 @@ class EmotionScanner(Scanner):
 
     def limit_value(self, value) -> int:
         return limit_in(value, 0, 150)
+
+    def move(self, vector) -> None:
+        super().move(vector)
+        self.ocr_model.buttons = [button.area for button in self.grids.buttons]
 
 
 class RarityScanner(Scanner):
@@ -288,7 +334,7 @@ class StatusScanner(Scanner):
 
     def _match(self, image) -> str:
         for template, status in self.templates.items():
-            if template.match(image, similarity=0.75):
+            if template.match(image, similarity=0.8):
                 return status
 
         return 'free'
@@ -302,17 +348,28 @@ class StatusScanner(Scanner):
         return value if value in self.value_list else 'any'
 
 
+class HashGenerator(Scanner):
+    def __init__(self, length=8) -> None:
+        super().__init__()
+        self._results = []
+        self.length = length
+        self.grids = CARD_GRIDS
+
+    def _scan(self, image) -> List:
+        image_list = [crop(image, button.area) for button in self.grids.buttons]
+
+        return [DHash(image, self.length) for image in image_list]
+
+    def limit_value(self, value) -> Any:
+        pass
+
+
 class ShipScanner(Scanner):
     """
     Ship Scanner is designed to use with an "Initial" page_dock, which means there cannot be
     any move once a dock filter was set. Otherwise, it may return untrustable results.
 
     If you need to scan rather more than the initial page, Please use DockScanner.
-
-    By default, all properties of the ship are scanned.
-    You can set the required properties by calling enable() or disable().
-    disable() will simply skip scanning and set those properties to None.
-    To keep them and ignore limitations, use set_limitation(property=None)
 
     Args:
         rarity (str, list): ['any', 'common', 'rare', 'elite', 'super_rare'].
@@ -326,6 +383,25 @@ class ShipScanner(Scanner):
             'in_hard_fleet',
             'in_event_fleet',
             ]
+
+    By default, all properties of the ship are scanned.
+    "False" and "None" are two special values for the properties of ship.
+
+    Using False:
+        The scanner will skip scanning by simply setting them to None.
+        Doing so will discard the property, assuming you don't care about them.
+        Once a property is set to False, it can only be enabled by calling enable().
+        And calling disable() has the same effect as using False.
+
+    Using None:
+        The scanner will work normally, but ignoring the limitations on them
+        when filter the results. set_limitation(property=...) can reset the limitaiions,
+        including set them to None.
+
+    Examples:
+        ShipScanner(rarity=False) will get a list of ships whose rarity is None.
+        This can be used in a situation where rarity is not a concern.
+        Then calling ShipScanner.enable('rairty').
     """
     def __init__(
         self,
@@ -353,6 +429,7 @@ class ShipScanner(Scanner):
             'rarity': RarityScanner(),
             'fleet': FleetScanner(),
             'status': StatusScanner(),
+            'hash': HashGenerator(),
         }
 
         self.set_limitation(
@@ -369,15 +446,17 @@ class ShipScanner(Scanner):
                 rarity=rarity,
                 fleet=fleet,
                 status=status,
-                button=button)
-            for level, emotion, rarity, fleet, status, button in
+                button=button,
+                hash_=hash_)
+            for level, emotion, rarity, fleet, status, button, hash_ in
             zip(
                 self.sub_scanners['level'].results,
                 self.sub_scanners['emotion'].results,
                 self.sub_scanners['rarity'].results,
                 self.sub_scanners['fleet'].results,
                 self.sub_scanners['status'].results,
-                self.grids.buttons)
+                self.grids.buttons,
+                self.sub_scanners['hash'].results)
         ]
 
         for scanner in self.sub_scanners.values():
@@ -452,6 +531,8 @@ class ShipScanner(Scanner):
         for attr in self.limitation.keys():
             value = kwargs.get(attr, self.limitation[attr])
             self.limit_value(key=attr, value=value)
+            if value is False:
+                self.sub_scanners[attr].disable()
 
         logger.info(f'Limitations set to {self.limitation}')
 
@@ -463,13 +544,148 @@ class DockScanner(ShipScanner):
     Same as ShipScanner, DockScanner must start at the initial page_dock.
     The scanning process can swipe the dock automatically and stop when finished.
     """
-    def __init__(self, rarity: str = 'any', level: Tuple[int, int] = (1, 125), emotion: Tuple[int, int] = (0, 150), fleet: int = 0, status: str = 'any') -> None:
-        raise NotImplementedError
-        super().__init__(rarity, level, emotion, fleet, status)
-        self.scan_zone = (93, 76, 1218, 719)
-        self.card_bottom = []
+    SCAN_ZONES: Dict[str, Tuple[int, int, int, int]] = {
+        'dock': (93, 55, 1219, 719),
+    }
+    def __init__(self, zone: str = 'dock') -> None:
+        self._results = []
+        self.scan_zone: Tuple[int, int, int, int] = self.SCAN_ZONES[zone]
+        self.zone_top: int = self.scan_zone[1]
+        self.zone_height: int = self.scan_zone[3] - self.scan_zone[1]
+        self.grids_top: int = 76
+        # For reposition and moving
+        self.mean_color = None
+        self.moving_distance: int = 0
+        self.bound = []
+        # For status
+        self._stable: bool = False
+        self._no_change: int = 0
+        self.last_results = []
+        self.retry: int = 0
 
-    def multi_scan(self, image):
+        self.scanner = ShipScanner(emotion=False, fleet=False, status=False)
+
+    def limit_value(self, value) -> Any:
+        pass
+
+    @property
+    def stable(self) -> bool:
+        if self._stable:
+            self._stable = False
+            return True
+        else:
+            return False
+
+    def no_change(self) -> bool:
+        return self._no_change > 3
+
+    def _find_bound(self, image) -> List[int]:
+        """
+        Roughly Adjust.
+
+        The standard deviation of blank line will show obvious troughs.
+        The position of the blank line can be obtained by locating the position
+        of the wave troughs. Although it is not accurate, we only need its center.
+        """
+        image = crop(image, self.scan_zone)
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+        std = np.std(image, axis=1)
+        move_avg = np.convolve(std, np.ones((5, )) / 5, mode='valid')
+        gap_seq = list(np.nonzero(move_avg < 20)[0]) + [1000]
+
+        bound = []
+        start = 0
+        for i in range(len(gap_seq) - 1):
+            if gap_seq[i + 1] - gap_seq[i] > 50 and i + 1 - start > 10:
+                bound.append(np.mean(gap_seq[start : i + 1]).astype(np.int))
+                start = i + 1
+        if len(bound) > 1:
+            # The last line is not credible
+            bound[-1] = min(bound[-2] + 225, bound[-1])
+
+        return bound
+
+    def reset_position(self) -> None:
+        offset = 76 - self.grids_top
+        self.grids_top += offset
+        self.scanner.move((0, offset))
+
+    def reposition(self, image, bound) -> None:
+        """
+        Preciously Adjust.
+
+        *bound* has given the centers of those blank line.
+        Go down from these central points, the first line whose color has a large
+        difference from mean_color is the top of the new CARD_GRIDS.
+        """
+        scan_image = crop(image, self.scan_zone)
+        if self.mean_color is not None:
+            for y in range(0, 20):
+                if not color_similar(np.mean(scan_image[bound[0] + y], axis=0), self.mean_color, 60):
+                    break
+            offset = y + self.zone_top + bound[0] + 1 - self.grids_top
+            self.grids_top += offset
+            self.scanner.move((0, offset))
+
+        self.mean_color = np.mean(scan_image[bound[-1]], axis=0)
+
+    def _remove_duplicate(self, results) -> int:
+        """
+        There are two kinds of duplicate in results.
+            Two lines:
+                The new result is exactly the same as the last one.
+            One lines.
+                The first line of new result is ths same as the last line of the old one.
+        In both cases, len(results) < 14 means reaching the bottom.
+        """
+        if self._results:
+            if any([old.hash_ == new.hash_ for new, old in zip(results, self._results[-len(results):])]):
+                self._no_change += 999 if len(results) < 14 else 1
+                return 0
+            elif any([old.hash_ == new.hash_ for new, old in zip(results[:7], self._results[-7:])]):
+                self._results.extend(results[7-len(results):])
+                self._no_change = 999 if len(results) < 14 else 0
+                return len(results)-7
+
+        self._no_change = 0
+        self._results.extend(results)
+        return 14
+
+    def _scan(self, image) -> None:
+        bound = self._find_bound(image)
+        if len(bound) == 1:
+            # No ship appears
+            self._stable = True
+            return 0
+
+        self.moving_distance = bound[-1] - (self.zone_height - 204 * 2 - 23 * 3) / 2 * 1.5
+        self.reposition(image, bound)
+        results = self.scanner.scan(image, cached=False, output=False)
+        if not results:
+            self.retry += 1
+            logger.info(f'No ship was detected, reset the position. Retry {self.retry} time(s)')
+            self.reset_position()
+            self.reposition(image, bound)
+            results = self.scanner.scan(image, cached=False, output=False)
+            if self.retry > 3:
+                self.moving_distance = random_normal_distribution_int(10, 20)
+                self.retry = 0
+        else:
+            self.retry = 0
+
+        if any([old.hash_ == new.hash_ for new, old in zip(results, self.last_results)]):
+        # if sum([old.hash_ == new.hash_ for new, old in zip(results, self.last_results)]) / len(results) >= 0.80:
+        # In the early test, the probability of hash conflict between two different ships is less than 0.002
+        # It can be boldly concluded that if any same position has a same hash value, then the two results is duplicate
+        # However, such a judgement will assume stable erroneously even when some cards are stiil loading
+        # Using any() is fast but may misjudge while using sum() / len() is slow but more accurate *maybe*
+        # not determine which is better
+            self._stable = True
+            self._remove_duplicate(results)
+
+        self.last_results = results
+
+    def multi_scan(self, main) -> None:
         """
         Here is a simple example,
             □ | □ | □                          --------- (*)
@@ -485,43 +701,35 @@ class DockScanner(ShipScanner):
         Therefore, graying the image and filter by np.std can get
         the position of those blanks.
         """
-        # Roughly Adjust
-        # After graying the image, calculate the standard deviation and take the part below the threshold
-        # Those parts should present multiple discontinuous subsequences, which here called gap_seq
-        scan_image = crop(image, self.scan_zone, copy=False)
+        main.device.disable_stuck_detection()
+        if DOCK_SCROLL.appear(main):
+            # can partly pre-load the image of ships,
+            # which reduce the possibility of getting stuck
+            DOCK_SCROLL.set_bottom(main)
+            DOCK_SCROLL.set_top(main)
 
-        def find_bound(image):
-            bound = []
-            image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-            std = np.std(image, axis=1)
-            gap_seq = [720] + list(np.nonzero(std < 10)[0])
-            logger.info(f'{gap_seq}')
-            for pos in range(len(gap_seq) - 1, 0, -1):
-                if abs(gap_seq[pos - 1] - gap_seq[pos]) > 50:
-                    bound.append(gap_seq[pos])
-            if len(bound) < 3:
-                bound = [0] + bound
-            return bound
+        while True:
+            while not self.stable:
+                main.device.screenshot()
+                self._scan(main.device.image)
 
-        bounds = [find_bound(crop(scan_image, button.area, copy=False)) for button in self.scan_grids.buttons]
-        card_bottom = (np.mean(bounds, axis=0) + 0.5).astype(np.uint8)
-        # Calculate the bound of gap_seq, usually we get 3 endpoints
-        # The offset is the difference between the two groups of endpoints
-        # Notice the example above, the first endpoint is the closest to the boundary, so we use its difference.
-        offset_rough = card_bottom[0] - self.card_bottom[0]
-        self.card_bottom.clear()
-        self.card_bottom.extend(card_bottom)
+            click_zone_index = random_normal_distribution_int(0, 6)
+            start = random_rectangle_point((
+                240 + click_zone_index * 165, 555, 250 + click_zone_index * 165, 719
+            ))
+            end = (start[0], start[1] - self.moving_distance)
+            sharp_end = (end[0] - 165, end[1])
+            main.device.swipe(start, end)
+            main.device.swipe(end, sharp_end)
 
-        # Preciously Adjust
-        # The adjustment here is based on CARD_RARITY_GRIDS, whose height is 5
-        # After binarization, the standard deviation of its surroundings is very small
-        # A correct offset will place CARD_RARITY_GRIDS on the first 5 lines
-        # Now do what similar to rough adjustment can get a precious offset
-        # !!!!
-        # Pratice shows that, the above method seems to have a poor effect
-        # Further work is needed.
-        offset = offset_rough
-        self.move(offset)
+            if not DOCK_SCROLL.appear(main) or (DOCK_SCROLL.at_bottom(main) and self.no_change()):
+                break
+
+    def scan(self, image, cached=False, output=True) -> Union[List, None]:
+        """
+        Please use multi_scan() instead.
+        """
+        pass
 
     def scan_one_fleet(self, fleet: int = None) -> List[Ship]:
         """
