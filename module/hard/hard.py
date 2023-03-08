ef0efe32be399f82b3691ebc4e90b8fca1ec6438
MengNianxiaoyao <2589141604@qq.com>
2023-03-08 08:55:53 +0800

    Add EnemyPriority

--- module/hard/hard.py
+++ module/hard/hard.py
@@ -2,8 +2,9 @@ import importlib
 
 from campaign.campaign_hard.campaign_hard import Campaign
 from module.campaign.run import CampaignRun
-from module.hard.assets import *
+from module.exception import CampaignEnd
 from module.logger import logger
+from module.hard.assets import *
 from module.ocr.ocr import Digit
 
 OCR_HARD_REMAIN = Digit(OCR_HARD_REMAIN, letter=(123, 227, 66), threshold=128, alphabet='0123')
@@ -23,12 +24,15 @@ class CampaignHard(CampaignRun):
             Campaign_UseAutoSearch=True,
             Fleet_FleetOrder='fleet1_all_fleet2_standby' if self.config.Hard_HardFleet == 1 else 'fleet1_standby_fleet2_all',
             Emotion_Mode='nothing',  # Dont calculate and dont ignore
+            EnemyPriority_EnemyScaleBalanceWeight='default_mode',
         )
         # Equipment take on
         # campaign/campaign_hard/campaign_hard.py Campaign.fleet_preparation()
 
         # Initial
-        self.load_campaign(name='campaign_hard', folder='campaign_hard')  # Load campaign file
+        self.handle_stage_name(name, folder='campaign_main')
+        self.load_campaign(name, folder='campaign_main') # Load campaign file(Not UseClearMode)
+        # self.load_campaign(name='campaign_hard', folder='campaign_hard')  # Load campaign file
         module = importlib.import_module('.' + name, 'campaign.campaign_main')  # Load map from normal mode.
         self.campaign.MAP = module.MAP
 
@@ -41,6 +45,23 @@ class CampaignHard(CampaignRun):
             name=self.config.Hard_HardStage,
             mode='hard'
         )
+        if self.campaign.is_in_map():
+            logger.info('Already in map, retreating.')
+            try:
+                self.campaign.withdraw()
+            except CampaignEnd:
+                pass
+            self.campaign.ensure_campaign_ui(name=self.stage, mode='hard')
+        elif self.campaign.is_in_auto_search_menu():
+            if self.can_use_auto_search_continue():
+                    logger.info('In auto search menu, skip ensure_campaign_ui.')
+            else:
+                logger.info('In auto search menu, closing.')
+                self.campaign.ensure_auto_search_exit()
+                self.campaign.ensure_campaign_ui(name=self.stage, mode='hard')
+        else:
+            self.campaign.ensure_campaign_ui(name=self.stage, mode='hard')
+        self.handle_commission_notice()
         
         # Run
         remain = OCR_HARD_REMAIN.ocr(self.device.image)
