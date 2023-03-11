a315e14604def1b624d633c7e98ce883576eb9da
SarContDeli <48789988+SaiCateDoan@users.noreply.github.com>
2023-03-11 16:59:04 +0800

    Fix: Wrong threshold in MAA infrast (#2356)

--- submodule/AlasMaaBridge/module/handler/handler.py
+++ submodule/AlasMaaBridge/module/handler/handler.py
@@ -325,7 +325,7 @@ class AssistantHandler:
         args = {
             "facility": self.split_filter(self.config.MaaInfrast_Facility),
             "drones": self.config.MaaInfrast_Drones,
-            "threshold": self.config.MaaInfrast_WorkThreshold,
+            "threshold": self.config.MaaInfrast_WorkThreshold / 24,
             "replenish": self.config.MaaInfrast_Replenish,
             "dorm_notstationed_enabled": self.config.MaaInfrast_Notstationed,
             "dorm_trust_enabled": self.config.MaaInfrast_Trust
