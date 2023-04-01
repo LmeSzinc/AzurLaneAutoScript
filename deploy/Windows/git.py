79d782d35bda609c181954b4ab20cd43c72c980d
LmeSzinc <37934724+LmeSzinc@users.noreply.github.com>
2023-04-01 21:22:35 +0800

    Opt: Faster git config set

--- deploy/Windows/git.py
+++ deploy/Windows/git.py
@@ -1,6 +1,18 @@
+import configparser
+
 from deploy.Windows.config import DeployConfig
 from deploy.Windows.logger import logger
 from deploy.Windows.utils import *
+
+
+class GitConfigParser(configparser.ConfigParser):
+    def check(self, section, option, value):
+        result = self.get(section, option, fallback=None)
+        if result == value:
+            logger.info(f'Git config {section}.{option} = {value}')
+            return True
+        else:
+            return False
 
 
 class GitManager(DeployConfig):
@@ -11,6 +23,12 @@ class GitManager(DeployConfig):
             logger.info(f'Removed file: {file}')
         except FileNotFoundError:
             logger.info(f'File not found: {file}')
+
+    @cached_property
+    def git_config(self):
+        conf = GitConfigParser()
+        conf.read('./.git/config')
+        return conf
 
     def git_repository_init(
             self, repo, source='origin', branch='master',
@@ -26,20 +44,27 @@ class GitManager(DeployConfig):
 
         logger.hr('Set Git Proxy', 1)
         if proxy:
-            self.execute(f'"{self.git}" config --local http.proxy {proxy}')
-            self.execute(f'"{self.git}" config --local https.proxy {proxy}')
+            if not self.git_config.check('http', 'proxy', value=proxy):
+                self.execute(f'"{self.git}" config --local http.proxy {proxy}')
+            if not self.git_config.check('https', 'proxy', value=proxy):
+                self.execute(f'"{self.git}" config --local https.proxy {proxy}')
         else:
-            self.execute(f'"{self.git}" config --local --unset http.proxy', allow_failure=True)
-            self.execute(f'"{self.git}" config --local --unset https.proxy', allow_failure=True)
+            if not self.git_config.check('http', 'proxy', value=None):
+                self.execute(f'"{self.git}" config --local --unset http.proxy', allow_failure=True)
+            if not self.git_config.check('https', 'proxy', value=None):
+                self.execute(f'"{self.git}" config --local --unset https.proxy', allow_failure=True)
 
         if ssl_verify:
-            self.execute(f'"{self.git}" config --local http.sslVerify true', allow_failure=True)
+            if not self.git_config.check('http', 'sslVerify', value='true'):
+                self.execute(f'"{self.git}" config --local http.sslVerify true', allow_failure=True)
         else:
-            self.execute(f'"{self.git}" config --local http.sslVerify false', allow_failure=True)
+            if not self.git_config.check('http', 'sslVerify', value='false'):
+                self.execute(f'"{self.git}" config --local http.sslVerify false', allow_failure=True)
 
         logger.hr('Set Git Repository', 1)
-        if not self.execute(f'"{self.git}" remote set-url {source} {repo}', allow_failure=True):
-            self.execute(f'"{self.git}" remote add {source} {repo}')
+        if not self.git_config.check(f'remote "{source}"', 'url', value=repo):
+            if not self.execute(f'"{self.git}" remote set-url {source} {repo}', allow_failure=True):
+                self.execute(f'"{self.git}" remote add {source} {repo}')
 
         logger.hr('Fetch Repository Branch', 1)
         self.execute(f'"{self.git}" fetch {source} {branch}')
