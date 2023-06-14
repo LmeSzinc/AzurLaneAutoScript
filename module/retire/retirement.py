4aeb859153c3fe04404f9566cf42ba3031411469
LmeSzinc <37934724+LmeSzinc@users.noreply.github.com>
2023-06-14 12:25:18 +0800

    Fix: Retire confirm end at EQUIP_CONFIRM cause game is slow to process

--- module/retire/retirement.py
+++ module/retire/retirement.py
@@ -113,10 +113,10 @@ class Retirement(Enhancement, QuickRetireSettingHandler):
                 self.interval_clear(GET_ITEMS_1)
                 continue
             if self.appear_then_click(EQUIP_CONFIRM, offset=(30, 30), interval=2):
+                executed = True
                 continue
             if self.appear_then_click(EQUIP_CONFIRM_2, offset=(30, 30), interval=2):
                 self.interval_clear(GET_ITEMS_1)
-                executed = True
                 continue
             if self.appear(GET_ITEMS_1, offset=(30, 30), interval=2):
                 self.device.click(GET_ITEMS_1_RETIREMENT_SAVE)
