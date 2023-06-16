d7bc6d82f6acd69e9c7978b3fe911939a0c69751
LmeSzinc <37934724+LmeSzinc@users.noreply.github.com>
2023-06-17 01:15:08 +0800

    Add: Combat finish assets

---
+++ tasks/combat/assets/assets_combat_finish.py
@@ -0,0 +1,25 @@
+from module.base.button import Button, ButtonWrapper
+
+# This file was auto-generated, do not modify it manually. To generate:
+# ``` python -m dev_tools.button_extract ```
+
+COMBAT_AGAIN = ButtonWrapper(
+    name='COMBAT_AGAIN',
+    cn=Button(
+        file='./assets/cn/combat/finish/COMBAT_AGAIN.png',
+        area=(846, 601, 924, 619),
+        search=(826, 581, 944, 639),
+        color=(162, 162, 162),
+        button=(709, 592, 979, 628),
+    ),
+)
+COMBAT_EXIT = ButtonWrapper(
+    name='COMBAT_EXIT',
+    cn=Button(
+        file='./assets/cn/combat/finish/COMBAT_EXIT.png',
+        area=(396, 601, 475, 619),
+        search=(376, 581, 495, 639),
+        color=(156, 156, 156),
+        button=(301, 593, 570, 628),
+    ),
+)
