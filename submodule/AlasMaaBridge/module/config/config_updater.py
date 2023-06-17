0c37354ed5a849a65063a93bc6e887eb6a8243d0
LmeSzinc <37934724+LmeSzinc@users.noreply.github.com>
2023-06-17 22:08:57 +0800

    Opt: List tasks instead of collapsing them in submodules

--- submodule/AlasMaaBridge/module/config/config_updater.py
+++ submodule/AlasMaaBridge/module/config/config_updater.py
@@ -34,12 +34,12 @@ class ConfigGenerator(config_updater.ConfigGenerator):
                 deep_set(new, keys=k, value=v)
 
         # Menu
-        for path, data in deep_iter(self.menu, depth=2):
-            func, group = path
-            deep_load(['Menu', func])
-            deep_load(['Menu', group])
-            for task in data:
-                deep_load([func, task])
+        for path, data in deep_iter(self.task, depth=3):
+            if 'tasks' not in path:
+                continue
+            task_group, _, task = path
+            deep_load(['Menu', task_group])
+            deep_load(['Task', task])
         # Arguments
         visited_group = set()
         for path, data in deep_iter(self.argument, depth=2):
