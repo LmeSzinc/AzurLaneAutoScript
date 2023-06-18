16d5e447df845fb87ba446d902c66c1f3d61c224
LmeSzinc <37934724+LmeSzinc@users.noreply.github.com>
2023-06-18 22:15:37 +0800

    Fix: Missing file assets_combat_interact.py (fixed #20)

---
+++ tasks/combat/assets/assets_combat_interact.py
@@ -0,0 +1,15 @@
+from module.base.button import Button, ButtonWrapper
+
+# This file was auto-generated, do not modify it manually. To generate:
+# ``` python -m dev_tools.button_extract ```
+
+DUNGEON_COMBAT_INTERACT = ButtonWrapper(
+    name='DUNGEON_COMBAT_INTERACT',
+    share=Button(
+        file='./assets/share/combat/interact/DUNGEON_COMBAT_INTERACT.png',
+        area=(779, 419, 807, 442),
+        search=(759, 399, 827, 462),
+        color=(190, 191, 191),
+        button=(777, 412, 997, 446),
+    ),
+)
