2c66b5e476606ad183624035f20df40689056042
MengNianxiaoyao <2589141604@qq.com>
2023-06-27 11:46:20 +0800

    Fix: dashboard config_updater

--- module/config/config_updater.py
+++ module/config/config_updater.py
@@ -276,11 +276,9 @@ class ConfigGenerator:
                 deep_load(['Task', task])
         # Arguments
         visited_group = set()
-        key=[]
-        for i in range(1):
-            key = deep_get(d=read_file(filepath_argument("task")), keys='Dashboard.tasks.Dashboard')
+        dashboard_args = deep_get(read_file(filepath_argument("task")), 'Dashboard.tasks.Dashboard')
         for path, data in deep_iter(self.argument, depth=2):
-            if path[0] not in key:
+            if path[0] not in dashboard_args:
                 if path[0] not in visited_group:
                     deep_load([path[0], '_info'])
                     visited_group.add(path[0])
