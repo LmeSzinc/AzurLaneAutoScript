7d3aed8a348935d0664a6a35cad0915520000c80
MengNianxiaoyao <2589141604@qq.com>
2023-07-30 18:43:48 +0800

    Upd: when Event PT limit = 0,not ocr Event PT

--- module/campaign/campaign_event.py
+++ module/campaign/campaign_event.py
@@ -47,20 +47,19 @@ class CampaignEvent(CampaignStatus):
         )
         tasks = EVENTS + RAIDS + COALITIONS + GEMS_FARMINGS
         command = self.config.Scheduler_Command
-        pt = self.get_event_pt()
-        if limit <= 0 or command not in tasks:
+        if limit < 0 or command not in tasks:
             return False
         if command == 'GemsFarming' and self.stage_is_main(self.config.Campaign_Name):
             return False
 
-        if limit != 0:
+        pt = self.get_event_pt()
+        if pt >= limit and limit > 0:
             logger.attr('Event_PT_limit', f'{pt}/{limit}')
-            if pt >= limit:
-                logger.hr(f'Reach event PT limit: {limit}')
-                self._disable_tasks(tasks)
-                return True
-            else:
-                return False
+            logger.hr(f'Reach event PT limit: {limit}')
+            self._disable_tasks(tasks)
+            return True
+        else:
+            return False
 
     def event_time_limit_triggered(self):
         """
