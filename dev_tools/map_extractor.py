b17450f14f7b109fa233ef58cf834442fab8a771
guoh064 <50830808+guoh064@users.noreply.github.com>
2023-08-17 13:27:15 +0800

    Add: The Fool's Scales (event_20230817_cn)

--- dev_tools/map_extractor.py
+++ dev_tools/map_extractor.py
@@ -216,6 +216,11 @@ DIC_SIREN_NAME_CHI_TO_ENG = {
     # Revelations of Dust
     'xiafei': 'Joffre',
     'lemaer': 'LeMars',
+
+    # The Fool's Scales
+    'sairenboss18': 'SirenBoss18',
+    'sairenboss19': 'SirenBoss19',
+    'srjiaohuangjijia': 'Dilloy',
 }
 
 
