f55daeaf4530cc3f9c9f72a8d764b9829db2b1b3
Zuosizhu <2839299264@qq.com>
2023-08-27 20:58:40 +0800

    Opt: users are allowed not to push lua before script runs in case of unsuccessful pushes.

--- module/config/config_generated.py
+++ module/config/config_generated.py
@@ -440,6 +440,7 @@ class GeneratedConfig:
     GGHandler_GGPackageName = 'com.'
     GGHandler_AutoRestartGG = False
     GGHandler_RestartEverytime = True
+    GGHandler_RepushLua = True
     GGHandler_GGMultiplyingFactor = 200
     GGHandler_DisabledTask = 'disable_all_dangerous_task'  # disable_all_dangerous_task, disable_guild_and_dangerous, disable_exercise, enable_all
 
