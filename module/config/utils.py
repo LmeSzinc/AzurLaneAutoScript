b734e1c40753586c9c957405729d6e6fb45d126f
LmeSzinc <37934724+LmeSzinc@users.noreply.github.com>
2023-09-11 23:49:07 +0800

    Fix: Template name at config add

--- module/config/utils.py
+++ module/config/utils.py
@@ -166,7 +166,7 @@ def alas_template():
     for file in os.listdir('./config'):
         name, extension = os.path.splitext(file)
         if name == 'template' and extension == '.json':
-            out.append(f'{name}-alas')
+            out.append(f'{name}-src')
 
     # out.extend(mod_template())
 
