1dcaf5355d49d423934c155a5ba4fdf34badb8bc
LmeSzinc <37934724+LmeSzinc@users.noreply.github.com>
2023-09-24 00:28:58 +0800

    Add: UI switching to Himeko trial

---
+++ tasks/daily/assets/assets_daily_trial.py
@@ -0,0 +1,75 @@
+from module.base.button import Button, ButtonWrapper
+
+# This file was auto-generated, do not modify it manually. To generate:
+# ``` python -m dev_tools.button_extract ```
+
+CHARACTER_TRIAL = ButtonWrapper(
+    name='CHARACTER_TRIAL',
+    share=Button(
+        file='./assets/share/daily/trial/CHARACTER_TRIAL.png',
+        area=(416, 672, 559, 677),
+        search=(396, 652, 579, 697),
+        color=(238, 238, 239),
+        button=(414, 649, 561, 678),
+    ),
+)
+HIMEKO_CHECK = ButtonWrapper(
+    name='HIMEKO_CHECK',
+    share=Button(
+        file='./assets/share/daily/trial/HIMEKO_CHECK.png',
+        area=(597, 165, 646, 192),
+        search=(577, 145, 666, 212),
+        color=(207, 137, 123),
+        button=(597, 165, 646, 192),
+    ),
+)
+HIMEKO_CLICK = ButtonWrapper(
+    name='HIMEKO_CLICK',
+    share=Button(
+        file='./assets/share/daily/trial/HIMEKO_CLICK.png',
+        area=(88, 142, 125, 178),
+        search=(42, 124, 176, 425),
+        color=(202, 144, 136),
+        button=(88, 142, 125, 178),
+    ),
+)
+INFO_CLOSE = ButtonWrapper(
+    name='INFO_CLOSE',
+    share=Button(
+        file='./assets/share/daily/trial/INFO_CLOSE.png',
+        area=(1129, 222, 1159, 252),
+        search=(1109, 202, 1179, 272),
+        color=(69, 69, 70),
+        button=(1129, 222, 1159, 252),
+    ),
+)
+REGULAR_GACHA_CHECK = ButtonWrapper(
+    name='REGULAR_GACHA_CHECK',
+    share=Button(
+        file='./assets/share/daily/trial/REGULAR_GACHA_CHECK.png',
+        area=(69, 332, 124, 367),
+        search=(15, 146, 150, 478),
+        color=(100, 92, 82),
+        button=(69, 332, 124, 367),
+    ),
+)
+REGULAR_GACHA_CLICK = ButtonWrapper(
+    name='REGULAR_GACHA_CLICK',
+    share=Button(
+        file='./assets/share/daily/trial/REGULAR_GACHA_CLICK.png',
+        area=(63, 324, 118, 349),
+        search=(15, 146, 150, 478),
+        color=(36, 34, 30),
+        button=(63, 324, 118, 349),
+    ),
+)
+START_TRIAL = ButtonWrapper(
+    name='START_TRIAL',
+    share=Button(
+        file='./assets/share/daily/trial/START_TRIAL.png',
+        area=(905, 634, 919, 674),
+        search=(885, 614, 939, 694),
+        color=(240, 238, 238),
+        button=(919, 636, 1191, 672),
+    ),
+)
