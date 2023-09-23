db5438f6b6363677ed8e0fc74f0048a627a83939
LmeSzinc <37934724+LmeSzinc@users.noreply.github.com>
2023-09-23 20:36:17 +0800

    Fix: Missing assets_forgotten_hall_team.py

---
+++ tasks/forgotten_hall/assets/assets_forgotten_hall_team.py
@@ -0,0 +1,45 @@
+from module.base.button import Button, ButtonWrapper
+
+# This file was auto-generated, do not modify it manually. To generate:
+# ``` python -m dev_tools.button_extract ```
+
+CHARACTER_1 = ButtonWrapper(
+    name='CHARACTER_1',
+    share=Button(
+        file='./assets/share/forgotten_hall/team/CHARACTER_1.png',
+        area=(44, 166, 140, 190),
+        search=(24, 146, 160, 210),
+        color=(211, 210, 213),
+        button=(44, 100, 140, 211),
+    ),
+)
+CHARACTER_2 = ButtonWrapper(
+    name='CHARACTER_2',
+    share=Button(
+        file='./assets/share/forgotten_hall/team/CHARACTER_2.png',
+        area=(150, 166, 246, 190),
+        search=(130, 146, 266, 210),
+        color=(212, 211, 210),
+        button=(150, 100, 246, 212),
+    ),
+)
+CHARACTER_3 = ButtonWrapper(
+    name='CHARACTER_3',
+    share=Button(
+        file='./assets/share/forgotten_hall/team/CHARACTER_3.png',
+        area=(256, 166, 352, 190),
+        search=(236, 146, 372, 210),
+        color=(214, 211, 212),
+        button=(256, 100, 352, 212),
+    ),
+)
+CHARACTER_4 = ButtonWrapper(
+    name='CHARACTER_4',
+    share=Button(
+        file='./assets/share/forgotten_hall/team/CHARACTER_4.png',
+        area=(44, 298, 140, 322),
+        search=(24, 278, 160, 342),
+        color=(212, 211, 213),
+        button=(44, 232, 140, 344),
+    ),
+)
