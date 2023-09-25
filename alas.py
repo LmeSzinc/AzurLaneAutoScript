6cd19cef87446694b39eee2bcfe287fe7af72bc6
Dreamry2C <58101503+Dreamry2C@users.noreply.github.com>
2023-09-25 21:00:49 +0800

    Update alas.py

--- alas.py
+++ alas.py
@@ -18,8 +18,10 @@ from module.gg_handler.gg_handler import GGHandler
 
 class AzurLaneAutoScript:
     stop_event: threading.Event = None
-    GAME_HAS_RESTARTED = False
-    GAME_RESTART_BECAUSE_ERROR = False
+    GameRestartBecauseErrorTimes = 0
+    AutoRestart_Enabled = False
+    AutoRestart_AttemptsToRestart = 0
+    AutoRestart_NotifyWhenAutoRestart = False
 
     def __init__(self, config_name='alas'):
         logger.hr('Start', level=0)
@@ -51,6 +53,9 @@ class AzurLaneAutoScript:
         except RequestHumanTakeover:
             logger.critical('Request human takeover')
             exit(1)
+        except EmulatorNotRunningError:
+            logger.critical('EmulatorNotRunningError')
+            exit(1)
         except Exception as e:
             logger.exception(e)
             exit(1)
@@ -66,12 +71,14 @@ class AzurLaneAutoScript:
             exit(1)
 
     def run(self, command):
+        self.AutoRestart_Enabled = deep_get(self.config.data, "Restart.AutoRestart.Enabled")
+        self.AutoRestart_NotifyWhenAutoRestart = deep_get(self.config.data, "Restart.AutoRestart.NotifyWhenAutoRestart")
+        self.AutoRestart_AttemptsToRestart = deep_get(self.config.data, "Restart.AutoRestart.AttemptsToRestart")
         try:
             self.device.screenshot()
             self.__getattribute__(command)()
-            if command != "restart" and self.GAME_RESTART_BECAUSE_ERROR:
-                self.GAME_HAS_RESTARTED = False
-                self.GAME_RESTART_BECAUSE_ERROR = False
+            if command != "restart" and self.GameRestartBecauseErrorTimes != 0:
+                self.GameRestartBecauseErrorTimes = 0
             return True
         except TaskEnd:
             return True
@@ -99,15 +106,19 @@ class AzurLaneAutoScript:
             logger.info('Game server may be under maintenance or network may be broken, check server status now')
             self.checker.check_now()
             if self.checker.is_available():
-                if not self.GAME_HAS_RESTARTED:
+                if self.AutoRestart_Enabled and self.GameRestartBecauseErrorTimes <= self.AutoRestart_AttemptsToRestart:
+                    if self.AutoRestart_NotifyWhenAutoRestart:
+                        handle_notify(
+                            self.config.Error_OnePushConfig,
+                            title=f"Alas <{self.config_name}> auto restarted",
+                            content=f"Command \"{command}\" failed because GamePageUnknownError, but alas auto restarted",
+                        )
                     self.config.task_call('Restart')
-                    self.GAME_HAS_RESTARTED = True
-                    self.GAME_RESTART_BECAUSE_ERROR = True
+                    self.GameRestartBecauseErrorTimes += 1
                     self.device.sleep(10)
                     return False
                 else:
-                    self.GAME_HAS_RESTARTED = False
-                    self.GAME_RESTART_BECAUSE_ERROR = False
+                    self.GameRestartBecauseErrorTimes = 0
                     logger.critical('Game page unknown')
                     self.save_error_log()
                     handle_notify(
@@ -123,7 +134,6 @@ class AzurLaneAutoScript:
             else:
                 self.checker.wait_until_available()
                 return False
-
         except ScriptError as e:
             logger.critical(e)
             logger.critical('This is likely to be a mistake of developers, but sometimes just random issues')
@@ -134,15 +144,19 @@ class AzurLaneAutoScript:
             )
             exit(1)
         except RequestHumanTakeover:
-            if not self.GAME_HAS_RESTARTED:
+            if self.AutoRestart_Enabled and self.GameRestartBecauseErrorTimes <= self.AutoRestart_AttemptsToRestart:
+                if self.AutoRestart_NotifyWhenAutoRestart:
+                    handle_notify(
+                        self.config.Error_OnePushConfig,
+                        title=f"Alas <{self.config_name}> auto restarted",
+                        content=f"Command \"{command}\" failed because RequestHumanTakeover, but alas auto restarted",
+                    )
                 self.config.task_call('Restart')
-                self.GAME_HAS_RESTARTED = True
-                self.GAME_RESTART_BECAUSE_ERROR = True
+                self.GameRestartBecauseErrorTimes += 1
                 self.device.sleep(10)
                 return False
             else:
-                self.GAME_HAS_RESTARTED = False
-                self.GAME_RESTART_BECAUSE_ERROR = False
+                self.GameRestartBecauseErrorTimes = 0
                 logger.critical('Request human takeover')
                 handle_notify(
                     self.config.Error_OnePushConfig,
@@ -150,12 +164,10 @@ class AzurLaneAutoScript:
                     content=f"<{self.config_name}> RequestHumanTakeover",
                 )
                 exit(1)
-                
         except AutoSearchSetError:
             logger.critical('Auto search could not be set correctly. Maybe your ships in hard mode are changed.')
             logger.critical('Request human takeover.')
             exit(1)
-
         except Exception as e:
             logger.exception(e)
             self.save_error_log()
@@ -173,7 +185,7 @@ class AzurLaneAutoScript:
         """
         from module.base.utils import save_image
         from module.handler.sensitive_info import (handle_sensitive_image,
-                                                    handle_sensitive_logs)
+                                                   handle_sensitive_logs)
         if self.config.Error_SaveError:
             if not os.path.exists('./log/error'):
                 os.mkdir('./log/error')
@@ -569,7 +581,16 @@ class AzurLaneAutoScript:
             self.device.stuck_record_clear()
             self.device.click_record_clear()
             logger.hr(task, level=0)
-            success = self.run(inflection.underscore(task))
+            success = True
+            if task in ["Research", "Reward", "Commission"]:
+                InfiniteDelay = deep_get(self.config.data, f"SomethingSpecial.InfiniteDelay.{task}")
+                if InfiniteDelay:
+                    logger.warning(f"Task '{task}' infinite delay: {InfiniteDelay}")
+                    self.config.task_delay(target=datetime.now() + timedelta(hours=6, seconds=-1), task=task)
+                else:
+                    success = self.run(inflection.underscore(task))
+            else:
+                success = self.run(inflection.underscore(task))
             logger.info(f'Scheduler: End task `{task}`')
             self.is_first_task = False
 
@@ -578,15 +599,19 @@ class AzurLaneAutoScript:
             failed = 0 if success else failed + 1
             deep_set(self.failure_record, keys=task, value=failed)
             if failed >= 3:
-                if not self.GAME_HAS_RESTARTED:
+                if self.AutoRestart_Enabled and self.GameRestartBecauseErrorTimes <= self.AutoRestart_AttemptsToRestart:
                     failed = 0
+                    if self.AutoRestart_NotifyWhenAutoRestart:
+                        handle_notify(
+                            self.config.Error_OnePushConfig,
+                            title=f"Alas <{self.config_name}> auto restarted",
+                            content=f"<{task}> failed, but alas auto restarted",
+                        )
                     self.config.task_call('Restart')
-                    self.GAME_HAS_RESTARTED = True
-                    self.GAME_RESTART_BECAUSE_ERROR = True
+                    self.GameRestartBecauseErrorTimes += 1
                     self.device.sleep(10)
                 else:
-                    self.GAME_HAS_RESTARTED = False
-                    self.GAME_RESTART_BECAUSE_ERROR = False
+                    self.GameRestartBecauseErrorTimes = 0
                     logger.critical(f"Task `{task}` failed 3 or more times.")
                     logger.critical("Possible reason #1: You haven't used it correctly. "
                                     "Please read the help text of the options.")
