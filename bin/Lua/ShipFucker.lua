02a804ccf172f15d083470e6e3aa5656657698b7
LA-DI-DA <11174151+0O0o0oOoO00@users.noreply.github.com>
2023-10-19 16:51:51 +0800

    Upd: ShipFucker.lua

--- bin/Lua/ShipFucker.lua
+++ bin/Lua/ShipFucker.lua
@@ -11,6 +11,8 @@ function ChangeShipAttribute()
 			 		    	,{"text"})
 
 	local TargetList = string.split(tostring(Result[1]), "|")
+	local OriginalRange = gg.getRanges()
+	gg.setRanges(gg.REGION_ANONYMOUS)
     for i = 1, #TargetList do
         local SplitedTarget = string.split(TargetList[i], ":")
         local ShipData = SplitedTarget[1]  -- {Id};{Star};{Type}
@@ -35,13 +37,9 @@ function ChangeShipAttribute()
 		end
 
 		for j = 1, #SearchResult do
-			local MemoryFrom = SearchResult[j].address - 0x800
-			local MemoryTo = SearchResult[j].address - 0x650
+			local MemoryFrom = SearchResult[j].address - 0x1850
+			local MemoryTo = SearchResult[j].address
 			for k = 1, #ShipAttributeList do
-				-- fuck gg
-				-- fuck lua
-				-- fuck dynamic type
-				-- fuck everything
 				local SplitedAttribute = string.split(tostring(ShipAttributeList[k]), ">")
 				local AttributeToSearchList = SplitedAttribute[1]
 				local TargetAttributeList = SplitedAttribute[2]
@@ -58,8 +56,38 @@ function ChangeShipAttribute()
 				end
 				gg.clearResults()
 			end
+			MemoryFrom = SearchResult[j].address - 0x300
+			MemoryTo = SearchResult[j].address
+			gg.searchNumber("A0000000h~B0000000h", gg.TYPE_DOUBLE, false, gg.SIGN_EQUAL, MemoryFrom, MemoryTo)
+			local AddressResult = gg.getResults(1024)
+			if next(AddressResult) == nil then
+				gg.searchNumber("A0000000h~B0000000h", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, MemoryFrom, MemoryTo)
+				AddressResult = gg.getResults(1024)
+			end
+			for l = 1, #AddressResult do
+				MemoryFrom = AddressResult[l].value
+				MemoryTo = AddressResult[l].value + 0x100
+				for m = 1, #ShipAttributeList do
+					local SplitedAttribute = string.split(tostring(ShipAttributeList[m]), ">")
+					local AttributeToSearchList = SplitedAttribute[1]
+					local TargetAttributeList = SplitedAttribute[2]
+					gg.clearResults()
+					gg.searchNumber(AttributeToSearchList, gg.TYPE_DOUBLE, false, gg.SIGN_EQUAL, MemoryFrom, MemoryTo)
+					local AttributeResult = gg.getResults(1024)
+					if next(AttributeResult) == nil then
+						gg.clearResults()
+						gg.searchNumber(AttributeToSearchList, gg.TYPE_DWORD, false, gg.SIGN_EQUAL, MemoryFrom, MemoryTo)
+						gg.getResults(1024)
+						gg.editAll(TargetAttributeList, gg.TYPE_DWORD)
+					else
+						gg.editAll(TargetAttributeList, gg.TYPE_DOUBLE)
+					end
+					gg.clearResults()
+				end
+			end
 		end
     end
+	gg.setRanges(OriginalRange)
 	Exit("修改成功！")
 end
 
