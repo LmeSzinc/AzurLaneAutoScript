b98be7dc1524988469f9a39f50d44a2bdb83a5e5
X-Zero-L <zeroeeau@gmail.com>
2023-10-28 19:16:19 +0800

    Upd: Character

--- module/config/config_updater.py
+++ module/config/config_updater.py
@@ -91,7 +91,7 @@ class ConfigGenerator:
             options=[dungeon.name for dungeon in DungeonList.instances.values() if dungeon.is_Echo_of_War])
         # Insert characters
         from tasks.character.keywords import CharacterList
-        unsupported_characters = ["Guinaifen", "TopazandNumby"]
+        unsupported_characters = []
         characters = [character.name for character in CharacterList.instances.values()
                       if character.name not in unsupported_characters]
         option_add(keys='DungeonSupport.Character.option', options=characters)
