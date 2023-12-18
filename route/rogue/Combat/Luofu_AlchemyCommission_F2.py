d1ded20872c0c1957275993704d13050622dd633
LmeSzinc <37934724+LmeSzinc@users.noreply.github.com>
2023-12-18 12:46:42 +0800

    Add: Copy of Luofu_AlchemyCommission_F2_X447Y508

--- route/rogue/Combat/Luofu_AlchemyCommission_F2.py
+++ route/rogue/Combat/Luofu_AlchemyCommission_F2.py
@@ -43,6 +43,27 @@ class Route(RouteBase):
 
         self.clear_enemy(enemy)
 
+    def Luofu_AlchemyCommission_F2_X454Y501(self):
+        """
+        | Waypoint | Position                  | Direction | Rotation |
+        | -------- | ------------------------- | --------- | -------- |
+        | spawn    | Waypoint((447.4, 508.1)), | 59.1      | 48       |
+        | enemy    | Waypoint((494.8, 461.2)), | 149.0     | 48       |
+        | exit_    | Waypoint((494.8, 461.2)), | 149.0     | 48       |
+        | exit1    | Waypoint((494.0, 448.0)), | 59.1      | 48       |
+        | exit2    | Waypoint((506.8, 460.8)), | 59.1      | 48       |
+        """
+        self.map_init(plane=Luofu_AlchemyCommission, floor="F2", position=(453.7, 501.3))
+        self.register_domain_exit(
+            Waypoint((494.8, 461.2)), end_rotation=48,
+            left_door=Waypoint((494.0, 448.0)), right_door=Waypoint((506.8, 460.8)))
+        enemy = Waypoint((494.8, 461.2))
+        # ===== End of generated waypoints =====
+
+        self.clear_enemy(enemy)
+
+        # Same as Luofu_AlchemyCommission_F2_X447Y508
+
     def Luofu_AlchemyCommission_F2_X457Y497(self):
         """
         | Waypoint | Position                  | Direction | Rotation |
