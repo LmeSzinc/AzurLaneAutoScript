6fe869702b1a1c8c291f920412289845698e83f7
LmeSzinc <37934724+LmeSzinc@users.noreply.github.com>
2024-01-07 10:44:20 +0800

    Add: Do map interacts in dialogue clicker

--- tasks/base/assets/assets_base_daemon.py
+++ tasks/base/assets/assets_base_daemon.py
@@ -23,6 +23,36 @@ CHAT_OPTION = ButtonWrapper(
         button=(649, 496, 1129, 525),
     ),
 )
+INTERACT_COLLECT = ButtonWrapper(
+    name='INTERACT_COLLECT',
+    share=Button(
+        file='./assets/share/base/daemon/INTERACT_COLLECT.png',
+        area=(754, 423, 772, 441),
+        search=(734, 403, 792, 461),
+        color=(154, 166, 170),
+        button=(754, 423, 772, 441),
+    ),
+)
+INTERACT_INVESTIGATE = ButtonWrapper(
+    name='INTERACT_INVESTIGATE',
+    share=Button(
+        file='./assets/share/base/daemon/INTERACT_INVESTIGATE.png',
+        area=(754, 419, 774, 439),
+        search=(734, 399, 794, 459),
+        color=(127, 129, 131),
+        button=(754, 419, 774, 439),
+    ),
+)
+INTERACT_TREASURE = ButtonWrapper(
+    name='INTERACT_TREASURE',
+    share=Button(
+        file='./assets/share/base/daemon/INTERACT_TREASURE.png',
+        area=(753, 420, 781, 440),
+        search=(733, 400, 801, 460),
+        color=(193, 194, 195),
+        button=(753, 420, 781, 440),
+    ),
+)
 STORY_NEXT = ButtonWrapper(
     name='STORY_NEXT',
     share=Button(
