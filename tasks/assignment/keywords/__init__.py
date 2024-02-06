18666ce8172153ea3746fb9f3531c81cea33a8c4
LmeSzinc <37934724+LmeSzinc@users.noreply.github.com>
2024-02-06 23:03:42 +0800

    Upd: Add assignments in 2.0

--- tasks/assignment/keywords/__init__.py
+++ tasks/assignment/keywords/__init__.py
@@ -11,6 +11,8 @@ KEYWORDS_ASSIGNMENT_GROUP.Character_Materials.entries = (
     KEYWORDS_ASSIGNMENT_ENTRY.Born_to_Obey,
     KEYWORDS_ASSIGNMENT_ENTRY.Root_Out_the_Turpitude,
     KEYWORDS_ASSIGNMENT_ENTRY.Fire_Lord_Inflames_Blades_of_War,
+    KEYWORDS_ASSIGNMENT_ENTRY.A_Startling_Night_Terror,
+    KEYWORDS_ASSIGNMENT_ENTRY.Tranquility_of_Vimala_bhumi,
 )
 KEYWORDS_ASSIGNMENT_GROUP.EXP_Materials_Credits.entries = (
     KEYWORDS_ASSIGNMENT_ENTRY.Nameless_Land_Nameless_People,
@@ -24,6 +26,8 @@ KEYWORDS_ASSIGNMENT_GROUP.Synthesis_Materials.entries = (
     KEYWORDS_ASSIGNMENT_ENTRY.The_Blossom_in_the_Storm,
     KEYWORDS_ASSIGNMENT_ENTRY.Legend_of_the_Puppet_Master,
     KEYWORDS_ASSIGNMENT_ENTRY.The_Wages_of_Humanity,
+    KEYWORDS_ASSIGNMENT_ENTRY.Fragments_of_Illusory_Dreams,
+    KEYWORDS_ASSIGNMENT_ENTRY.Scalpel_and_Screwdriver,
 )
 KEYWORDS_ASSIGNMENT_EVENT_GROUP.Space_Station_Task_Force.entries = (
     KEYWORDS_ASSIGNMENT_EVENT_ENTRY.Repulsion_Bridge_Errors,
