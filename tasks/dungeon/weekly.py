282ab6da72c3eabfac5fc6ea1de418eef1a804d9
LmeSzinc <37934724+LmeSzinc@users.noreply.github.com>
2024-02-12 23:29:21 +0800

    Chore: _dungeon_nav_goto now receive nav input

--- tasks/dungeon/weekly.py
+++ tasks/dungeon/weekly.py
@@ -3,7 +3,7 @@ from module.ocr.ocr import DigitCounter
 from tasks.daily.keywords import KEYWORDS_DAILY_QUEST
 from tasks.dungeon.assets.assets_dungeon_ui import OCR_DUNGEON_LIST, OCR_WEEKLY_LIMIT
 from tasks.dungeon.dungeon import Dungeon
-from tasks.dungeon.keywords import DungeonList, KEYWORDS_DUNGEON_TAB
+from tasks.dungeon.keywords import DungeonList, KEYWORDS_DUNGEON_NAV, KEYWORDS_DUNGEON_TAB
 from tasks.dungeon.ui import DUNGEON_LIST
 
 
@@ -53,7 +53,7 @@ class WeeklyDungeon(Dungeon):
         self.dungeon_tab_goto(KEYWORDS_DUNGEON_TAB.Survival_Index)
         # Equivalent to self.dungeon_goto(dungeon), but check limit remains
         DUNGEON_LIST.search_button = OCR_DUNGEON_LIST
-        self._dungeon_nav_goto(dungeon)
+        self._dungeon_nav_goto(KEYWORDS_DUNGEON_NAV.Echo_of_War)
 
         # Check limit
         remain = self.get_weekly_remain()
