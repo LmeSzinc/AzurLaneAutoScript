3ae48fedd8e96e9344e1f0691e3c74c9ca13223f
LmeSzinc <37934724+LmeSzinc@users.noreply.github.com>
2024-02-23 12:36:17 +0800

    Upd: [EN] OCR_DOUBLE_EVENT_REMAIN

--- tasks/dungeon/assets/assets_dungeon_event.py
+++ tasks/dungeon/assets/assets_dungeon_event.py
@@ -37,10 +37,10 @@ OCR_DOUBLE_EVENT_REMAIN = ButtonWrapper(
     name='OCR_DOUBLE_EVENT_REMAIN',
     share=Button(
         file='./assets/share/dungeon/event/OCR_DOUBLE_EVENT_REMAIN.png',
-        area=(523, 181, 1087, 203),
-        search=(503, 161, 1107, 223),
-        color=(200, 162, 88),
-        button=(523, 181, 1087, 203),
+        area=(513, 181, 1103, 203),
+        search=(493, 161, 1123, 223),
+        color=(201, 164, 88),
+        button=(513, 181, 1103, 203),
     ),
 )
 OCR_DOUBLE_EVENT_REMAIN_AT_COMBAT = ButtonWrapper(
