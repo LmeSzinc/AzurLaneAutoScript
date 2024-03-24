189458440d315eb47ccae47196bcf879cc3ed6a1
guoh064 <50830808+guoh064@users.noreply.github.com>
2024-03-24 19:53:23 +0800

    Del: redundant assets MOB_MOVE_ICON.png

--- campaign/campaign_main/campaign_15_base.py
+++ campaign/campaign_main/campaign_15_base.py
@@ -1,7 +1,7 @@
 from module.base.mask import Mask
 from module.base.timer import Timer
 from module.campaign.campaign_base import CampaignBase as CampaignBase_
-from module.handler.assets import MOB_MOVE_ICON, STRATEGY_OPENED
+from module.handler.assets import STRATEGY_OPENED
 from module.map_detection.utils_assets import ASSETS
 from module.logger import logger
 from module.map.utils import location_ensure
