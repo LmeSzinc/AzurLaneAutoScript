46c3cbbdfa3dbd301b5f0fa1484622ea04a57e43
fro0116 <githubfro@lewisl.net>
2024-04-08 21:50:23 -0700

    Ensure interactivity

--- module/ui/ui.py
+++ module/ui/ui.py
@@ -144,6 +144,11 @@ class UI(InfoHandler):
 
         timeout = Timer(10, count=20).start()
         while 1:
+            if not self.device.get_interactive():
+                logger.info('Device not interactive, attempting to wake device')
+                self.device.adb_shell(['input', 'keyevent', 'KEYCODE_POWER'])
+                self.device.sleep(1)
+                continue
             if skip_first_screenshot:
                 skip_first_screenshot = False
                 if not hasattr(self.device, "image") or self.device.image is None:
