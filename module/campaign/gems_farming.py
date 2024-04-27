7e75b7fdd7e3e0616a0b5490b2ca4f1369c5653a
Zuosizhu <2839299264@qq.com>
2024-04-27 12:57:29 +0800

    Revert "Fix: scan ships in correct fleet"
    This reverts commit 0eee91a891de078eeb303b2d30c8f49dcb437870.

--- module/campaign/gems_farming.py
+++ module/campaign/gems_farming.py
@@ -95,12 +95,11 @@ class GemsFarming(CampaignRun, FleetEquipment, Dock):
     def change_vanguard_equip(self):
         return 'equip' in self.config.GemsFarming_ChangeVanguard
 
-    @property
-    def fleet_to_attack(self):
+    def fleet_enter(self, fleet=None):
+        fleet = self.config.Fleet_Fleet1
         if self.config.Fleet_FleetOrder == 'fleet1_standby_fleet2_all':
-            return self.config.Fleet_Fleet2
-        else:
-            return self.config.Fleet_Fleet1
+            fleet = self.config.Fleet_Fleet2
+        super().fleet_enter(fleet)
 
     def flagship_change(self):
         """
@@ -117,7 +116,7 @@ class GemsFarming(CampaignRun, FleetEquipment, Dock):
             index_list = range(0, 5)
         logger.hr('Change flagship', level=1)
         logger.attr('ChangeFlagship', self.config.GemsFarming_ChangeFlagship)
-        self.fleet_enter(self.fleet_to_attack)
+        self.fleet_enter()
         if self.change_flagship_equip:
             logger.hr('Record flagship equipment', level=2)
             self.fleet_enter_ship(FLEET_DETAIL_ENTER_FLAGSHIP)
@@ -147,7 +146,7 @@ class GemsFarming(CampaignRun, FleetEquipment, Dock):
 
         logger.hr('Change vanguard', level=1)
         logger.attr('ChangeVanguard', self.config.GemsFarming_ChangeVanguard)
-        self.fleet_enter(self.fleet_to_attack)
+        self.fleet_enter()
         if self.change_vanguard_equip:
             logger.hr('Record vanguard equipment', level=2)
             self.fleet_enter_ship(FLEET_DETAIL_ENTER)
@@ -184,8 +183,8 @@ class GemsFarming(CampaignRun, FleetEquipment, Dock):
 
         logger.hr('FINDING FLAGSHIP')
 
-        scanner = ShipScanner(level=(1, 31), emotion=(10, 150),
-                              fleet=self.fleet_to_attack, status='free')
+        scanner = ShipScanner(
+            level=(1, 31), emotion=(10, 150), fleet=self.config.Fleet_Fleet1, status='free')
         scanner.disable('rarity')
 
         if self.config.GemsFarming_CommonCV == 'any':
@@ -244,7 +243,7 @@ class GemsFarming(CampaignRun, FleetEquipment, Dock):
             max_level = 70
 
         scanner = ShipScanner(level=(max_level, max_level), emotion=(10, 150),
-                              fleet=self.fleet_to_attack, status='free')
+                              fleet=self.config.Fleet_Fleet1, status='free')
         scanner.disable('rarity')
 
         self.dock_sort_method_dsc_set()
