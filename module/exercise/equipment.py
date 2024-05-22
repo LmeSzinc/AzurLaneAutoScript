551d10cdb8d4731f3feb960bf85b88680640d349
LA_DI_DA <11174151+0O0o0oOoO00@users.noreply.github.com>
2024-05-22 10:33:49 +0800

    fix: Adapt to new APIs in retire, exercise, hard
    # module/retire/dock.py
    # module/exercise/equipment.py
    # module/hard/equipment.py

--- module/exercise/equipment.py
+++ module/exercise/equipment.py
@@ -1,10 +1,13 @@
 from module.base.timer import Timer
 from module.combat.assets import BATTLE_PREPARATION
+from module.equipment.equipment import Equipment
 from module.equipment.equipment_change import EquipmentChange
 from module.exercise.assets import *
+from module.exercise.assets_override import exer_assets_override
 
 
-class ExerciseEquipment(EquipmentChange):
+class ExerciseEquipmentOld(Equipment):
+    @exer_assets_override("old")
     def _active_edit(self):
         timer = Timer(5)
         while 1:
@@ -18,6 +21,7 @@ class ExerciseEquipment(EquipmentChange):
                 self.device.sleep((0.2, 0.3))
                 break
 
+    @exer_assets_override("old")
     def _inactive_edit(self):
         timer = Timer(5)
         while 1:
@@ -31,13 +35,73 @@ class ExerciseEquipment(EquipmentChange):
                 self.device.sleep((0.2, 0.3))
                 break
 
+    @exer_assets_override("old")
+    def equipment_take_on(self):
+        if self.config.EXERCISE_FLEET_EQUIPMENT is None:
+            return False
+        if self.equipment_has_take_on:
+            return False
+
+        self._active_edit()
+        super().equipment_take_on(enter=EQUIP_ENTER, out=BATTLE_PREPARATION, fleet=self.config.EXERCISE_FLEET_EQUIPMENT)
+        self._inactive_edit()
+        return True
+
+    @exer_assets_override("old")
+    def equipment_take_off(self):
+        if self.config.EXERCISE_FLEET_EQUIPMENT is None:
+            return False
+        if not self.equipment_has_take_on:
+            return False
+
+        self._active_edit()
+        super().equipment_take_off(enter=EQUIP_ENTER, out=BATTLE_PREPARATION, fleet=self.config.EXERCISE_FLEET_EQUIPMENT)
+        self._inactive_edit()
+        return True
+
+
+class ExerciseEquipmentNew(EquipmentChange):
+    @exer_assets_override("new")
+    def _active_edit(self):
+        timer = Timer(5)
+        while 1:
+            self.device.screenshot()
+
+            if timer.reached() and self.appear_then_click(EQUIP_EDIT_INACTIVE):
+                timer.reset()
+
+            # End
+            if self.appear(EQUIP_EDIT_ACTIVE):
+                self.device.sleep((0.2, 0.3))
+                break
+
+    @exer_assets_override("new")
+    def _inactive_edit(self):
+        timer = Timer(5)
+        while 1:
+            self.device.screenshot()
+
+            if timer.reached() and self.appear_then_click(EQUIP_EDIT_ACTIVE):
+                timer.reset()
+
+            # End
+            if self.appear(EQUIP_EDIT_INACTIVE):
+                self.device.sleep((0.2, 0.3))
+                break
+
+    @exer_assets_override("new")
     def equipment_take_on(self):
         self._active_edit()
         self.fleet_equipment_take_on_preset(preset_record=self.config.EXERCISE_FLEET_EQUIPMENT, enter=EQUIP_ENTER,
                                             long_click=True, out=BATTLE_PREPARATION)
         self._inactive_edit()
 
+    @exer_assets_override("new")
     def equipment_take_off(self):
         self._active_edit()
         self.fleet_equipment_take_off(enter=EQUIP_ENTER, long_click=True, out=BATTLE_PREPARATION)
         self._inactive_edit()
+
+
+class ExerciseEquipment(ExerciseEquipmentOld if globals().get("g_current_task", "") == "GemsFarming" else ExerciseEquipmentNew):
+    ...
--- module/hard/equipment.py
+++ module/hard/equipment.py
@@ -1,9 +1,32 @@
+from module.equipment.equipment import Equipment
 from module.equipment.equipment_change import EquipmentChange
 from module.hard.assets import *
 from module.map.assets import *
 
 
-class HardEquipment(EquipmentChange):
+class HardEquipmentOld(Equipment):
+    def equipment_take_on(self):
+        if self.config.FLEET_HARD_EQUIPMENT is None:
+            return False
+        if self.equipment_has_take_on:
+            return False
+
+        enter = EQUIP_ENTER_1 if self.config.FLEET_HARD == 1 else EQUIP_ENTER_2
+        super().equipment_take_on(enter=enter, out=FLEET_PREPARATION, fleet=self.config.FLEET_HARD_EQUIPMENT)
+        return True
+
+    def equipment_take_off(self):
+        if self.config.FLEET_HARD_EQUIPMENT is None:
+            return False
+        if not self.equipment_has_take_on:
+            return False
+
+        enter = EQUIP_ENTER_1 if self.config.FLEET_HARD == 1 else EQUIP_ENTER_2
+        super().equipment_take_off(enter=enter, out=FLEET_PREPARATION, fleet=self.config.FLEET_HARD_EQUIPMENT)
+        return True
+
+
+class HardEquipmentNew(EquipmentChange):
 
     def equipment_take_on(self):
         if self.config.FLEET_HARD_EQUIPMENT is None:
@@ -25,3 +48,7 @@ class HardEquipment(EquipmentChange):
         enter = EQUIP_ENTER_1 if self.config.Hard_HardFleet == 1 else EQUIP_ENTER_2
         self.fleet_equipment_take_off(enter=enter, long_click=True, out=FLEET_PREPARATION)
         return True
+
+
+class HardEquipment(HardEquipmentOld if globals().get("g_current_task", "") == "GemsFarming" else HardEquipmentNew):
+    ...
