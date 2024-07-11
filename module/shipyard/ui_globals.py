d161fc8cbfbef7401905c62d20fff89d649918fc
LmeSzinc <37934724+LmeSzinc@users.noreply.github.com>
2024-07-12 00:31:34 +0800

    Upd: Series selecting page in shipyard

--- module/shipyard/ui_globals.py
+++ module/shipyard/ui_globals.py
@@ -1,5 +1,5 @@
 from module.base.button import ButtonGrid
-from module.ocr.ocr import Digit, DigitCounter, Ocr
+from module.ocr.ocr import Digit
 from module.shipyard.assets import *
 
 SHIPYARD_FACE_GRID = ButtonGrid(origin=(188, 607), delta=(181, 0),
@@ -10,8 +10,8 @@ SHIPYARD_BP_COUNT_GRID = ButtonGrid(origin=(324, 688), delta=(181, 0),
                                     button_shape=(45, 30), grid_shape=(6, 1),
                                     name='SHIPYARD_BP_COUNT_GRID')
 
-SHIPYARD_SERIES_GRID = ButtonGrid(origin=(450, 260), delta=(280, 90),
-                                  button_shape=(155, 40), grid_shape=(2, 3),
+SHIPYARD_SERIES_GRID = ButtonGrid(origin=(75, 133), delta=(399, 91),
+                                  button_shape=(337, 60), grid_shape=(3, 3),
                                   name='SHIPYARD_SERIES_GRID')
 
 OCR_SHIPYARD_BP_COUNT_GRID = Digit(SHIPYARD_BP_COUNT_GRID.buttons,
