ff6307b09df73d498f93c01f68d2587fb4b89fb4
LmeSzinc <37934724+LmeSzinc@users.noreply.github.com>
2024-07-18 21:52:47 +0800

    Fix: Allow Hermit on VMOS only

--- module/device/device.py
+++ module/device/device.py
@@ -131,6 +131,10 @@ class Device(Screenshot, Control, AppControl):
         # if self.config.Emulator_ScreenshotMethod != 'nemu_ipc' and self.config.Emulator_ControlMethod == 'nemu_ipc':
         #     logger.warning('When not using nemu_ipc, both screenshot and control should not use nemu_ipc')
         #     self.config.Emulator_ControlMethod = 'minitouch'
+        # Allow Hermit on VMOS only
+        if self.config.Emulator_ControlMethod == 'Hermit' and not self.is_vmos:
+            logger.warning('ControlMethod is allowed on VMOS only')
+            self.config.Emulator_ControlMethod = 'minitouch'
         pass
 
     def handle_night_commission(self, daily_trigger='21:00', threshold=30):
