d6dd8cc16796996b407102bbbe9eb33889afac3d
sui-feng-cb <wyy13761770280@126.com>
2024-09-21 01:38:55 +0800

    Add: War Archives Sundered Blue, The Flame-Touched Dagger, Upon the Shimmering Blue, Daedalian Hymn

---
+++ campaign/war_archives_20201012_cn/sp3.py
@@ -0,0 +1,63 @@
+from ..campaign_war_archives.campaign_base import CampaignBase
+from module.logger import logger
+from module.map.map_base import CampaignMap
+from module.map.map_grids import RoadGrids, SelectedGrids
+
+from .sp1 import Config as ConfigBase
+
+MAP = CampaignMap('SP3')
+MAP.shape = 'I6'
+MAP.camera_data = ['D2', 'D4', 'F2', 'F4']
+MAP.camera_data_spawn_point = ['F2', 'D2']
+MAP.map_data = """
+    ++ ++ Me ++ ++ ++ ME Me ME
+    ++ ++ -- -- MB -- -- -- --
+    -- ME -- SP MB SP -- ++ ME
+    -- Me -- -- __ -- -- ME --
+    ++ ME -- Me ++ Me -- ++ ++
+    ++ ME -- ME -- ME -- Me ++
+"""
+MAP.weight_data = """
+    50 50 50 50 50 50 50 50 50
+    50 50 50 50 50 50 50 50 50
+    50 50 50 50 50 50 50 50 50
+    50 50 50 50 50 50 50 50 50
+    50 50 50 50 50 50 50 50 50
+    50 50 50 50 50 50 50 50 50
+"""
+MAP.spawn_data = [
+    {'battle': 0, 'enemy': 4},
+    {'battle': 1, 'enemy': 3},
+    {'battle': 2, 'enemy': 2},
+    {'battle': 3, 'enemy': 1},
+    {'battle': 4, 'enemy': 1},
+    {'battle': 5, 'boss': 1},
+]
+A1, B1, C1, D1, E1, F1, G1, H1, I1, \
+A2, B2, C2, D2, E2, F2, G2, H2, I2, \
+A3, B3, C3, D3, E3, F3, G3, H3, I3, \
+A4, B4, C4, D4, E4, F4, G4, H4, I4, \
+A5, B5, C5, D5, E5, F5, G5, H5, I5, \
+A6, B6, C6, D6, E6, F6, G6, H6, I6, \
+    = MAP.flatten()
+
+
+class Config(ConfigBase):
+    # ===== Start of generated config =====
+    MAP_HAS_MAP_STORY = True
+    MAP_HAS_FLEET_STEP = False
+    MAP_HAS_AMBUSH = False
+    # ===== End of generated config =====
+
+
+class Campaign(CampaignBase):
+    MAP = MAP
+    ENEMY_FILTER = '1L > 1M > 1E > 1C > 2L > 2M > 2E > 2C > 3L > 3M > 3E > 3C'
+
+    def battle_0(self):
+        if self.clear_filter_enemy(self.ENEMY_FILTER, preserve=0):
+            return True
+        return self.battle_default()
+
+    def battle_5(self):
+        return self.fleet_boss.clear_boss()
