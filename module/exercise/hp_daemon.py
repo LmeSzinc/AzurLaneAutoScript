83b59cbc86b1df541d3538a80a41bb0b14731913
LmeSzinc <37934724+LmeSzinc@users.noreply.github.com>
2024-09-25 14:31:31 +0800

    Fix: Handle early quit on backline skills
    Exercise_LowHpConfirmWait is unused now, because skill animation lasts much longer

--- module/exercise/hp_daemon.py
+++ module/exercise/hp_daemon.py
@@ -13,7 +13,7 @@ class HpDaemon(ModuleBase):
     low_hp_confirm_timer: Timer
 
     @staticmethod
-    def _calculate_hp(image, area, reverse=False, starter=2, prev_color=(239, 32, 33)):
+    def _calculate_hp(image, area, reverse=False, starter=2, prev_color=(239, 32, 33), threshold=30):
         """
         Args:
             image:
@@ -21,6 +21,7 @@ class HpDaemon(ModuleBase):
             reverse: True if HP is left align.
             starter:
             prev_color:
+            threshold:
 
         Returns:
             float: HP. 0 to 1.
@@ -39,7 +40,8 @@ class HpDaemon(ModuleBase):
         #         prev_index = index
         #
         # return prev_index / length
-        return color_bar_percentage(image, area, prev_color=prev_color, starter=starter, reverse=reverse)
+        return color_bar_percentage(
+            image, area, prev_color=prev_color, starter=starter, reverse=reverse, threshold=threshold)
 
     def _show_hp(self, low_hp_time=0):
         """
@@ -55,8 +57,8 @@ class HpDaemon(ModuleBase):
             text += ' - Low HP: %ss' % str(round(low_hp_time, 3)).ljust(5, '0')
         logger.info(text)
 
-    def _at_low_hp(self, image, pause=None):
-        if pause is None or pause == PAUSE:
+    def _at_low_hp(self, image, pause=PAUSE):
+        if pause == PAUSE:
             self.attacker_hp = self._calculate_hp(image, area=ATTACKER_HP_AREA.area, reverse=True)
             self.defender_hp = self._calculate_hp(image, area=DEFENDER_HP_AREA.area, reverse=False)
         elif pause in [PAUSE_New, PAUSE_Iridescent_Fantasy]:
@@ -67,6 +69,9 @@ class HpDaemon(ModuleBase):
             self.attacker_hp = self._calculate_hp(image, area=ATTACKER_HP_AREA.area, reverse=True)
             self.defender_hp = self._calculate_hp(image, area=DEFENDER_HP_AREA.area, reverse=False)
 
+        # Opponent died or HP bar get covered
+        if self.defender_hp < 0.01:
+            self.low_hp_confirm_timer.reset()
         if 0.01 < self.attacker_hp <= self.config.Exercise_LowHpThreshold:
             if self.low_hp_confirm_timer.reached() and self.low_hp_confirm_timer.current() < 300:
                 self._show_hp(self.low_hp_confirm_timer.current())
