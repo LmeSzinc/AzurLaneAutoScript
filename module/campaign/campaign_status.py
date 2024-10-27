9ff5de178356749c230a62f9c80fd341057ae525
Zuosizhu <2839299264@qq.com>
2024-10-27 13:25:40 +0800

    Fix: Merge error causing Ocr failure
    (cherry picked from commit 47608ccfb2b7cced17eebc3d52171124462cf3fe)

--- module/campaign/campaign_status.py
+++ module/campaign/campaign_status.py
@@ -14,10 +14,10 @@ from module.ocr.ocr import Digit, Ocr
 from module.ui.ui import UI
 from module.log_res.log_res import LogRes
 
-if server.server != 'jp':
-    OCR_COIN = Digit(OCR_COIN, name='OCR_COIN', letter=(239, 239, 239), threshold=128)
-else:
-    OCR_COIN = Digit(OCR_COIN, name='OCR_COIN', letter=(201, 201, 201), threshold=128)
+#if server.server != 'jp':
+#    OCR_COIN = Digit(OCR_COIN, name='OCR_COIN', letter=(239, 239, 239), threshold=128)
+#else:
+#    OCR_COIN = Digit(OCR_COIN, name='OCR_COIN', letter=(201, 201, 201), threshold=128)
 
 
 class PtOcr(Ocr):
@@ -120,7 +120,10 @@ class CampaignStatus(UI):
         color = get_color(self.device.image, OCR_OIL_CHECK.button)
         if color_similar(color, OCR_OIL_CHECK.color):
             # Original color
-            ocr = Digit(_button, name=name, letter=(247, 247, 247), threshold=128)
+            if server.server != 'jp':
+                ocr = Digit(_button, name=name, letter=(247, 247, 247), threshold=128)
+            else:
+                ocr = Digit(_button, name=name, letter=(201, 201, 201), threshold=128)
         elif color_similar(color, (59, 59, 64)):
             # With black overlay
             ocr = Digit(_button, name=name, letter=(165, 165, 165), threshold=128)
