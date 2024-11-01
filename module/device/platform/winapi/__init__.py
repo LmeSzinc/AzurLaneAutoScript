31c4c60937533082034f948876fa854c4873f5c0
PPPlatelet <161500686+PPPlatelet@users.noreply.github.com>
2024-11-01 22:28:08 +0800

    Upd: fix winapi/__init__.py

--- module/device/platform/winapi/__init__.py
+++ module/device/platform/winapi/__init__.py
@@ -1,5 +1,3 @@
 from module.device.platform.winapi.const_windows import *
 from module.device.platform.winapi.functions_windows import *
 from module.device.platform.winapi.structures_windows import *
-
-__all__ = [name for name in dir() if not name.startswith('_')]
