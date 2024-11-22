be37625f50117cfe6fcb65b8d9d25b2f42837aa8
LmeSzinc <37934724+LmeSzinc@users.noreply.github.com>
2024-11-22 21:49:44 +0800

    Add: Chapter TTL

--- campaign/event_20241121_cn/campaign_base.py
+++ campaign/event_20241121_cn/campaign_base.py
@@ -6,9 +6,21 @@ from module.logger import logger
 class CampaignBase(CampaignBase_):
     STAGE_INCREASE = [
         'T1 > T2 > T3 > T4 > T5 > T6',
-        'ttl1 > ttl2 > ttl3 > ttl4 > ttl5',
+        'TTL1 > TTL2 > TTL3 > TTL4 > TTL5',
     ]
 
+    @staticmethod
+    def _campaign_get_chapter_index(name):
+        """
+        Args:
+            name (str, int):
+
+        Returns:
+            int
+        """
+        if name == 'ttl':
+            return 1
+        return CampaignBase_._campaign_get_chapter_index(name)
 
     def campaign_set_chapter(self, name, mode='normal'):
         """
@@ -17,16 +29,24 @@ class CampaignBase(CampaignBase_):
             mode (str): 'normal' or 'hard'.
         """
         chapter, stage = self._campaign_separate_name(name)
+        logger.info([chapter, stage])
 
         if chapter in ['t']:
             self.ui_goto_event()
             MODE_SWITCH_20240725.set('combat', main=self)
             if stage in ['1', '2', '3', '4', '5']:
                 CHAPTER_SWITCH_20241024.set('ab', main=self)
-            elif stage in ['6']:
+            else:
+                logger.warning(f'Stage {name} is not in CHAPTER_SWITCH_20241024')
+            self.campaign_ensure_chapter(index=chapter)
+        elif chapter in ['ttl']:
+            self.ui_goto_event()
+            MODE_SWITCH_20240725.set('combat', main=self)
+            if stage in ['1', '2', '3', '4', '5']:
                 CHAPTER_SWITCH_20241024.set('cd', main=self)
             else:
                 logger.warning(f'Stage {name} is not in CHAPTER_SWITCH_20241024')
+            logger.info('campaign_ensure_chapter')
             self.campaign_ensure_chapter(index=chapter)
         elif chapter in ['ex_sp']:
             self.ui_goto_event()
