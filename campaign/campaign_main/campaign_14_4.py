95adf4d593c3ebd76186d16ed886f136d297c683
dragonheart107 <153553566+dragonheart107@users.noreply.github.com>
2025-01-10 18:36:54 +0100

    revert 14-4 changes due to further testing needed

--- campaign/campaign_main/campaign_14_4.py
+++ campaign/campaign_main/campaign_14_4.py
@@ -113,7 +113,17 @@ class Campaign(CampaignBase):
 
     def battle_0(self):
         self.pick_up_light_house(A9)
-        self.pick_up_flare(A5)
+
+        if self.clear_roadblocks([road_A8, road_H9], weakest=False):
+            return True
+        if self.clear_filter_enemy(self.ENEMY_FILTER, preserve=1):
+            return True
+
+        return self.battle_default()
+
+    def battle_3(self):
+        self.pick_up_light_house(A9)
+        self.pick_up_ammo()
         self.pick_up_flare(H9)
 
         if self.clear_roadblocks([road_A8, road_H9], weakest=False):
@@ -123,13 +133,19 @@ class Campaign(CampaignBase):
 
         return self.battle_default()
 
-    def battle_1(self):
+    def battle_6(self):
         self.pick_up_light_house(A9)
-        self.pick_up_ammo(H5)
+        self.pick_up_ammo()
+        self.pick_up_flare(H9)
+
+        if self.clear_roadblocks([road_A8, road_H9], weakest=False):
+            return True
+        if self.clear_filter_enemy(self.ENEMY_FILTER, preserve=0):
+            return True
 
         return self.battle_default()
 
     def battle_7(self):
+        self.fleet_boss.pick_up_flare(A5)
 
         return self.fleet_boss.clear_boss()
-    
