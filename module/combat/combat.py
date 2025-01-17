66dabddd72ce0aff5b0f202ceba018a13d1bfb77
sui_feng <115386623+sui-feng-cb@users.noreply.github.com>
2025-01-17 23:56:15 +0800

    Upd: raid EXP_INFO_D

--- module/combat/combat.py
+++ module/combat/combat.py
@@ -418,6 +418,12 @@ class Combat(Level, HPBalancer, Retirement, SubmarineCall, CombatAuto, CombatMan
         if self.appear_then_click(EXP_INFO_B):
             self.device.sleep((0.25, 0.5))
             return True
+        if self.appear_then_click(EXP_INFO_C):
+            self.device.sleep((0.25, 0.5))
+            return True
+        if self.appear_then_click(EXP_INFO_D):
+            sslf.device.sleep((0.25, 0.5))
+            return True
 
         return False
 
