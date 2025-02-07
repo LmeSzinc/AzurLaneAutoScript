efbc6a5cbfc442a9607c4851472a48d7144e420b
Shane Xue <qq2198631773@outlook.com>
2025-02-07 20:12:58 +0800

    Remove mistakenly ctrl-c-ed override

--- campaign/campaign_main/campaign_3_base.py
+++ campaign/campaign_main/campaign_3_base.py
@@ -9,7 +9,3 @@ class CampaignBase(CampaignBase_):
         if self.ui_page_appear(page_campaign):
             return False
         return super().handle_exp_info()
-
-    def map_data_init(self, map_):
-        super().map_data_init(map_)
-        self.config.override(EnemyPriority_EnemyScaleBalanceWeight='default_mode')
