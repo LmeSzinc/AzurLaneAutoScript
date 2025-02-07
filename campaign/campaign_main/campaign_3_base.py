b4e46bdd14b77a08fcab7d95fc23a64c9633ea44
Shane Xue <qq2198631773@outlook.com>
2025-02-07 18:32:12 +0800

    Bugfix Resolved misrecognition of EXP_INFO_B in campaign_main_2 and campaign_main_3. Refer to issue #4553. Reason was the background map was similar to the exp info page. Recognition also appears in-stage since the chapter name flashes past but this will have no adverse affects.

---
+++ campaign/campaign_main/campaign_3_base.py
@@ -0,0 +1,15 @@
+from module.campaign.campaign_base import CampaignBase as CampaignBase_
+from module.logger import logger
+from module.ui.page import page_campaign
+
+
+class CampaignBase(CampaignBase_):
+    def handle_exp_info(self):
+        # Random background of Main Chapter 3 hits EXP_INFO_B
+        if self.ui_page_appear(page_campaign):
+            return False
+        return super().handle_exp_info()
+
+    def map_data_init(self, map_):
+        super().map_data_init(map_)
+        self.config.override(EnemyPriority_EnemyScaleBalanceWeight='default_mode')
