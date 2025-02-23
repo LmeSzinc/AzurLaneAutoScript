28cb29e3ceedc5152923224daa607bedbd3264eb
guoh064 <50830808+guoh064@users.noreply.github.com>
2025-02-23 11:19:04 +0800

    Fix: Check current ime before getting clipboard

--- module/equipment/equipment_code.py
+++ module/equipment/equipment_code.py
@@ -221,6 +221,8 @@ class EquipmentCodeHandler(StorageHandler):
     def clear_all_equip(self):
         self.enter_equip_code_page()
         ship = self.current_ship()
+        self.device.u2_set_fastinput_ime(True)
+        logger.attr("Current_ime", self.device.u2_current_ime())
         self.click_export_button()
         if self.codes.__getattribute__(ship) is None:
             self.export_equip_code(ship)
