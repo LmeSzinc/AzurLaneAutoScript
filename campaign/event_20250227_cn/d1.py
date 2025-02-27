dfa3b74667857be65931f4e75bb456973451569e
sui-feng-cb <wyy13761770280@126.com>
2025-02-27 13:54:24 +0800

    Add: event_20250227_cn entrance

---
+++ campaign/event_20250227_cn/d1.py
@@ -0,0 +1,100 @@
+from .campaign_base import CampaignBase
+from module.map.map_base import CampaignMap
+from module.map.map_grids import SelectedGrids, RoadGrids
+from module.logger import logger
+
+MAP = CampaignMap('D1')
+MAP.shape = 'K8'
+MAP.camera_data = ['D2', 'D6', 'H2', 'H6']
+MAP.camera_data_spawn_point = ['D2']
+MAP.map_data = """
+    -- -- -- ++ SP -- SP ++ ++ ++ MB
+    -- ME ++ ++ -- __ -- -- Me -- --
+    ME -- Me -- -- MS -- -- -- -- ME
+    -- -- -- -- MS -- MS Me -- ++ --
+    -- ME ++ Me -- -- ++ ++ -- ME --
+    -- -- ++ -- Me ++ ++ ++ -- -- --
+    ++ -- ME -- -- ME -- ME -- -- ME
+    -- -- -- -- ++ -- ME -- -- ME ++
+"""
+MAP.weight_data = """
+    50 50 50 50 50 50 50 50 50 50 50
+    50 50 50 50 50 50 50 50 50 50 50
+    50 50 50 50 50 50 50 50 50 50 50
+    50 50 50 50 50 50 50 50 50 50 50
+    50 50 50 50 50 50 50 50 50 50 50
+    50 50 50 50 50 50 50 50 50 50 50
+    50 50 50 50 50 50 50 50 50 50 50
+    50 50 50 50 50 50 50 50 50 50 50
+"""
+MAP.spawn_data = [
+    {'battle': 0, 'enemy': 2, 'siren': 2},
+    {'battle': 1, 'enemy': 1},
+    {'battle': 2, 'enemy': 2},
+    {'battle': 3, 'enemy': 1},
+    {'battle': 4, 'enemy': 2},
+    {'battle': 5, 'enemy': 1, 'boss': 1},
+]
+A1, B1, C1, D1, E1, F1, G1, H1, I1, J1, K1, \
+A2, B2, C2, D2, E2, F2, G2, H2, I2, J2, K2, \
+A3, B3, C3, D3, E3, F3, G3, H3, I3, J3, K3, \
+A4, B4, C4, D4, E4, F4, G4, H4, I4, J4, K4, \
+A5, B5, C5, D5, E5, F5, G5, H5, I5, J5, K5, \
+A6, B6, C6, D6, E6, F6, G6, H6, I6, J6, K6, \
+A7, B7, C7, D7, E7, F7, G7, H7, I7, J7, K7, \
+A8, B8, C8, D8, E8, F8, G8, H8, I8, J8, K8, \
+    = MAP.flatten()
+
+
+class Config:
+    # ===== Start of generated config =====
+    MAP_SIREN_TEMPLATE = []
+    MOVABLE_ENEMY_TURN = (2,)
+    MAP_HAS_SIREN = True
+    MAP_HAS_MOVABLE_ENEMY = True
+    MAP_HAS_MAP_STORY = False
+    MAP_HAS_FLEET_STEP = True
+    MAP_HAS_AMBUSH = False
+    MAP_HAS_MYSTERY = False
+    # ===== End of generated config =====
+
+    STAGE_ENTRANCE = ['half', '20240725']
+    MAP_CHAPTER_SWITCH_20241219 = True
+    MAP_HAS_MODE_SWITCH = True
+    MAP_HAS_MOVABLE_NORMAL_ENEMY = True
+    MAP_SIREN_HAS_BOSS_ICON_SMALL = True
+
+    MOVABLE_NORMAL_ENEMY_TURN = (2,)
+    MAP_SIREN_MOVE_WAIT = 0.7
+    INTERNAL_LINES_FIND_PEAKS_PARAMETERS = {
+        'height': (80, 255 - 33),
+        'width': (0.9, 10),
+        'prominence': 10,
+        'distance': 35,
+    }
+    EDGE_LINES_FIND_PEAKS_PARAMETERS = {
+        'height': (255 - 33, 255),
+        'prominence': 10,
+        'distance': 50,
+        # 'width': (0, 7),
+        'wlen': 1000
+    }
+    MAP_SWIPE_MULTIPLY = (1.254, 1.278)
+    MAP_SWIPE_MULTIPLY_MINITOUCH = (1.213, 1.235)
+    MAP_SWIPE_MULTIPLY_MAATOUCH = (1.178, 1.199)
+
+
+class Campaign(CampaignBase):
+    MAP = MAP
+    ENEMY_FILTER = '1L > 1M > 1E > 1C > 2L > 2M > 2E > 2C > 3L > 3M > 3E > 3C'
+
+    def battle_0(self):
+        if self.clear_siren():
+            return True
+        if self.clear_filter_enemy(self.ENEMY_FILTER, preserve=0):
+            return True
+
+        return self.battle_default()
+
+    def battle_5(self):
+        return self.fleet_boss.clear_boss()
