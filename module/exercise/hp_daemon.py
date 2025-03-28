eaf008f55559d0636bdb792ea3c43e48b64eacb6
LmeSzinc <37934724+LmeSzinc@users.noreply.github.com>
2025-03-29 00:47:34 +0800

    Upd: Battle UI Nurse

--- module/exercise/hp_daemon.py
+++ module/exercise/hp_daemon.py
@@ -1,16 +1,8 @@
 from module.base.base import ModuleBase
 from module.base.timer import Timer
 from module.base.utils import color_bar_percentage
-from module.combat_ui.assets import (
-    PAUSE,
-    PAUSE_Christmas,
-    PAUSE_Cyber,
-    PAUSE_HolyLight,
-    PAUSE_Iridescent_Fantasy,
-    PAUSE_Neon,
-    PAUSE_New,
-    PAUSE_Pharaoh,
-)
+from module.combat_ui.assets import (PAUSE, PAUSE_Christmas, PAUSE_Cyber, PAUSE_HolyLight, PAUSE_Iridescent_Fantasy,
+                                     PAUSE_Neon, PAUSE_New, PAUSE_Nurse, PAUSE_Pharaoh)
 from module.exercise.assets import *
 from module.logger import logger
 
@@ -78,6 +70,7 @@ class HpDaemon(ModuleBase):
             PAUSE_Cyber,
             PAUSE_HolyLight,
             PAUSE_Pharaoh,
+            PAUSE_Nurse,
         ]:
             self.attacker_hp = self._calculate_hp(image, area=ATTACKER_HP_AREA_New.area, reverse=True)
             self.defender_hp = self._calculate_hp(image, area=DEFENDER_HP_AREA_New.area, reverse=True)
