06fc46147fc5735262a44f5e1054d8d3c830c2bc
LmeSzinc <37934724+LmeSzinc@users.noreply.github.com>
2025-05-22 00:23:46 +0800

    Upd: Battle UI Seaside

--- module/exercise/hp_daemon.py
+++ module/exercise/hp_daemon.py
@@ -1,8 +1,7 @@
 from module.base.base import ModuleBase
 from module.base.timer import Timer
 from module.base.utils import color_bar_percentage
-from module.combat_ui.assets import (PAUSE, PAUSE_Christmas, PAUSE_Cyber, PAUSE_HolyLight, PAUSE_Iridescent_Fantasy,
-                                     PAUSE_Neon, PAUSE_New, PAUSE_Nurse, PAUSE_Pharaoh, PAUSE_Devil)
+from module.combat_ui.assets import *
 from module.exercise.assets import *
 from module.logger import logger
 
@@ -72,6 +71,7 @@ class HpDaemon(ModuleBase):
             PAUSE_Pharaoh,
             PAUSE_Nurse,
             PAUSE_Devil,
+            PAUSE_Seaside,
         ]:
             self.attacker_hp = self._calculate_hp(image, area=ATTACKER_HP_AREA_New.area, reverse=True)
             self.defender_hp = self._calculate_hp(image, area=DEFENDER_HP_AREA_New.area, reverse=True)
