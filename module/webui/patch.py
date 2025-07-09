843e573aad85fc715b8b20a15f6f221871c6d9f7
LmeSzinc <37934724+LmeSzinc@users.noreply.github.com>
2025-07-10 03:02:32 +0800

    Upd: patch mimetype

--- module/webui/patch.py
+++ module/webui/patch.py
@@ -39,3 +39,28 @@ def patch_executor():
 
     loop = asyncio.get_event_loop()
     loop.set_default_executor(CachedThreadPoolExecutor.executor)
+
+
+def patch_mimetype():
+    """
+    Patch mimetype db to use the builtin table instead of reading from environment.
+
+    By default, mimetype reads user configured mimetype table from environment. It's good for server but bad on our
+    side, because we deploy on user's machine which may have polluted environment. To have a consistent behaviour on
+    all deployment, we use the builtin mimetype table only.
+    """
+    import mimetypes
+    if mimetypes.inited:
+        # ohno mimetypes already inited
+        db = mimetypes.MimeTypes()
+        mimetypes._db = db
+        # override global variable
+        mimetypes.encodings_map = db.encodings_map
+        mimetypes.suffix_map = db.suffix_map
+        mimetypes.types_map = db.types_map[True]
+        mimetypes.common_types = db.types_map[False]
+    else:
+        # init db with the default table
+        db = mimetypes.MimeTypes()
+        mimetypes._db = db
+        mimetypes.inited = True
