7ca937ebdd2a9e504610013399cda18409d2f5aa
MetricVoid <metricvoidlx@gmail.com>
2025-07-11 12:28:00 -0400

    Inherit map

--- campaign/campaign_main/campaign_15_4_121.py
+++ campaign/campaign_main/campaign_15_4_121.py
@@ -1,49 +1,12 @@
-from module.logger import logger
-from module.map.map_base import CampaignMap
-from module.map.map_grids import SelectedGrids, RoadGrids
+import copy
 
-from .campaign_15_base import CampaignBase, W15GridInfo
-from .campaign_15_base import Config as ConfigBase
+# Do not remove the Config import. It's inherited from 15-4 and needs to be exported.
+from .campaign_15_4 import Config # noqa # pylint: disable=unused-import
+from .campaign_15_4 import MAP as MAP_15_4, Campaign as Campaign_15_4 
 
-MAP = CampaignMap('15-4-121')
-MAP.grid_class = W15GridInfo
-MAP.shape = 'K9'
-MAP.camera_data = ['C2', 'C5', 'C7', 'F2', 'F5', 'F7', 'H2', 'H5', 'H7']
-MAP.camera_data_spawn_point = ['H2']
-MAP.camera_sight = (-2, -1, 3, 2)
-MAP.map_data = """
-    Me -- ME ME Me -- ME ++ ++ ME ME
-    ME -- -- -- -- ME -- ++ ++ -- ME
-    ++ -- -- MS -- -- ME SP SP ME Me
-    ++ ME -- ++ ++ -- -- -- -- ME --
-    -- Me ME MA ++ ME -- MS -- -- ME
-    ME ME ME -- -- -- -- ++ ME -- Me
-    ME -- __ -- ME ME -- ME ME -- ++
-    -- -- ++ -- Me -- ME ME ME Me ME
-    MB Me -- ME ME Me ++ ++ ++ -- Me
-"""
-MAP.weight_data = """
-    50 50 50 50 50 50 50 50 50 50 50
-    50 50 50 50 50 50 50 50 50 50 50
-    50 50 50 50 50 50 50 50 50 50 50
-    50 50 50 50 50 50 50 50 50 50 50
-    50 50 50 50 50 50 50 50 50 50 50
-    50 50 50 50 50 50 50 50 50 50 50
-    50 50 50 50 50 50 50 50 50 50 50
-    50 50 50 50 50 50 50 50 50 50 50
-    50 50 50 50 50 50 50 50 50 50 50
-"""
-MAP.spawn_data = [
-    {'battle': 0, 'enemy': 8},
-    {'battle': 1, 'enemy': 1},
-    {'battle': 2, 'enemy': 1},
-    {'battle': 3, 'enemy': 1, 'siren': 1},
-    {'battle': 4, 'enemy': 2},
-    {'battle': 5},
-    {'battle': 6, 'siren': 1},
-    {'battle': 7, 'enemy': 1},
-    {'battle': 8, 'boss': 1},
-]
+MAP = copy.copy(MAP_15_4)
+MAP.name = '15-4-121'
+
 A1, B1, C1, D1, E1, F1, G1, H1, I1, J1, K1, \
 A2, B2, C2, D2, E2, F2, G2, H2, I2, J2, K2, \
 A3, B3, C3, D3, E3, F3, G3, H3, I3, J3, K3, \
@@ -55,40 +18,8 @@ A8, B8, C8, D8, E8, F8, G8, H8, I8, J8, K8, \
 A9, B9, C9, D9, E9, F9, G9, H9, I9, J9, K9, \
     = MAP.flatten()
 
-
-class Config(ConfigBase):
-    # ===== Start of generated config =====
-    # MAP_SIREN_TEMPLATE = ['BOSS']
-    # MOVABLE_ENEMY_TURN = (2,)
-    # MAP_HAS_SIREN = True
-    # MAP_HAS_MOVABLE_ENEMY = True
-    MAP_HAS_MAP_STORY = False
-    MAP_HAS_FLEET_STEP = False
-    MAP_HAS_AMBUSH = True
-    # MAP_HAS_MYSTERY = True
-    # ===== End of generated config =====
-
-    MAP_SWIPE_MULTIPLY = (1.055, 1.075)
-    MAP_SWIPE_MULTIPLY_MINITOUCH = (1.020, 1.039)
-    MAP_SWIPE_MULTIPLY_MAATOUCH = (0.990, 1.008)
-
-
-class Campaign(CampaignBase):
+class Campaign(Campaign_15_4):
     MAP = MAP
-
-    def battle_function(self):
-        if not self.config.MAP_CLEAR_ALL_THIS_TIME:
-            return super().battle_function()
-
-        if self.battle_count in [3, 6] \
-                or (self.battle_count in [0, 1] and not self.map_is_clear_mode):
-            func = self.FUNCTION_NAME_BASE + str(self.battle_count)
-            logger.info(f'Using function: {func}')
-            func = self.__getattribute__(func)
-            result = func()
-            return result
-
-        return super().battle_function()
 
     def battle_0(self):
         if not self.map_is_clear_mode and self.map_has_mob_move:
