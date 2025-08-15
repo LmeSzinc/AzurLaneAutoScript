cf56488e5f64516dc41be00fb4318fef15a5a6c4
LmeSzinc <37934724+LmeSzinc@users.noreply.github.com>
2025-08-16 00:21:55 +0800

    Chore: simplify set chapter override

--- campaign/event_20250814_cn/campaign_base.py
+++ campaign/event_20250814_cn/campaign_base.py
@@ -6,20 +6,7 @@ from module.logger import logger
 class CampaignBase(CampaignBase_):
 
     def campaign_set_chapter_20241219(self, chapter, stage, mode='combat'):
-        if chapter == 't':
-            if self._campaign_name_is_hard(f'{chapter}{stage}'):
-                self.config.override(Campaign_Mode='hard')
-            self.ui_goto_event()
-            MODE_SWITCH_20241219.set('combat', main=self)
-            if stage in ['1', '2', '3']:
-                ASIDE_SWITCH_20241219.set('part1', main=self)
-            elif stage in ['4', '5', '6']:
-                ASIDE_SWITCH_20241219.set('part2', main=self)
-            else:
-                logger.warning(f'Stage {chapter}{stage} is not in event')
-            self.campaign_ensure_chapter(chapter)
-            return True
-        if chapter == 'ht':
+        if chapter in ['t', 'ht']:
             if self._campaign_name_is_hard(f'{chapter}{stage}'):
                 self.config.override(Campaign_Mode='hard')
             self.ui_goto_event()
