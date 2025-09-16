c11103b2df61bca36703d2e5a986e3bfad76ecfa
雪风大人保佑你 <hanyecheng0907@gmail.com>
2025-09-16 22:51:47 +0800

    Update campaign_7_2.py
    7-2三战撤退，因为是在地图文件上直接修改，所以没打过7-2的萌新需通过7-2再使用此功能

--- campaign/campaign_main/campaign_7_2.py
+++ campaign/campaign_main/campaign_7_2.py
@@ -48,43 +48,61 @@ class Campaign(CampaignBase):
     MAP = MAP
 
     def battle_0(self):
-        if self.fleet_2_step_on(FLEET_2_STEP_ON, roadblocks=[ROAD_MAIN]):
-            return True
+        if self.config.C72MysteryFarming_StepOnA3:
+            if self.fleet_2_step_on(FLEET_2_STEP_ON, roadblocks=[ROAD_MAIN]):
+                return True
 
-        ignore = None
-        if self.fleet_at(A3, fleet=2) and A1.enemy_scale != 3 and not self.fleet_at(A1, fleet=1):
-            ignore = SelectedGrids([A2])
-        if self.fleet_at(G3, fleet=2):
-            ignore = SelectedGrids([H3])
-        self.clear_all_mystery(nearby=False, ignore=ignore)
+            ignore = None
+            if self.fleet_at(A3, fleet=2) and A1.enemy_scale != 3 and not self.fleet_at(A1, fleet=1):
+                ignore = SelectedGrids([A2])
+            if self.fleet_at(G3, fleet=2):
+                ignore = SelectedGrids([H3])
+
+            self.clear_all_mystery(nearby=False, ignore=ignore)
+        else:
+            self.clear_all_mystery(nearby=False)
 
         if self.clear_roadblocks([ROAD_MAIN], strongest=True):
-            return True
-
-        if self.clear_enemy(scale=(3,)):
             return True
         if self.clear_potential_roadblocks([ROAD_MAIN], strongest=True):
             return True
 
-        if self.clear_enemy(strongest=True):
+        if self.clear_enemy(scale=(3,)):
+            return True
+
+        if self.clear_grids_for_faster(GRIDS_FOR_FASTER, scale=(2,)):
+            return True
+        if self.clear_enemy(scale=(2,)):
+            return True
+        if self.clear_grids_for_faster(GRIDS_FOR_FASTER):
             return True
 
         return self.battle_default()
 
-    def battle_5(self):
-        ignore = None
-        if self.fleet_at(A3, fleet=2):
-            ignore = SelectedGrids([A2])
-        if self.fleet_at(G3, fleet=2):
-            ignore = SelectedGrids([H3])
-        self.clear_all_mystery(nearby=False, ignore=ignore)
+    def battle_3(self):
+        if self.config.C72MysteryFarming_StepOnA3:
+            ignore = None
+            if self.fleet_at(A3, fleet=2):
+                ignore = SelectedGrids([A2])
+            if self.fleet_at(G3, fleet=2):
+                ignore = SelectedGrids([H3])
+            self.clear_all_mystery(nearby=False, ignore=ignore)
 
-        if self.clear_roadblocks([ROAD_MAIN]):
-            return True
+            if self.fleet_at(A3, fleet=2) and A2.is_mystery:
+                self.fleet_2.clear_chosen_mystery(A2)
+            if self.fleet_at(G3, fleet=2) and H3.is_mystery:
+                self.fleet_2.clear_chosen_mystery(H3)
+        else:
+            self.clear_all_mystery(nearby=False)
 
-        if self.fleet_at(A3, fleet=2) and A2.is_mystery:
-            self.fleet_2.clear_chosen_mystery(A2)
-        if self.fleet_at(G3, fleet=2) and H3.is_mystery:
-            self.fleet_2.clear_chosen_mystery(H3)
+        if self.map.select(is_mystery=True, is_accessible=False):
+            logger.info('Roadblock blocks mystery.')
+            if self.fleet_1.clear_roadblocks([ROAD_MAIN]):
+                return True
 
-        return self.fleet_boss.clear_boss()
+        if not self.map.select(is_mystery=True):
+            self.withdraw()
+
+    @property
+    def _map_battle(self):
+        return 3
