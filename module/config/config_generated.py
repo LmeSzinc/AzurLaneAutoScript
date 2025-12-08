356a7cabfc89008ccbc30a717ea5e2aee99c4307
1156925917 <84648397+1156925917@users.noreply.github.com>
2025-12-08 19:04:03 +0800

    Revert "squash air11 dashboard changes"

--- module/config/config_generated.py
+++ module/config/config_generated.py
@@ -155,12 +155,6 @@ class GeneratedConfig:
     GemsFarming_EquipmentCode = 'DD: null\nbogue: null\nhermes: null\nlangley: null\nranger: null'
     GemsFarming_CommissionLimit = True
 
-    # Group `CampaignStorage`
-    CampaignStorage_Oil = {}
-    CampaignStorage_Coin = {}
-    CampaignStorage_Gem = {}
-    CampaignStorage_Pt = {}
-
     # Group `EventGeneral`
     EventGeneral_PtLimit = 0
     EventGeneral_TimeLimit = datetime.datetime(2020, 1, 1, 0, 0)
@@ -347,8 +341,6 @@ class GeneratedConfig:
     PrivateQuarters_BuyCake = False
     PrivateQuarters_TargetInteract = True
     PrivateQuarters_TargetShip = 'anchorage'  # anchorage, noshiro, sirius, new_jersey, taihou
-    # Group `GachaStorage`
-    GachaStorage_Cube = {}
 
     # Group `Daily`
     Daily_UseDailySkip = True
@@ -444,11 +436,6 @@ class GeneratedConfig:
 
     # Group `OpsiHazard1Leveling`
     OpsiHazard1Leveling_TargetZone = 0  # 0, 44, 22
-
-    # Group `OpsiStorage`
-    OpsiStorage_YellowCoin = {}
-    OpsiStorage_PurpleCoin = {}
-    OpsiStorage_ActionPoint = {}
 
     # Group `Daemon`
     Daemon_EnterMap = True
