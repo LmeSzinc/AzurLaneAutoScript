a7cf03411124d66496a49f2c6acbd413ab8bd476
guoh064 <50830808+guoh064@users.noreply.github.com>
2026-03-13 00:11:38 +0800

    Del: [JP] temporary MetaDigitCounter color

--- module/os_ash/meta.py
+++ module/os_ash/meta.py
@@ -47,10 +47,7 @@ class MetaDigitCounter(DigitCounter):
 class Meta(UI, MapEventHandler):
 
     def digit_ocr_point_and_check(self, button: Button, check_number: int):
-        if server.server != 'jp':
-            point_ocr = MetaDigitCounter(button, letter=(235, 235, 235), threshold=160, name='POINT_OCR')
-        else:
-            point_ocr = MetaDigitCounter(button, letter=(192, 192, 192), threshold=160, name='POINT_OCR')
+        point_ocr = MetaDigitCounter(button, letter=(235, 235, 235), threshold=160, name='POINT_OCR')
         point, _, _ = point_ocr.ocr(self.device.image)
         if point >= check_number:
             return True
