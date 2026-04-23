f6b44d65826a8c05c5daca4a68ecd214485bd4ff
guoh064 <50830808+guoh064@users.noreply.github.com>
2026-04-23 15:53:06 +0800

    Add: event entrance for dual event 20201126/20260417

--- campaign/event_20260417_cn/campaign_base.py
+++ campaign/event_20260417_cn/campaign_base.py
@@ -1,12 +1,23 @@
 from module.base.button import Button
+from module.campaign.assets import EVENT_20260417_PT_ICON
 from module.campaign.campaign_base import CampaignBase as CampaignBase_
 from module.logger import logger
+from module.ui.page import page_campaign_menu, page_event
 
 EVENT_ANIMATION = Button(area=(49, 229, 119, 400), color=(118, 215, 240), button=(49, 229, 119, 400),
                          name='EVENT_ANIMATION')
 
 
 class CampaignBase(CampaignBase_):
+    def ui_goto_event(self):
+        if self.appear(EVENT_20260417_PT_ICON, offset=(20, 20)) and self.ui_page_appear(page_event):
+            logger.info('Already at EVENT_20260417')
+            return True
+        self.ui_ensure(page_campaign_menu)
+        if self.is_event_entrance_available():
+            self.ui_goto(page_event)
+            return True
+
     @staticmethod
     def _campaign_ocr_result_process(result):
         result = CampaignBase_._campaign_ocr_result_process(result)
