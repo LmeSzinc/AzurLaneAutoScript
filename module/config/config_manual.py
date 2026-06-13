02913dd151fe47e0f22efbc1a8665d9fff1c3033
guoh064 <50830808+guoh064@users.noreply.github.com>
2026-06-14 00:20:19 +0800

    Add: [CN, JP] island business

--- module/config/config_manual.py
+++ module/config/config_manual.py
@@ -22,7 +22,8 @@ class ManualConfig:
     > OpsiAshBeacon
     > OpsiDaily > OpsiShop > OpsiVoucher
     > OpsiAbyssal > OpsiStronghold > OpsiObscure > OpsiArchive
-    > IslandFreebie > IslandCollect > IslandSeasonTask
+    > IslandFreebie > IslandCollect
+    > IslandBusiness > IslandSeasonTask
     > Daily > Hard > OpsiAshBeacon > OpsiAshAssist > OpsiMonthBoss
     > Sos > EventSp > EventA > EventB > EventC > EventD
     > RaidDaily > CoalitionSp > WarArchives > MaritimeEscort
