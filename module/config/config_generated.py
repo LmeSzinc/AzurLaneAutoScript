b3abc16eb44e75f6ca8baddda173c9a32b97e8f6
nEEtdo0d <66565274+nEEtdo0d@users.noreply.github.com>
2026-06-28 05:35:47 -0400

    Add: [EN] add nakhimov private quarters support (#5761)

--- module/config/config_generated.py
+++ module/config/config_generated.py
@@ -339,7 +339,7 @@ class GeneratedConfig:
     PrivateQuarters_BuyRoses = True
     PrivateQuarters_BuyCake = False
     PrivateQuarters_TargetInteract = True
-    PrivateQuarters_TargetShip = 'anchorage'  # anchorage, noshiro, sirius, new_jersey, taihou, aegir
+    PrivateQuarters_TargetShip = 'anchorage'  # anchorage, noshiro, sirius, new_jersey, taihou, aegir, nakhimov
 
     # Group `Daily`
     Daily_UseDailySkip = True
