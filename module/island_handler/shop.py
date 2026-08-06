111ec2b0bfb4844ec7cf4a5f76d4937aebcdf747
guoh064 <50830808+guoh064@users.noreply.github.com>
2026-08-06 20:00:40 +0800

    Refactor: IslandShopUI for fish meat exchange

--- module/island_handler/shop.py
+++ module/island_handler/shop.py
@@ -4,8 +4,9 @@ import module.config.server as server
 from module.base.decorator import cached_property, del_cached_property
 from module.base.button import ButtonGrid
 from module.island.data import DIC_ISLAND_SHOP, DIC_ISLAND_SHOP_ITEM_TO_RECIPE, DIC_ISLAND_SHOP_RECIPE
-from module.island.ui import IslandUI, NestedNavbar
+from module.island.ui import NestedNavbar
 from module.island_handler.assets import *
+from module.island_handler.shop_ui import IslandShopUI
 from module.logger import logger
 from module.ocr.ocr import Digit, Ocr
 from module.ui.assets import ISLAND_GOTO_ISLAND_SHOP
@@ -17,45 +18,7 @@ from module.ui_white.assets import BACK_ARROW_WHITE
 SHOP_ITEM_NAME_AREA = (12, 142, 134, 163)
 
 
-class IslandShop(IslandUI):
-    has_shop_banner = False
-
-    def ui_goto_island_shop(self):
-        self.ui_goto(page_island)
-        for _ in self.loop():
-            if self.ui_page_appear(page_island_shop, offset=(0, 20)):
-                return True
-            elif self.appear(ISLAND_SHOP_MILL_CHECK, offset=(20, 20)):
-                return True
-            elif self.appear(ISLAND_SHOP_RECOMMEND, offset=(0, 20)):
-                self.has_shop_banner = True
-                return True
-            if self.appear_then_click(ISLAND_GOTO_ISLAND_SHOP, offset=(20, 20), interval=1):
-                continue
-
-    @cached_property
-    def _island_shop_side_navbar(self):
-        if not self.has_shop_banner:
-            return NestedNavbar(
-                grids=ButtonGrid(origin=(12, 80), delta=(0, 70), button_shape=(127, 70), grid_shape=(1, 6), name='ISLAND_SHOP_NAVBAR'),
-                subgrid_delta=(0, 58), subgrid_button_shape=(127, 58),
-                subgrid_shapes=[(1, 1), (1, 2), (1, 1), (1, 1), (1, 2), (1, 0)],
-                direction='vertical',
-            )
-        else:
-            return NestedNavbar(
-                grids=ButtonGrid(origin=(12, 80), delta=(0, 70), button_shape=(127, 70), grid_shape=(1, 7), name='ISLAND_SHOP_NAVBAR'),
-                subgrid_delta=(0, 58), subgrid_button_shape=(127, 58),
-                subgrid_shapes=[(1, 0), (1, 1), (1, 2), (1, 1), (1, 1), (1, 2), (1, 0)],
-                direction='vertical',
-            )
-
-    def island_shop_side_navbar_ensure(self, main_index, sub_index=None, skip_first_screenshot=True):
-        if not self.has_shop_banner:
-            return self._island_shop_side_navbar.set(self, main_index=main_index, sub_index=sub_index, skip_first_screenshot=skip_first_screenshot)
-        else:
-            return self._island_shop_side_navbar.set(self, main_index=main_index+1, sub_index=sub_index, skip_first_screenshot=skip_first_screenshot)
-
+class IslandShop(IslandShopUI):
     @property
     def _island_shop_item_grid(self):
         return ButtonGrid(
@@ -271,21 +234,6 @@ class IslandShop(IslandUI):
                     success = self._island_shop_tab.set(main=self, left=index+1)
                     return True
         return success
-
-    def wait_island_shop_loading(self):
-        for _ in self.loop(timeout=1.5):
-            # poor implementation, shop anime has blinking effect of about 1.5 seconds
-            pass
-        for _ in self.loop(timeout=5):
-            if self.ui_page_appear(page_island_shop, offset=(0, 20)):
-                return True
-            if self.appear(ISLAND_SHOP_MILL_CHECK, offset=(20, 20)):
-                return True
-            if self.appear(ISLAND_SHOP_RECOMMEND, offset=(0, 20)):
-                self.has_shop_banner = True
-                return True
-            if self.appear(ISLAND_SHOP_LOADING, offset=(20, 20)):
-                continue
 
     def island_shop_buy(self, shopping_dict={}, isolated=True):
         """
