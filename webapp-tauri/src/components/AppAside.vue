963bf8cc331d747958782fc1707771c656d8f07f
Joxos <xujunhao61@163.com>
2026-08-14 18:33:40 +0800

    fix(webui): aside icons matching the original layout
    - Load develop/run/setting svg icons in the aside buttons via CSS mask
      so they follow the text color in both themes
    - Adjust btn-aside padding for the inline icon layout

--- webapp-tauri/src/components/AppAside.vue
+++ webapp-tauri/src/components/AppAside.vue
@@ -20,27 +20,30 @@ function stateClass(state: number): string {
 <template>
   <aside class="app-aside">
     <button
-      class="btn-aside"
+      class="btn btn-aside"
       :class="{ 'btn-aside-active': active === 'Home' }"
       @click="emit('select', 'Home')"
     >
+      <span class="aside-icon icon-develop" />
       Home
     </button>
     <button
       v-for="inst in status.instances"
       :key="inst.name"
-      class="btn-aside"
+      class="btn btn-aside"
       :class="{ 'btn-aside-active': active === inst.name }"
       @click="emit('select', inst.name)"
     >
+      <span class="aside-icon icon-run" />
       <span class="aside-state-dot" :class="stateClass(inst.state)" />
       {{ inst.name }}
     </button>
     <button
-      class="btn-aside"
+      class="btn btn-aside"
       :class="{ 'btn-aside-active': active === 'Manage' }"
       @click="emit('select', 'Manage')"
     >
+      <span class="aside-icon icon-setting" />
       Manage
     </button>
   </aside>
@@ -56,6 +59,27 @@ function stateClass(state: number): string {
   flex-shrink: 0;
   display: flex;
   flex-direction: column;
+}
+.aside-icon {
+  display: block;
+  width: 2rem;
+  height: 2rem;
+  margin: 0 auto 2px;
+  background-color: currentColor;
+  -webkit-mask: no-repeat center / contain;
+  mask: no-repeat center / contain;
+}
+.icon-develop {
+  -webkit-mask-image: url('/icon/develop.svg');
+  mask-image: url('/icon/develop.svg');
+}
+.icon-run {
+  -webkit-mask-image: url('/icon/run.svg');
+  mask-image: url('/icon/run.svg');
+}
+.icon-setting {
+  -webkit-mask-image: url('/icon/setting.svg');
+  mask-image: url('/icon/setting.svg');
 }
 .aside-state-dot {
   display: inline-block;
