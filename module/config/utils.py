2f1d9c17aa3cf3203798513ab056d0d85ffeb185
guoh064 <50830808+guoh064@users.noreply.github.com>
2026-08-14 14:34:52 +0800

    Fix: get_os_next_reset for local EST/EDT timezone (#5359)

--- module/config/utils.py
+++ module/config/utils.py
@@ -361,7 +361,8 @@ def get_os_next_reset():
     server_now = datetime.now() - diff
     server_reset = (server_now.replace(day=1) + timedelta(days=32)) \
         .replace(day=1, hour=0, minute=0, second=0, microsecond=0)
-    local_reset = server_reset + diff
+    server_reset = server_reset.replace(tzinfo=timezone(server_timezone()))
+    local_reset = server_reset.astimezone().replace(tzinfo=None)
     return local_reset
 
 
