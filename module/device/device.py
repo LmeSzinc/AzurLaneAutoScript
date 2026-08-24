b95a6cd18da3e89cd75f7146a245ac8fb5dc743f
Horizon101011 <43370844+Horizon101011@users.noreply.github.com>
2026-08-25 00:04:13 +0800

    Fix: DroidCast actually support to SDK 33

--- module/device/device.py
+++ module/device/device.py
@@ -153,7 +153,7 @@ class Device(Screenshot, Control, AppControl):
             if not (self.is_emulator and self.is_ldplayer_bluestacks_family):
                 logger.warning('ScreenshotMethod ldopengl is available on LD Player only, fallback to auto')
                 self.config.Emulator_ScreenshotMethod = 'auto'
-        # DroidCast is available on SDK 23 (Android 6.0) to SDK 32 (Android 12)
+        # DroidCast is available on SDK 23 (Android 6.0) to SDK 33 (Android 12)
         if self.config.Emulator_ScreenshotMethod in ['DroidCast', 'DroidCast_raw']:
             if self.sdk_ver < 23 or self.sdk_ver > 32:
                 logger.warning(f'ScreenshotMethod {self.config.Emulator_ScreenshotMethod} is available on '
