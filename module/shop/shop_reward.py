99f4d8097ae154261da895c75335725add8eaf63
MingSDzzz <47287314+MingSDzzz@users.noreply.github.com>
2026-09-17 22:44:11 +0800

    Add per-shop purchase labels to other shops

--- module/shop/shop_reward.py
+++ module/shop/shop_reward.py
@@ -23,23 +23,27 @@ class RewardShop(ShopUI):
         self.device.click_record_clear()
         self.shop_nav_250814.set(NAV_GENERAL, main=self)
         self.shop_tab_250814.set(TAB_MERIT, main=self)
-        MeritShop_250814(self.config, self.device).run()
+        if self.config.MeritShop_Enable:
+            MeritShop_250814(self.config, self.device).run()
 
         self.device.click_record_clear()
         self.shop_nav_250814.set(NAV_GENERAL, main=self)
         self.shop_tab_250814.set(TAB_GUILD, main=self)
-        GuildShop_250814(self.config, self.device).run()
+        if self.config.GuildShop_Enable:
+            GuildShop_250814(self.config, self.device).run()
 
         # core limited, core monthly, medal, prototype
         self.device.click_record_clear()
         self.shop_nav_250814.set(NAV_MONTHLY, main=self)
         self.shop_tab_250814.set(TAB_CORE_MONTHLY, main=self)
-        CoreShop_250814(self.config, self.device).run()
+        if self.config.CoreShop_Enable:
+            CoreShop_250814(self.config, self.device).run()
 
         self.device.click_record_clear()
         self.shop_nav_250814.set(NAV_MONTHLY, main=self)
         self.shop_tab_250814.set(TAB_MEDAL, main=self)
-        MedalShop2_250814(self.config, self.device).run()
+        if self.config.MedalShop2_Enable:
+            MedalShop2_250814(self.config, self.device).run()
 
         self.config.task_delay(server_update=True)
 
